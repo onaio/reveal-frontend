4130bf6389c9b2de4a955a037b25ccd954205dac
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-02-11 16:19:51 +0300

    Add Header component

---
+++ src/components/page/Header.tsx
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import {
+  Collapse,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+} from 'reactstrap';
+
+import { WEBSITE_NAME } from '../../constants';
+
+interface State {
+  isOpen: boolean;
+}
+
+class Header extends React.Component<State> {
+  constructor(props: any) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  private toggle() {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
+  private render() {
+    return (
+      <div>
+        <Navbar color="light" light={true} expand="md">
+          <NavbarBrand href="/">{WEBSITE_NAME}</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar={true}>
+            <Nav className="ml-auto" navbar={true}>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav={true} inNavbar={true}>
+                <DropdownToggle nav={true} caret={true}>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right={true}>
+                  <DropdownItem>Option 1</DropdownItem>
+                  <DropdownItem>Option 2</DropdownItem>
+                  <DropdownItem divider={true} />
+                  <DropdownItem>Reset</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Header;
