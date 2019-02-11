a44835d971b103fabdb0545799b5e10093383dc1
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-02-11 18:20:43 +0300

    Reformat test for App

---
+++ src/tests/App.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
+
+describe('App', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
