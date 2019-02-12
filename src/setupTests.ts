dff55d593e5cf568a6f0a3d88216ed82ef8387c3
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-02-12 15:51:56 +0300

    Add fontawesome icon library for tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,7 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
 import enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 enzyme.configure({ adapter: new Adapter() });
+library.add(faUser);
