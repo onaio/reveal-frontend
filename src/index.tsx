0c3951ae62170e3b63358898e939705b2e28e685
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-02-12 16:54:34 +0300

    Add BrowserRouter to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('reveal-root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('reveal-root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
