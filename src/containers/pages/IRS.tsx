3425b877c9bb65253a59820a66db02c40b54924a
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-02-12 16:54:05 +0300

    Add IRS page container

---
+++ src/containers/pages/IRS.tsx
@@ -0,0 +1,14 @@
+// this is the IRS page component
+import * as React from 'react';
+
+class IRS extends React.Component<{}, {}> {
+  constructor(props: {}) {
+    super(props);
+  }
+
+  public render() {
+    return <div>IRS</div>;
+  }
+}
+
+export default IRS;
