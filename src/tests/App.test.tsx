db37a2981490fdf79ac3935e2af33a123a12c8d4
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-02-12 17:36:32 +0300

    Fix broken tests

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,6 +1,10 @@
+import { createBrowserHistory } from 'history';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from 'react-router';
 import App from '../App';
+
+const history = createBrowserHistory();
 
 describe('App', () => {
   beforeEach(() => {
@@ -9,7 +13,12 @@ describe('App', () => {
 
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(
+      <Router history={history}>
+        <App />
+      </Router>,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 });
