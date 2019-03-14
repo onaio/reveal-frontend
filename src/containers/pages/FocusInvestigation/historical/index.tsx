c9437e10c21a675f8205cf651f9134b1c5120b49
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-14 16:16:10 +0300

    Add basic table

--- src/containers/pages/FocusInvestigation/historical/index.tsx
+++ src/containers/pages/FocusInvestigation/historical/index.tsx
@@ -1,6 +1,8 @@
 // this is the FocusInvestigation page component
+import DrillDownTable from '@onaio/drill-down-table';
 import * as React from 'react';
 import HeaderBreadcrumb from '../../../../components/page/HeaderBreadcrumb/HeaderBreadcrumb';
+import { data } from './tests/fixtures';
 
 /** Historical data reporting for Focus Investigation */
 class HistoricalFocusInvestigation extends React.Component<{}, {}> {
@@ -9,10 +11,80 @@ class HistoricalFocusInvestigation extends React.Component<{}, {}> {
   }
 
   public render() {
+    const tableProps = {
+      columns: [
+        {
+          Header: 'Location',
+          columns: [
+            {
+              Header: '',
+              accessor: 'name',
+            },
+          ],
+        },
+        {
+          Header: 'Mapped Foci Areas',
+          columns: [
+            {
+              Header: 'Total',
+              accessor: 'Total',
+            },
+            {
+              Header: 'A1',
+              accessor: 'A1',
+            },
+            {
+              Header: 'A2',
+              accessor: 'A2',
+            },
+            {
+              Header: 'B1',
+              accessor: 'B1',
+            },
+            {
+              Header: 'B2',
+              accessor: 'B2',
+            },
+          ],
+        },
+        {
+          Header: 'A1 screened twice in last year',
+          columns: [
+            {
+              Header: 'Number',
+              accessor: 'Number',
+            },
+            {
+              Header: '%',
+              accessor: '%',
+            },
+          ],
+        },
+        {
+          Header: 'F1 response',
+          columns: [
+            {
+              Header: 'adherence',
+              accessor: 'Adherence',
+            },
+          ],
+        },
+      ],
+      data,
+      identifierField: 'name',
+      linkerField: 'name',
+      minRows: 0,
+      parentIdentifierField: 'parent',
+      rootParentId: null,
+      showPageSizeOptions: false,
+      showPagination: false,
+    };
     return (
       <div>
         <HeaderBreadcrumb />
-        <div>Focus Investigation</div>
+        <div>
+          <DrillDownTable {...tableProps} />
+        </div>
       </div>
     );
   }
