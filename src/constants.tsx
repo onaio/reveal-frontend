98c5b622a93cf604b1e92342556d10c90b0e2a55
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-14 18:57:36 +0300

    Add indicator colors and thresholds to constants

--- src/constants.tsx
+++ src/constants.tsx
@@ -7,3 +7,21 @@ export type IRS_URL = typeof IRS_URL;
 
 export const FOCUS_INVESTIGATION_URL = '/focus-investigation';
 export type FOCUS_INVESTIGATION_URL = typeof FOCUS_INVESTIGATION_URL;
+
+// colors
+export const GREEN = '#33ad33';
+export type GREEN = typeof GREEN;
+export const YELLOW = '#ff3';
+export type YELLOW = typeof YELLOW;
+export const ORANGE = '#ff8533';
+export type ORANGE = typeof ORANGE;
+export const RED = '#ff5c33';
+export type RED = typeof RED;
+
+// thresholds
+export const GREEN_THRESHOLD = 0.9;
+export type GREEN_THRESHOLD = typeof GREEN_THRESHOLD;
+export const YELLOW_THRESHOLD = 0.2;
+export type YELLOW_THRESHOLD = typeof YELLOW_THRESHOLD;
+export const ORANGE_THRESHOLD = 0.8;
+export type ORANGE_THRESHOLD = typeof ORANGE_THRESHOLD;
