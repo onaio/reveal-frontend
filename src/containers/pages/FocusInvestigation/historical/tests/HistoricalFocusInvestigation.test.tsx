60f17642e153816d0ccc86c083d694550177e56a
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-20 16:23:19 +0300

    Improve tests for HistoricalFocusInvestigation

--- src/containers/pages/FocusInvestigation/historical/tests/HistoricalFocusInvestigation.test.tsx
+++ src/containers/pages/FocusInvestigation/historical/tests/HistoricalFocusInvestigation.test.tsx
@@ -2,7 +2,8 @@ import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import { createBrowserHistory } from 'history';
 import React from 'react';
-import { Router } from 'react-router';
+import { Route, Router, Switch } from 'react-router';
+import { Link } from 'react-router-dom';
 import HistoricalFocusInvestigation from '../../historical';
 
 const history = createBrowserHistory();
@@ -13,20 +14,107 @@ describe('containers/pages/HistoricalFocusInvestigation', () => {
   });
 
   it('renders without crashing', () => {
+    const mock: any = jest.fn();
+    const props = {
+      history,
+      location: mock,
+      match: {
+        isExact: true,
+        params: { id: 'Province 1' },
+        path: '/focus-investigation/:id',
+        url: '/focus-investigation/Province 1',
+      },
+    };
     shallow(
       <Router history={history}>
-        <HistoricalFocusInvestigation />
+        <HistoricalFocusInvestigation {...props} />
       </Router>
     );
   });
 
   it('renders HistoricalFocusInvestigation correctly', () => {
+    const mock: any = jest.fn();
+    const props = {
+      history,
+      location: mock,
+      match: {
+        isExact: true,
+        params: {},
+        path: '/focus-investigation/:id',
+        url: '/focus-investigation/Province 1',
+      },
+    };
     const wrapper = mount(
       <Router history={history}>
-        <HistoricalFocusInvestigation />
+        <HistoricalFocusInvestigation {...props} />
       </Router>
     );
     expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.unmount();
+  });
+
+  it('drilling down HistoricalFocusInvestigation works correctly', () => {
+    const wrapper = mount(
+      <Router history={history}>
+        <div>
+          <Link to="/focus-investigation">Start</Link>
+          <Switch>
+            <Route
+              exact={true}
+              path="/focus-investigation"
+              component={HistoricalFocusInvestigation}
+            />
+            <Route
+              exact={true}
+              path="/focus-investigation/:id"
+              component={HistoricalFocusInvestigation}
+            />
+          </Switch>
+        </div>
+      </Router>
+    );
+    expect(wrapper.find('a[href$="focus-investigation"]').length).toEqual(1);
+    wrapper
+      .find('a[href$="focus-investigation"]')
+      .first()
+      .simulate('click', { button: 0 });
+    expect(wrapper.find('.rt-td a[href$="focus-investigation/Province 1"]').length).toEqual(1);
+    wrapper
+      .find('a[href$="focus-investigation/Province 1"]')
+      .first()
+      .simulate('click', { button: 0 });
+    wrapper
+      .find('a[href$="focus-investigation/Province 1"]')
+      .first()
+      .simulate('click', { button: 0 });
+    expect(wrapper.find('a[href$="focus-investigation/District 1"]').length).toEqual(1);
+    wrapper
+      .find('a[href$="focus-investigation/District 1"]')
+      .first()
+      .simulate('click', { button: 0 });
+    wrapper
+      .find('a[href$="focus-investigation/District 1"]')
+      .first()
+      .simulate('click', { button: 0 });
+    expect(wrapper.find('a[href$="focus-investigation/Canton 1"]').length).toEqual(1);
+    wrapper
+      .find('a[href$="focus-investigation/Canton 1"]')
+      .first()
+      .simulate('click', { button: 0 });
+    wrapper
+      .find('a[href$="focus-investigation/Canton 1"]')
+      .first()
+      .simulate('click', { button: 0 });
+    expect(wrapper.find('a[href$="focus-investigation/Village 1"]').length).toEqual(1);
+    wrapper
+      .find('a[href$="focus-investigation/Village 1"]')
+      .first()
+      .simulate('click', { button: 0 });
+    wrapper
+      .find('a[href$="focus-investigation/Village 1"]')
+      .first()
+      .simulate('click', { button: 0 });
+    expect(toJson(wrapper.find('ReactTable'))).toMatchSnapshot();
     wrapper.unmount();
   });
 });
