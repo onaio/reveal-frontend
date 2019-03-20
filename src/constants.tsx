6ae2c23077c41f3e250f724e8e5274f990553ce3
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-20 19:06:25 +0300

    Update constants

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,11 +1,24 @@
+// env vars
 export const WEBSITE_NAME = process.env.REACT_APP_WEBSITE_NAME;
 export type WEBSITE_NAME = typeof WEBSITE_NAME;
+
+// strings
+export const LOCATION = 'Location';
+export type LOCATION = typeof LOCATION;
+export const PROVINCE = 'Province';
+export type PROVINCE = typeof PROVINCE;
+export const HOME = 'Home';
+export type HOME = typeof HOME;
+export const FOCUS_INVESTIGATION = 'Focus Investigation';
+export type FOCUS_INVESTIGATION = typeof FOCUS_INVESTIGATION;
+export const FOCUS_INVESTIGATIONS = 'Focus Investigations';
+export type FOCUS_INVESTIGATIONS = typeof FOCUS_INVESTIGATIONS;
 
 // internal urls
 export const IRS_URL = '/irs';
 export type IRS_URL = typeof IRS_URL;
-export const FOCUS_INVESTIGATION_URL = '/focus-investigation';
-export type FOCUS_INVESTIGATION_URL = typeof FOCUS_INVESTIGATION_URL;
+export const FI_URL = '/focus-investigation';
+export type FI_URL = typeof FI_URL;
 
 // colors
 export const GREEN = '#33ad33';
