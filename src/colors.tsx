3d21957d9a256cc1d4ca0dcb57db1247eadcb3e8
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-21 16:13:37 +0300

    Add colors module

---
+++ src/colors.tsx
@@ -0,0 +1,9 @@
+// colors
+export const GREEN = '#33ad33';
+export type GREEN = typeof GREEN;
+export const YELLOW = '#ff3';
+export type YELLOW = typeof YELLOW;
+export const ORANGE = '#ff8533';
+export type ORANGE = typeof ORANGE;
+export const RED = '#ff5c33';
+export type RED = typeof RED;
