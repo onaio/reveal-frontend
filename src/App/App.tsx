9e7e19e1e25d6967c8b85824396451a2bf305bad
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-25 14:59:55 +0300

    Register map icon

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,17 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faMap } from '@fortawesome/free-solid-svg-icons';
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
 import { Col, Container, Row } from 'reactstrap';
 import Header from '../components/page/Header/Header';
 import { FI_URL, IRS_URL } from '../constants';
+import ActiveFocusInvestigation from '../containers/pages/FocusInvestigation/active';
 import HistoricalFocusInvestigation from '../containers/pages/FocusInvestigation/historical';
 import Home from '../containers/pages/Home/Home';
 import IRS from '../containers/pages/IRS/IRS';
 
+library.add(faMap);
 library.add(faUser);
 
 import './App.css';
@@ -23,8 +26,17 @@ class App extends Component {
             <Switch>
               <Route exact={true} path="/" component={Home} />
               <Route exact={true} path={IRS_URL} component={IRS} />
-              <Route exact={true} path={FI_URL} component={HistoricalFocusInvestigation} />
-              <Route exact={true} path={`${FI_URL}/:id`} component={HistoricalFocusInvestigation} />
+              <Route exact={true} path={FI_URL} component={ActiveFocusInvestigation} />
+              <Route
+                exact={true}
+                path={`${FI_URL}/historical`}
+                component={HistoricalFocusInvestigation}
+              />
+              <Route
+                exact={true}
+                path={`${FI_URL}/historical/:id`}
+                component={HistoricalFocusInvestigation}
+              />
             </Switch>
           </Col>
         </Row>
