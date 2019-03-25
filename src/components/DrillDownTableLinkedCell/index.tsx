961f9c366322b11719304d2149e47825fb76c96c
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-25 16:58:20 +0300

    Add a way to specify url path for DrillDownTableLinkedCell

--- src/components/DrillDownTableLinkedCell/index.tsx
+++ src/components/DrillDownTableLinkedCell/index.tsx
@@ -3,16 +3,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FI_URL } from '../../constants';
 
+/** Interface for linked cell props */
+export interface LinkedCellProps extends DropDownCellProps {
+  urlPath?: string;
+}
+
 /** Component that will be rendered in drop-down table cells showing a link
  * that moves you to the next hierarchical level.
  */
-const DrillDownTableLinkedCell: React.ElementType<DropDownCellProps> = (
-  props: DropDownCellProps
-) => {
-  const { cellValue, hasChildren } = props;
+const DrillDownTableLinkedCell: React.ElementType<LinkedCellProps> = (props: LinkedCellProps) => {
+  const { cellValue, hasChildren, urlPath } = props;
   return (
-    <div>{hasChildren ? <Link to={`${FI_URL}/${cellValue}`}>{cellValue}</Link> : cellValue}</div>
+    <div>{hasChildren ? <Link to={`${urlPath}/${cellValue}`}>{cellValue}</Link> : cellValue}</div>
   );
+};
+
+DrillDownTableLinkedCell.defaultProps = {
+  urlPath: `${FI_URL}/historical`,
 };
 
 export default DrillDownTableLinkedCell;
