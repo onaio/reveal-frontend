9132ee266e9a4d28593a9c1951b2a41dd187d567
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-25 15:39:35 +0300

    Add `get137Adherencendicator`

--- src/helpers/indicators.tsx
+++ src/helpers/indicators.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { CellInfo } from 'react-table';
 import { GREEN, ORANGE, RED, YELLOW } from '../colors';
-import { GREEN_THRESHOLD, ORANGE_THRESHOLD, YELLOW_THRESHOLD } from '../constants';
+import { GREEN_THRESHOLD, ORANGE_THRESHOLD, THRESHOLD_137, YELLOW_THRESHOLD } from '../constants';
 import { percentage } from '../helpers/utils';
 
 /** Returns a table cell rendered with different colors based on focus
@@ -23,6 +23,22 @@ export function getFIAdherencendicator(row: CellInfo) {
       }}
     >
       {percentage(row.value)}
+    </div>
+  );
+}
+
+/** Returns a table cell rendered with different colors based on focus
+ * investigation 1-3-7 adherence conditional formatting
+ */
+export function get137Adherencendicator(row: CellInfo) {
+  return (
+    <div
+      className="indicator-container"
+      style={{
+        color: row.value > THRESHOLD_137 ? GREEN : row.value < THRESHOLD_137 ? RED : ORANGE,
+      }}
+    >
+      {row.value}d to go
     </div>
   );
 }
