7a436562685241bd39ee94796f06c3c48e71b91f
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-04-15 17:52:53 +0300

    Add function to generate location columns

--- src/helpers/utils.tsx
+++ src/helpers/utils.tsx
@@ -1,3 +1,7 @@
+import { uniq } from 'lodash';
+import { Column } from 'react-table';
+import { locationHierarchy, LocationItem } from '../configs/locations';
+
 /** Interface for an object that is allowed to have any property */
 export interface FlexObject {
   [key: string]: any;
@@ -11,4 +15,33 @@ export interface RouteParams {
 /** Returns a number as a decimal e.g. 0.18 becomes 18% */
 export function percentage(num: number, decimalPoints: number = 0) {
   return `${(num * 100).toFixed(decimalPoints)}%`;
+}
+
+/** Gets react table columns from the location hierarchy in configs */
+export function getLocationColumns(
+  locations: LocationItem[] = locationHierarchy,
+  padHeader: boolean = false
+): Column[] {
+  const locationSet = uniq(locations);
+
+  if (padHeader === true) {
+    return locationSet.map(el => {
+      return {
+        Header: el.name,
+        columns: [
+          {
+            Header: '',
+            accessor: el.identifier,
+          },
+        ],
+      };
+    });
+  }
+
+  return locationSet.map(el => {
+    return {
+      Header: el.name,
+      accessor: el.identifier,
+    };
+  });
 }
