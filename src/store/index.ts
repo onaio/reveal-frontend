33da6c8fa14da2582d8112fd86e6aed9a708007b
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-04-16 15:27:44 +0300

    Use `getConnectedStore` from @onaio/connected-reducer-registry to create the store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,45 +1,6 @@
-import { combine } from '@onaio/redux-reducer-registry';
-import reducerRegistry from '@onaio/redux-reducer-registry';
-import { connectRouter, routerMiddleware } from 'connected-react-router';
-import { createBrowserHistory } from 'history';
-import { applyMiddleware, compose, createStore, Reducer } from 'redux';
-import thunk from 'redux-thunk';
+import { connectReducer, getConnectedStore } from '@onaio/connected-reducer-registry';
+import reducerRegistry, { combine, Registry } from '@onaio/redux-reducer-registry';
 import users, { reducerName as usersReducer } from './ducks/users';
-
-/** Add redux devtools to Window */
-declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-  }
-}
-
-/** Declare history variable */
-export const history = createBrowserHistory();
-
-/** Declare type for reducer Registry */
-export interface Registry {
-  [key: string]: Reducer;
-}
-
-/** Function to create the store */
-export function getStore(reducers: Registry) {
-  /** Register each of the initial reducers */
-  Object.keys(reducers).forEach(reducerName => {
-    reducerRegistry.register(reducerName, reducers[reducerName]);
-  });
-
-  /** Combine reducers */
-  const reducer = combine(reducerRegistry.getReducers());
-
-  /** Add redux devtools to enhancers */
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-  /** Create the store */
-  return createStore(reducer, composeEnhancers(applyMiddleware(thunk, routerMiddleware(history))));
-}
-
-/** Router reducer */
-const connectReducer = connectRouter(history);
 
 /** Initial reducers in the reducer registry */
 const defaultReducers: Registry = {
@@ -50,7 +11,7 @@ const defaultReducers: Registry = {
 defaultReducers[usersReducer] = users;
 
 /** The initial store for the reveal web app */
-const store = getStore(defaultReducers);
+const store = getConnectedStore(defaultReducers);
 
 /** Set listener to add reducers to store when registered */
 reducerRegistry.setChangeListener(reducers => {
