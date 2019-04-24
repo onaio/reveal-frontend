cec4fe33dccbfc17ab17e8c8ecc08d5336381ddc
cKellyDesign <ckelly@ona.io>
2019-04-24 18:26:49 -0400

    Init Gisida Wrapper Component

--- src/containers/pages/FocusInvestigation/map/active/index.tsx
+++ src/containers/pages/FocusInvestigation/map/active/index.tsx
@@ -1,138 +1,27 @@
-import reducerRegistry from '@onaio/redux-reducer-registry';
-import { Actions, ducks, loadLayers, prepareLayer } from 'gisida';
-import { Map } from 'gisida-react';
+import { Actions, prepareLayer } from 'gisida';
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import { AnyAction } from 'redux';
 
-import {
-  FlexObject,
-  MapConfigs,
-  MapProps,
-  RouteParams,
-  SiteConfig,
-  SiteConfigApp,
-  SiteConfigAppMapconfig,
-} from '../../../../../helpers/utils';
+import GisidaWrapper from '../../../../../components/GisidaWrapper';
+import { FlexObject, MapProps, RouteParams } from '../../../../../helpers/utils';
 import store from '../../../../../store';
-import './gisida.css';
-
-// Temporary Object containing different map config params
-// todo - dynamically generate these from somewhere
-const configs: MapConfigs = {
-  '13': {
-    accessToken: 'pk.eyJ1Ijoib25hIiwiYSI6IlVYbkdyclkifQ.0Bz-QOOXZZK01dq4MuMImQ',
-    apiAccessToken: '138a7ff6dfdcb5b4e41eb2d39bcc76ce5d296e89',
-    appName: 'Test Map',
-    layers: [
-      'https://raw.githubusercontent.com/onaio/cycloneidai-2019-data/master/moz/province-admin/province-admin.json',
-    ],
-    mapConfig: {
-      center: [33.852072, -18.850944],
-      container: 'map',
-      style: 'mapbox://styles/ona/cjestgt7ldbet2sqnqth4xx8c',
-      zoom: 6.5,
-    },
-  },
-  '14': {
-    accessToken: 'pk.eyJ1Ijoib25hIiwiYSI6IlVYbkdyclkifQ.0Bz-QOOXZZK01dq4MuMImQ',
-    apiAccessToken: '138a7ff6dfdcb5b4e41eb2d39bcc76ce5d296e89',
-    appName: 'Test Map 2',
-    layers: [
-      'https://raw.githubusercontent.com/onaio/zim-data/master/boundaries-labels/province/province-admin.json',
-      'https://raw.githubusercontent.com/onaio/zim-data/master/boundaries-labels/district/district-admin.json',
-    ],
-    mapConfigCenter: [27.199728142287427, -18.94022942134295],
-    mapConfigContainer: 'map',
-    mapConfigStyle: 'mapbox://styles/ona/cjestgt7ldbet2sqnqth4xx8c',
-    mapConfigZoom: 5.76,
-  },
-  '15': {
-    accessToken: 'pk.eyJ1Ijoib25hIiwiYSI6IlVYbkdyclkifQ.0Bz-QOOXZZK01dq4MuMImQ',
-    apiAccessToken: '138a7ff6dfdcb5b4e41eb2d39bcc76ce5d296e89',
-    appName: 'Test Map 3',
-    center: [69.13155473084771, 34.50960383103761],
-    layers: ['/config/layers/province-admin.json'],
-    style: 'mapbox://styles/ona/cjestgt7ldbet2sqnqth4xx8c',
-    zoom: 10.695873279884117,
-  },
-};
-
-/** Returns a single layer configuration */
-const LayerStore = (layer: any) => {
-  if (typeof layer === 'string') {
-    return layer;
-  }
-  // todo - dynamically build layer configs based on layerObj params and layer type defaults
-};
-
-/** Returns a single `site-config` object for a Gisida Map  */
-const ConfigStore = (options: FlexObject) => {
-  // Define basic config properties
-  const { accessToken, apiAccessToken, appName, mapConfig: mbConfig, layers } = options;
-  // Define flattened APP.mapConfig properties
-  const { mapConfigCenter, mapConfigContainer, mapConfigStyle, mapConfigZoom } = options;
-  // Define non-flattened APP.Config properties
-  const { center, container, style, zoom } = mbConfig || options;
-
-  // Build options for mapbox-gl-js initialization
-  const mapConfig: SiteConfigAppMapconfig = {
-    center: center || mapConfigCenter || [0, 0],
-    container: container || mapConfigContainer || 'map',
-    style: style || mapConfigStyle || 'mapbox://styles/ona/cjestgt7ldbet2sqnqth4xx8c',
-    zoom: zoom || mapConfigZoom || 0,
-  };
-  // Build APP options for Gisida
-  const APP: SiteConfigApp = {
-    accessToken,
-    apiAccessToken,
-    appName,
-    mapConfig,
-  };
-  // Build SiteConfig
-  const config: SiteConfig = {
-    APP,
-    LAYERS: layers.map(LayerStore),
-  };
-  return config;
-};
 
 /** Map View for Single Active Focus Investigation */
 class SingleActiveFIMap extends React.Component<RouteComponentProps<RouteParams> & MapProps, {}> {
   constructor(props: RouteComponentProps<RouteParams> & MapProps) {
     super(props);
-    const initialState = store.getState();
-    const { id } = props.match.params;
-    // 1. Register mapReducers in reducer registery;
-    if (!initialState.APP && ducks.APP) {
-      reducerRegistry.register('APP', ducks.APP.default);
-    }
-    if (!initialState['map-1'] && ducks.MAP) {
-      reducerRegistry.register('map-1', ducks.MAP.default);
-    }
-
-    // 2. Build/Load site-config js
-    if (typeof id !== 'undefined' && typeof configs[id] !== 'undefined') {
-      const config = ConfigStore(configs[id]);
-
-      // 3. Initialize Gisida stores
-      store.dispatch(Actions.initApp(config.APP));
-      loadLayers('map-1', store.dispatch, config.LAYERS);
-    }
   }
 
   public render() {
     const currentState = store.getState();
     const mapName = (currentState.APP && currentState.APP.appName) || '';
-    const doRenderMap = typeof currentState['map-1'] !== 'undefined';
 
     return (
       <div>
         <h2 className="page-title mt-4 mb-5">Map View: {mapName}</h2>
         <div className="map">
-          {doRenderMap ? (
-            <Map mapId={'map-1'} store={store} handlers={this.buildHandlers()} />
-          ) : null}
+          <GisidaWrapper id={this.props.match.params.id} handlers={this.buildHandlers()} />
         </div>
       </div>
     );
