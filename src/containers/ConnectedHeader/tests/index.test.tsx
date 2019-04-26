3729b6ddb99911efd69184939b280ffb130913dd
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-04-26 17:29:06 +0300

    Add connected header component

---
+++ src/containers/ConnectedHeader/tests/index.test.tsx
@@ -0,0 +1,41 @@
+import { authenticateUser } from '@onaio/session-reducer';
+import { mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router';
+import ConnectedHeader from '..';
+import store from '../../../store';
+
+describe('components/ConnectedHeader', () => {
+  it('renders the ConnectedHeader component', () => {
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter>
+          <ConnectedHeader />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(wrapper.find('Navbar').props()).toMatchSnapshot();
+    wrapper.unmount();
+  });
+
+  it('renders the ConnectedHeader logged in', () => {
+    store.dispatch(
+      authenticateUser(true, {
+        email: 'bob@example.com',
+        name: 'Bobbie',
+        username: 'RobertBaratheon',
+      })
+    );
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter>
+          <ConnectedHeader />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(wrapper.find('Navbar').props()).toMatchSnapshot();
+    wrapper.unmount();
+  });
+});
