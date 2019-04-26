a80991bef2578ba1281e1518f1a2c42d8ddfdd4b
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-04-26 17:07:20 +0300

    Add logout url as constant

--- src/constants.tsx
+++ src/constants.tsx
@@ -29,6 +29,8 @@ export type FOCUS_INVESTIGATIONS = typeof FOCUS_INVESTIGATIONS;
 // internal urls
 export const LOGIN_URL = '/login';
 export type LOGIN_URL = typeof LOGIN_URL;
+export const LOGOUT_URL = '/logout';
+export type LOGOUT_URL = typeof LOGOUT_URL;
 export const HOME_URL = '/';
 export type HOME_URL = typeof HOME_URL;
 export const IRS_URL = '/irs';
