75a6273d24d56b0b4f3254fe81acbf3b5ee26e25
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-04-26 17:06:42 +0300

    Update Header component tests

--- src/components/page/Header/tests/Header.test.tsx
+++ src/components/page/Header/tests/Header.test.tsx
@@ -3,7 +3,7 @@ import toJson from 'enzyme-to-json';
 import { createBrowserHistory } from 'history';
 import React from 'react';
 import { Router } from 'react-router';
-import Header from '../Header';
+import HeaderComponent from '../Header';
 
 const history = createBrowserHistory();
 
@@ -13,17 +13,51 @@ describe('components/page/Header', () => {
   });
 
   it('renders without crashing', () => {
+    const props = {
+      authenticated: false,
+      user: {
+        email: '',
+        name: '',
+        username: '',
+      },
+    };
     shallow(
       <Router history={history}>
-        <Header />
+        <HeaderComponent {...props} />
       </Router>
     );
   });
 
   it('renders header correctly', () => {
+    const props = {
+      authenticated: false,
+      user: {
+        email: '',
+        name: '',
+        username: '',
+      },
+    };
     const wrapper = mount(
       <Router history={history}>
-        <Header />
+        <HeaderComponent {...props} />
+      </Router>
+    );
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.unmount();
+  });
+
+  it('renders header correctly when authenticated', () => {
+    const props = {
+      authenticated: true,
+      user: {
+        email: 'bob@example.com',
+        name: 'Bobbie',
+        username: 'RobertBaratheon',
+      },
+    };
+    const wrapper = mount(
+      <Router history={history}>
+        <HeaderComponent {...props} />
       </Router>
     );
     expect(toJson(wrapper)).toMatchSnapshot();
