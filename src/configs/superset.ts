120798da113ac3eb48f0abbb3359dfeec7170313
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-05-02 14:54:33 +0300

    Add initial superset configs

---
+++ src/configs/superset.ts
@@ -0,0 +1,7 @@
+import { SupersetConfig } from '@onaio/superset-connector';
+
+/** superset configs */
+export const CONFIG: Partial<SupersetConfig> = {
+  base: 'https://discover.ona.io/',
+  endpoint: 'slice',
+};
