f1670942b7924d5757c8152f7070b967f2d84442
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-05-02 17:17:19 +0300

    Add superset service

--- src/services/superset/index.ts
+++ src/services/superset/index.ts
@@ -1,8 +1,17 @@
+import reducerRegistry from '@onaio/redux-reducer-registry';
 import superset from '@onaio/superset-connector';
 import { clone } from 'lodash';
 import { CONFIG } from '../../configs/superset';
 import store from '../../store';
-import { getApiToken } from '../../store/selectors';
+import supersetReducer, {
+  authorizeSuperset,
+  isAuthorized,
+  reducerName as supersetReducerName,
+} from '../../store/ducks/superset';
+import { getAccessToken } from '../../store/selectors';
+
+/** register the superset reducer */
+reducerRegistry.register(supersetReducerName, supersetReducer);
 
 /** middleware for fetching from Superset */
 export const fetchMiddleware = (res: { [key: string]: any }) => {
@@ -11,7 +20,7 @@ export const fetchMiddleware = (res: { [key: string]: any }) => {
 
 /** callback for fetching from Superset */
 export const fetchCallback = (parsedResponse: Array<{ [key: string]: any }>) => {
-  const sliceData = superset.processData(parsedResponse);
+  return superset.processData(parsedResponse);
 };
 
 /** generic async function for fetching from superset */
@@ -22,8 +31,23 @@ const supersetFetch = async (
 ) => {
   const config = clone(CONFIG);
   config.extraPath = sliceId;
-  config.token = getApiToken(store.getState());
-  superset.api.fetch(config, middleware).then(callback);
+  config.token = getAccessToken(store.getState());
+
+  const isSupersetAuthorized = isAuthorized(store.getState());
+
+  if (isSupersetAuthorized === true) {
+    return superset.api.fetch(config, middleware).then(callback);
+  } else {
+    superset.authZ(config, (result: { [key: string]: any }) => {
+      if (result.status === 200) {
+        store.dispatch(authorizeSuperset(true));
+        return superset.api.fetch(config, middleware).then(callback);
+      } else {
+        store.dispatch(authorizeSuperset(false));
+        return false;
+      }
+    });
+  }
 };
 
 export default supersetFetch;
