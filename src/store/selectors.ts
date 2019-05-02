76428661a44fe304deb5763d71c6d4eaaf56ff80
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-05-02 17:13:27 +0300

    Add selector for access token

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -1,3 +1,4 @@
+import { gateKeeperReducerName } from '@onaio/gatekeeper';
 import { reducerName as sessionReducerName } from '@onaio/session-reducer';
 import { Store } from 'redux';
 
@@ -7,4 +8,11 @@ import { Store } from 'redux';
 export function getApiToken(state: Partial<Store>): string {
   const extraData = (state as any)[sessionReducerName].extraData;
   return extraData.api_token || '';
+}
+
+/** get Access Token from the Redux store
+ * @param {Partial<Store>} state - the redux store
+ */
+export function getAccessToken(state: Partial<Store>): string {
+  return (state as any)[gateKeeperReducerName].accessToken;
 }
