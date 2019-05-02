ab11e066c4c7d9ed8a1b1015cf813098075476b0
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-05-02 14:54:16 +0300

    Add selector for API token

---
+++ src/store/selectors.ts
@@ -0,0 +1,10 @@
+import { reducerName as sessionReducerName } from '@onaio/session-reducer';
+import { Store } from 'redux';
+
+/** get API Token from the Redux store
+ * @param {Partial<Store>} state - the redux store
+ */
+export function getApiToken(state: Partial<Store>): string {
+  const extraData = (state as any)[sessionReducerName].extraData;
+  return extraData.api_token || '';
+}
