3bd1aa8e91a9dc1d1a2caeb3e9a56c590791f76d
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-05-03 15:08:51 +0300

    Add initial tests for superset service

--- src/services/superset/tests/index.test.ts
+++ src/services/superset/tests/index.test.ts
@@ -3,8 +3,10 @@ import fetchMock from 'fetch-mock';
 import supersetFetch from '../../../services/superset';
 import store from '../../../store';
 import supersetReducer, {
+  authorizeSuperset,
   isAuthorized,
   reducerName as supersetReducerName,
+  resetSuperset,
 } from '../../../store/ducks/superset';
 import * as fixtures from './fixtures';
 
@@ -17,12 +19,31 @@ describe('services/superset', () => {
   });
 
   it('authZ should not authorize superset when it fails', async () => {
+    store.dispatch(resetSuperset()); /** reset to null */
+
+    fetchMock.get('https://discover.ona.io/oauth-authorized/onadata', 500);
+    await supersetFetch('1337');
+    expect(isAuthorized(store.getState())).toEqual(false);
+
+    store.dispatch(authorizeSuperset(false));
+
+    await supersetFetch('1337');
+    expect(isAuthorized(store.getState())).toEqual(false);
+  });
+
+  it('authZ should authorize superset when it works', async () => {
     fetchMock.get('https://discover.ona.io/oauth-authorized/onadata', JSON.stringify({}));
     fetchMock.get(
       'https://discover.ona.io/superset/slice_json/1337',
       JSON.stringify(fixtures.sliceResponse)
     );
+
+    store.dispatch(resetSuperset()); /** reset to null */
     await supersetFetch('1337');
-    expect(isAuthorized(store.getState())).toEqual(false);
+    expect(isAuthorized(store.getState())).toEqual(true);
+
+    store.dispatch(authorizeSuperset(false));
+    await supersetFetch('1337');
+    expect(isAuthorized(store.getState())).toEqual(true);
   });
 });
