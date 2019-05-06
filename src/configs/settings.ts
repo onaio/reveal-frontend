0bcc163223d72fc6660d0730126ee1935ebf59f8
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-05-06 11:40:56 +0300

    Consolidate all configurations in `settings.ts`

---
+++ src/configs/settings.ts
@@ -0,0 +1,101 @@
+/** This is the main configuration module */
+import { Providers } from '@onaio/gatekeeper';
+import { DOMAIN_NAME, ONADATA_CLIENT_ID } from './env';
+
+/** Interfaces and Types */
+
+/** Interface for a Focus Investigation Classification */
+export interface Classification {
+  code: string;
+  name: string;
+  description: string;
+}
+
+/** Interface that describes location items */
+export interface LocationItem {
+  identifier: string /** Should match the name of the column in data */;
+  level: number /** The HDX-compliant level of the location in the hierarchy */;
+  name: string /** The name of the location */;
+}
+
+/** Authentication Configs */
+export const providers: Providers = {
+  Ona: {
+    accessTokenUri: 'https://stage-api.ona.io/o/token/',
+    authorizationUri: 'https://stage-api.ona.io/o/authorize/',
+    clientId: ONADATA_CLIENT_ID,
+    redirectUri: `${DOMAIN_NAME}/oauth/callback/Ona/`,
+    scopes: ['read', 'write'],
+    state: 'abc',
+    userUri: 'https://stage-api.ona.io/api/v1/user.json',
+  },
+};
+
+/** Location configs */
+
+/** Location item hierarchy
+ * This is a list of locations.  The "level" field will be used to sort the
+ * locations hierarchically, from lowest to highest.
+ */
+export const locationHierarchy: LocationItem[] = [
+  {
+    identifier: 'province',
+    level: 1,
+    name: 'Province',
+  },
+  {
+    identifier: 'district',
+    level: 2,
+    name: 'District',
+  },
+  {
+    identifier: 'canton',
+    level: 3,
+    name: 'Canton',
+  },
+  {
+    identifier: 'village',
+    level: 4,
+    name: 'Village',
+  },
+];
+
+/** Focus investigation configs */
+
+/** Focus Investigation case classifications */
+export const FIClassifications: Classification[] = [
+  {
+    code: 'A1',
+    description: 'Indigenous case recorded within the last year.',
+    name: 'Active',
+  },
+  {
+    code: 'A2',
+    description: 'No indigenous case during the last year, but withing the last 3 years.',
+    name: 'Residual Non-Active',
+  },
+  {
+    code: 'B1',
+    description: 'Receptive area but no indigenous cases within the last 3 years.',
+    name: 'Cleared Receptive',
+  },
+  {
+    code: 'B2',
+    description: 'Non-receptive area.',
+    name: 'Cleared Non-Receptive',
+  },
+];
+
+/** Indicators configs */
+
+// thresholds
+export const GREEN_THRESHOLD = 0.9;
+export type GREEN_THRESHOLD = typeof GREEN_THRESHOLD;
+export const YELLOW_THRESHOLD = 0.2;
+export type YELLOW_THRESHOLD = typeof YELLOW_THRESHOLD;
+export const ORANGE_THRESHOLD = 0.8;
+export type ORANGE_THRESHOLD = typeof ORANGE_THRESHOLD;
+
+// 1-3-7 thresholds
+export const ONE = 0;
+export const ZERO = 0;
--- src/configs/superset.ts
+++
@@ -1,5 +0,0 @@
-export const SUPERSET_API_BASE = 'https://discover.ona.io/';
-export type SUPERSET_API_BASE = typeof SUPERSET_API_BASE;
-
-export const SUPERSET_API_ENDPOINT = 'slice';
-export type SUPERSET_API_ENDPOINT = typeof SUPERSET_API_ENDPOINT;
