a86db3545d29b7284cd644c845cf4e968df76502
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-05-10 13:04:28 +0300

    Add tests for oAuthUserInfoGetter
    Credits on mocking: https://stackoverflow.com/a/53209819

--- src/helpers/tests/utils.test.tsx
+++ src/helpers/tests/utils.test.tsx
@@ -1,4 +1,11 @@
-import { getLocationColumns } from '../utils';
+import * as gatekeeper from '@onaio/gatekeeper';
+import { ONADATA_OAUTH_STATE, OPENSRP_OAUTH_STATE } from '../../configs/env';
+import { getLocationColumns, oAuthUserInfoGetter } from '../utils';
+
+jest.mock('@onaio/gatekeeper', () => ({
+  getOnadataUserInfo: jest.fn(),
+  getOpenSRPUserInfo: jest.fn(),
+}));
 
 describe('helpers/utils', () => {
   beforeEach(() => {
@@ -50,5 +57,21 @@ describe('helpers/utils', () => {
         ],
       },
     ]);
+  });
+
+  it('oAuthUserInfoGetter works for OpenSRP', () => {
+    const mock = jest.spyOn(gatekeeper, 'getOpenSRPUserInfo');
+    const resp = { foo: 'bar', oAuth2Data: { state: OPENSRP_OAUTH_STATE } };
+    oAuthUserInfoGetter(resp);
+    expect(mock).toHaveBeenCalledWith(resp);
+    mock.mockRestore();
+  });
+
+  it('oAuthUserInfoGetter works for Ona', () => {
+    const mock = jest.spyOn(gatekeeper, 'getOnadataUserInfo');
+    const resp = { foo: 'bar', oAuth2Data: { state: ONADATA_OAUTH_STATE } };
+    oAuthUserInfoGetter(resp);
+    expect(mock).toHaveBeenCalledWith(resp);
+    mock.mockRestore();
   });
 });
