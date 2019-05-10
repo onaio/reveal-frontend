4e322e38124bf57c76874fb68913c094cb098571
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-05-10 21:55:39 +0300

    Connect Active FI component to redux store

--- src/containers/pages/FocusInvestigation/active/index.tsx
+++ src/containers/pages/FocusInvestigation/active/index.tsx
@@ -1,12 +1,16 @@
 // this is the FocusInvestigation "active" page component
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import DrillDownTable from '@onaio/drill-down-table';
+import reducerRegistry from '@onaio/redux-reducer-registry';
+import superset from '@onaio/superset-connector';
 import * as React from 'react';
+import { connect } from 'react-redux';
 import { RouteComponentProps } from 'react-router';
 import { Link } from 'react-router-dom';
 import { CellInfo, Column } from 'react-table';
 import 'react-table/react-table.css';
 import { Table } from 'reactstrap';
+import { Store } from 'redux';
 import DrillDownTableLinkedCell from '../../../../components/DrillDownTableLinkedCell';
 import OneThreeSevenAdherence from '../../../../components/formatting/OneThreeSevenAdherence';
 import { FIClassifications, locationHierarchy } from '../../../../configs/settings';
@@ -17,16 +21,57 @@ import {
   renderClassificationRow,
 } from '../../../../helpers/indicators';
 import '../../../../helpers/tables.css';
-import { getLocationColumns, RouteParams } from '../../../../helpers/utils';
+import { extractPlan, getLocationColumns, RouteParams } from '../../../../helpers/utils';
+import supersetFetch from '../../../../services/superset';
+import plansReducer, {
+  fetchPlans,
+  getPlansArray,
+  Plan,
+  reducerName as plansReducerName,
+} from '../../../../store/ducks/plans';
+import { plan1, plans } from '../../../../store/ducks/tests/fixtures';
 import { data } from './tests/fixtures';
 
+// /** callback for fetching from Superset */
+// export const fetchPlanback = (parsedResponse: Array<{ [key: string]: any }>) => {
+//   const sliceData = superset.processData(parsedResponse);
+//   return sliceData.map((element));
+// };
+
+/** register the plans reducer */
+reducerRegistry.register(plansReducerName, plansReducer);
+
+/** interface to describe props for ActiveFI component */
+export interface ActiveFIProps {
+  fetchPlansActionCreator: typeof fetchPlans;
+  plansArray: Plan[];
+}
+
+/** default props for ActiveFI component */
+export const defaultActiveFIProps: ActiveFIProps = {
+  fetchPlansActionCreator: fetchPlans,
+  plansArray: [plan1],
+};
+
 /** Reporting for Active Focus Investigations */
-class ActiveFocusInvestigation extends React.Component<RouteComponentProps<RouteParams>, {}> {
-  constructor(props: RouteComponentProps<RouteParams>) {
+class ActiveFocusInvestigation extends React.Component<
+  ActiveFIProps & RouteComponentProps<RouteParams>,
+  {}
+> {
+  public static defaultProps = defaultActiveFIProps;
+  constructor(props: ActiveFIProps & RouteComponentProps<RouteParams>) {
     super(props);
   }
 
+  // componentDidMount() {
+  //   const { fetchPlansActionCreator } = this.props;
+  //   supersetFetch('223').then((result: Plan[]) => fetchPlansActionCreator(result));
+  // }
+
   public render() {
+    // const { plansArray } = this.props;
+    const plansArray = plans;
+    const thePlans = plansArray.map((item: Plan) => extractPlan(item));
     const locationColumns: Column[] = getLocationColumns(locationHierarchy, true);
     const otherColumns: Column[] = [
       {
@@ -36,11 +81,11 @@ class ActiveFocusInvestigation extends React.Component<RouteComponentProps<Route
             Cell: (cell: CellInfo) => {
               return (
                 <div>
-                  <Link to={`${FI_SINGLE_MAP_URL}/13`}>
+                  {/* <Link to={`${FI_SINGLE_MAP_URL}/13`}>
                     <FontAwesomeIcon icon={['fas', 'map']} />
                   </Link>
-                  &nbsp;&nbsp;
-                  <Link to={`${FI_SINGLE_URL}/13`}>{cell.value}</Link>
+                  &nbsp;&nbsp; */}
+                  <Link to={`${FI_SINGLE_URL}/${cell.original.id}`}>{cell.value}</Link>
                 </div>
               );
             },
@@ -50,20 +95,30 @@ class ActiveFocusInvestigation extends React.Component<RouteComponentProps<Route
           },
         ],
       },
+      // {
+      //   Header: 'Case Notif. Date',
+      //   columns: [
+      //     {
+      //       Cell: (cell: CellInfo) => {
+      //         return (
+      //           <div>
+      //             <a href="#date">{cell.value}</a>
+      //           </div>
+      //         );
+      //       },
+      //       Header: '',
+      //       accessor: 'caseNotificationDate',
+      //       minWidth: 120,
+      //     },
+      //   ],
+      // },
       {
-        Header: 'Case Notif. Date',
+        Header: 'Reason',
         columns: [
           {
-            Cell: (cell: CellInfo) => {
-              return (
-                <div>
-                  <a href="#date">{cell.value}</a>
-                </div>
-              );
-            },
             Header: '',
-            accessor: 'caseNotificationDate',
-            minWidth: 120,
+            accessor: 'reason',
+            minWidth: 100,
           },
         ],
       },
@@ -77,45 +132,45 @@ class ActiveFocusInvestigation extends React.Component<RouteComponentProps<Route
           },
         ],
       },
-      {
-        Header: 'Case Class.',
-        columns: [
-          {
-            Header: '',
-            accessor: 'caseClassification',
-          },
-        ],
-      },
-      {
-        Header: '1-3-7 adherence',
-        columns: [
-          {
-            Cell: (cell: CellInfo) => get137Value(cell.value),
-            Header: '1',
-            accessor: 'adherence1',
-            maxWidth: 40,
-          },
-          {
-            Cell: (cell: CellInfo) => get137Value(cell.value),
-            Header: '3',
-            accessor: 'adherence3',
-            maxWidth: 40,
-          },
-          {
-            Cell: (cell: CellInfo) => get137AdherenceIndicator(cell),
-            Header: '7',
-            accessor: 'adherence7',
-            maxWidth: 105,
-          },
-        ],
-      },
+      // {
+      //   Header: 'Case Class.',
+      //   columns: [
+      //     {
+      //       Header: '',
+      //       accessor: 'caseClassification',
+      //     },
+      //   ],
+      // },
+      // {
+      //   Header: '1-3-7 adherence',
+      //   columns: [
+      //     {
+      //       Cell: (cell: CellInfo) => get137Value(cell.value),
+      //       Header: '1',
+      //       accessor: 'adherence1',
+      //       maxWidth: 40,
+      //     },
+      //     {
+      //       Cell: (cell: CellInfo) => get137Value(cell.value),
+      //       Header: '3',
+      //       accessor: 'adherence3',
+      //       maxWidth: 40,
+      //     },
+      //     {
+      //       Cell: (cell: CellInfo) => get137AdherenceIndicator(cell),
+      //       Header: '7',
+      //       accessor: 'adherence7',
+      //       maxWidth: 105,
+      //     },
+      //   ],
+      // },
     ];
     const allColumns: Column[] = locationColumns.concat(otherColumns);
 
     const tableProps = {
       CellComponent: DrillDownTableLinkedCell,
       columns: allColumns,
-      data,
+      data: thePlans,
       identifierField: 'id',
       linkerField: 'id',
       minRows: 0,
@@ -134,10 +189,37 @@ class ActiveFocusInvestigation extends React.Component<RouteComponentProps<Route
         <Table className="definitions">
           <tbody>{FIClassifications.map(el => renderClassificationRow(el))}</tbody>
         </Table>
-        <OneThreeSevenAdherence />
+        {/* <OneThreeSevenAdherence /> */}
       </div>
     );
   }
 }
 
-export default ActiveFocusInvestigation;
+export { ActiveFocusInvestigation };
+
+/** Connect the component to the store */
+
+/** interface to describe props from mapStateToProps */
+interface DispatchedStateProps {
+  plansArray: Plan[];
+}
+
+/** map state to props */
+const mapStateToProps = (state: Partial<Store>): DispatchedStateProps => {
+  const result = {
+    plansArray: getPlansArray(state),
+  };
+  return result;
+};
+
+const mapDispatchToProps = { fetchPlansActionCreator: fetchPlans, getPlans: getPlansArray };
+
+/** create connected component */
+
+/** Connected ActiveFI component */
+const ConnectedActiveFocusInvestigation = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ActiveFocusInvestigation);
+
+export default ConnectedActiveFocusInvestigation;
