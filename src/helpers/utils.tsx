7eb9b2eaf581c393fbf0b01213573d85512f54f4
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-05-10 13:03:41 +0300

    Add oAuthUserInfoGetter helper function

--- src/helpers/utils.tsx
+++ src/helpers/utils.tsx
@@ -1,5 +1,8 @@
+import { getOnadataUserInfo, getOpenSRPUserInfo } from '@onaio/gatekeeper';
+import { SessionState } from '@onaio/session-reducer';
 import { uniq } from 'lodash';
 import { Column } from 'react-table';
+import { ONADATA_OAUTH_STATE, OPENSRP_OAUTH_STATE } from '../configs/env';
 import { locationHierarchy, LocationItem } from '../configs/settings';
 
 /** Interface for an object that is allowed to have any property */
@@ -45,4 +48,21 @@ export function getLocationColumns(
       accessor: el.identifier,
     };
   });
+}
+
+/** Custom function to get oAuth user info depending on the oAuth2 provider
+ * It compares the value of the `state` param in the oAuth2 provider config
+ * to the one received from the oAuth2 provider in order to return the correct
+ * user info getter function
+ * @param {{[key: string]: any }} apiResponse - the API response object
+ */
+export function oAuthUserInfoGetter(apiResponse: { [key: string]: any }): SessionState | void {
+  if (Object.keys(apiResponse).includes('oAuth2Data')) {
+    switch (apiResponse.oAuth2Data.state) {
+      case OPENSRP_OAUTH_STATE:
+        return getOpenSRPUserInfo(apiResponse);
+      case ONADATA_OAUTH_STATE:
+        return getOnadataUserInfo(apiResponse);
+    }
+  }
 }
