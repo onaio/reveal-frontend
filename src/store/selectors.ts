e5f4a6a067d97d3d8393f5ba983e0c7630f315e0
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-05-16 13:24:20 +0300

    Add getOauthProviderState selector

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -19,3 +19,14 @@ export function getAccessToken(state: Partial<Store>): string | null {
   }
   return null;
 }
+
+/** get the oAuth2 provider state parameter from the Redux store
+ * @param {Partial<Store>} state - the redux store
+ */
+export function getOauthProviderState(state: Partial<Store>): string | null {
+  const extraData = (state as any)[sessionReducerName].extraData;
+  if (extraData.oAuth2Data && extraData.oAuth2Data.state) {
+    return extraData.oAuth2Data.state;
+  }
+  return null;
+}
