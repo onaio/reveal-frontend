6eecd9cae31d2612a5927c3d0af36f8f427e1387
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-05-16 13:27:34 +0300

    Add test for getOauthProviderState

--- src/store/tests/selectors.test.ts
+++ src/store/tests/selectors.test.ts
@@ -22,7 +22,7 @@ describe('store/selectors', () => {
           name: 'Bobbie',
           username: 'RobertBaratheon',
         },
-        { api_token: 'hunter2', oAuth2Data: { access_token: 'iLoveOov' } }
+        { api_token: 'hunter2', oAuth2Data: { access_token: 'iLoveOov', state: 'abcde' } }
       )
     );
     expect(getAccessToken(store.getState())).toEqual('iLoveOov');
@@ -38,9 +38,25 @@ describe('store/selectors', () => {
           name: 'Bobbie',
           username: 'RobertBaratheon',
         },
-        { api_token: 'hunter2', oAuth2Data: { access_token: 'iLoveOov' } }
+        { api_token: 'hunter2', oAuth2Data: { access_token: 'iLoveOov', state: 'abcde' } }
       )
     );
     expect(getApiToken(store.getState())).toEqual('hunter2');
+  });
+
+  it('should be able to get the oAuth2 state parameter', () => {
+    expect(getOauthProviderState(store.getState())).toEqual(null);
+    store.dispatch(
+      authenticateUser(
+        true,
+        {
+          email: 'bob@example.com',
+          name: 'Bobbie',
+          username: 'RobertBaratheon',
+        },
+        { api_token: 'hunter2', oAuth2Data: { access_token: 'iLoveOov', state: 'abcde' } }
+      )
+    );
+    expect(getOauthProviderState(store.getState())).toEqual('abcde');
   });
 });
