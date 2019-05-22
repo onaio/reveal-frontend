e07befd7db4c6f56fad8febc363ea4a41e7907ed
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-05-22 14:40:30 +0300

    Add more selector tests for Tasks reducer
    - getTasksByPlanId
    - getTasksByGoalId
    - getTasksByJurisdictionId
    - getTasksByStructureId

--- src/store/ducks/tests/tasks.test.ts
+++ src/store/ducks/tests/tasks.test.ts
@@ -6,7 +6,11 @@ import reducer, {
   fetchTasks,
   getTaskById,
   getTasksArray,
+  getTasksByGoalId,
   getTasksById,
+  getTasksByJurisdictionId,
+  getTasksByPlanId,
+  getTasksByStructureId,
   getTasksIdArray,
   reducerName,
   Task,
@@ -28,6 +32,10 @@ describe('reducers/tasks', () => {
     expect(getTasksIdArray(store.getState())).toEqual([]);
     expect(getTasksArray(store.getState())).toEqual([]);
     expect(getTaskById(store.getState(), 'someId')).toEqual(null);
+    expect(getTasksByPlanId(store.getState(), 'someId')).toEqual([]);
+    expect(getTasksByGoalId(store.getState(), 'someId')).toEqual([]);
+    expect(getTasksByJurisdictionId(store.getState(), 'someId')).toEqual([]);
+    expect(getTasksByStructureId(store.getState(), 'someId')).toEqual([]);
   });
 
   it('should fetch tasks', () => {
@@ -38,6 +46,18 @@ describe('reducers/tasks', () => {
     expect(getTasksArray(store.getState())).toEqual(values(expected));
     expect(getTaskById(store.getState(), '01d0b84c-df06-426c-a272-6858e84fea31')).toEqual(
       fixtures.task4
+    );
+    expect(getTasksByPlanId(store.getState(), '10f9e9fa-ce34-4b27-a961-72fab5206ab6')).toEqual([
+      fixtures.task1,
+      fixtures.task2,
+      fixtures.task4,
+    ]);
+    expect(getTasksByGoalId(store.getState(), 'RACD_bednet_dist_1km_radius')).toEqual([
+      fixtures.task2,
+    ]);
+    expect(getTasksByJurisdictionId(store.getState(), '3952')).toEqual([fixtures.task3]);
+    expect(getTasksByStructureId(store.getState(), 'a19eeb63-45d0-4744-9a9d-76d0694103f6')).toEqual(
+      [fixtures.task1]
     );
   });
 });
