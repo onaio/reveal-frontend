e7f92d6693e1c247b8ae0cb3b7524b82bdc6f582
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-05-23 12:38:29 +0300

    Add tests for jurisdictions reducer module

--- src/store/ducks/tests/jurisdiction.test.ts
+++ src/store/ducks/tests/jurisdiction.test.ts
@@ -1,15 +1,21 @@
 import reducerRegistry from '@onaio/redux-reducer-registry';
-
 import { keyBy, values } from 'lodash';
-
 import { FlushThunks } from 'redux-testkit';
-
 import store from '../../index';
-import reducer, { fetchGeoJSON, GeoJSON, getGeoJSONs, reducerName } from '../jurisdictions';
-reducerRegistry.register(reducerName, reducer);
+import reducer, {
+  fetchJurisdictions,
+  getJurisdictionById,
+  getJurisdictionsArray,
+  getJurisdictionsById,
+  getJurisdictionsIdArray,
+  Jurisdiction,
+  reducerName,
+} from '../jurisdictions';
 import * as fixtures from './fixtures';
 
-describe('reducers/superset', () => {
+reducerRegistry.register(reducerName, reducer);
+
+describe('reducers/jurisdictions', () => {
   let flushThunks;
 
   beforeEach(() => {
@@ -18,13 +24,23 @@ describe('reducers/superset', () => {
   });
 
   it('should have initial state', () => {
-    expect(getGeoJSONs(store.getState(), '10f9e9fa-ce34-4b27-a961-72fab5206ab6')).toEqual(null);
+    expect(getJurisdictionsById(store.getState())).toEqual({});
+    expect(getJurisdictionsIdArray(store.getState())).toEqual([]);
+    expect(getJurisdictionsArray(store.getState())).toEqual([]);
+    expect(getJurisdictionById(store.getState(), 'someId')).toEqual(null);
   });
 
-  it('should fetch geojsons', () => {
-    store.dispatch(fetchGeoJSON(fixtures.geojson));
-    expect(getGeoJSONs(store.getState(), '10f9e9fa-ce34-4b27-a961-72fab5206ab6')).toEqual(
-      fixtures.singleGeoJSON
+  it('should fetch jurisdictions', () => {
+    store.dispatch(fetchJurisdictions(fixtures.jurisdictions));
+    const expected = keyBy(
+      fixtures.jurisdictions,
+      (jurisdiction: Jurisdiction) => jurisdiction.jurisdiction_id
+    );
+    expect(getJurisdictionsById(store.getState())).toEqual(expected);
+    expect(getJurisdictionsIdArray(store.getState())).toEqual(fixtures.jurisdictionsIdsArray);
+    expect(getJurisdictionsArray(store.getState())).toEqual(values(expected));
+    expect(getJurisdictionById(store.getState(), '450fc15b-5bd2-468a-927a-49cb10d3bcac')).toEqual(
+      fixtures.jurisdictions[0]
     );
   });
 });
