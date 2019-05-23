b86d77384a8531a51c0ba5c55da8f4dcc817e127
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-05-23 18:16:28 +0300

    Add sidebar to map

--- src/containers/pages/FocusInvestigation/map/active/index.tsx
+++ src/containers/pages/FocusInvestigation/map/active/index.tsx
@@ -1,44 +1,65 @@
 import reducerRegistry from '@onaio/redux-reducer-registry';
-import { Actions, prepareLayer } from 'gisida';
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { RouteComponentProps } from 'react-router';
-import { AnyAction } from 'redux';
 import { Store } from 'redux';
 import GisidaWrapper from '../../../../../components/GisidaWrapper';
-import { SUPERSET_JURISDICTIONS_SLICE } from '../../../../../configs/env';
+import Loading from '../../../../../components/page/Loading';
+import {
+  SUPERSET_GOALS_SLICE,
+  SUPERSET_JURISDICTIONS_SLICE,
+  SUPERSET_PLANS_SLICE,
+} from '../../../../../configs/env';
+import { FOCUS_INVESTIGATION } from '../../../../../constants';
 import { FlexObject, RouteParams } from '../../../../../helpers/utils';
 import supersetFetch from '../../../../../services/superset';
-import geojsonReducer, {
+import goalsReducer, {
+  fetchGoals,
+  getGoalsArrayByPlanId,
+  Goal,
+  reducerName as goalsReducerName,
+} from '../../../../../store/ducks/goals';
+import jurisdictionReducer, {
   fetchJurisdictions,
   getJurisdictionById,
   Jurisdiction,
   reducerName as jurisdictionReducerName,
 } from '../../../../../store/ducks/jurisdictions';
 import plansReducer, {
+  fetchPlans,
   getPlanById,
+  getPlansArray,
+  getPlansIdArray,
   Plan,
   reducerName as plansReducerName,
 } from '../../../../../store/ducks/plans';
-import { jurisdictions, plan1 } from '../../../../../store/ducks/tests/fixtures';
-reducerRegistry.register(jurisdictionReducerName, geojsonReducer);
+import * as fixtures from '../../../../../store/ducks/tests/fixtures';
+import './style.css';
+
+/** register reducers */
+reducerRegistry.register(jurisdictionReducerName, jurisdictionReducer);
+reducerRegistry.register(goalsReducerName, goalsReducer);
 reducerRegistry.register(plansReducerName, plansReducer);
-// import store from '../../../../../store';
 
 /** interface to describe props for ActiveFI component */
 export interface MapSingleFIProps {
+  fetchGoalsActionCreator: typeof fetchGoals;
   fetchJurisdictionsActionCreator: typeof fetchJurisdictions;
-  geoJSONData: Jurisdiction | null;
+  fetchPlansActionCreator: typeof fetchPlans;
+  goals: Goal[] | null;
+  jurisdiction: Jurisdiction | null;
   plan: Plan | null;
   supersetService: typeof supersetFetch;
 }
 
 /** default props for ActiveFI component */
 export const defaultMapSingleFIProps: MapSingleFIProps = {
+  fetchGoalsActionCreator: fetchGoals,
   fetchJurisdictionsActionCreator: fetchJurisdictions,
-  geoJSONData: jurisdictions[0],
-  plan: plan1,
-  supersetService: supersetFetch,
+  fetchPlansActionCreator: fetchPlans,
+  goals: fixtures.plan1Goals,
+  jurisdiction: fixtures.jurisdictions[0],
+  plan: fixtures.plan1,
 };
 /** Map View for Single Active Focus Investigation */
 class SingleActiveFIMap extends React.Component<
@@ -51,46 +72,62 @@ class SingleActiveFIMap extends React.Component<
   }
 
   public async componentDidMount() {
-    const { fetchJurisdictionsActionCreator, supersetService } = this.props;
-    await supersetService(SUPERSET_JURISDICTIONS_SLICE).then((result: Jurisdiction[]) =>
+    const {
+      fetchGoalsActionCreator,
+      fetchJurisdictionsActionCreator,
+      fetchPlansActionCreator,
+    } = this.props;
+    await supersetFetch(SUPERSET_JURISDICTIONS_SLICE).then((result: Jurisdiction[]) =>
       fetchJurisdictionsActionCreator(result)
+    );
+    await supersetFetch(SUPERSET_PLANS_SLICE).then((result2: Plan[]) =>
+      fetchPlansActionCreator(result2)
+    );
+    await supersetFetch(SUPERSET_GOALS_SLICE).then((result3: Goal[]) =>
+      fetchGoalsActionCreator(result3)
     );
   }
   public render() {
-    // const currentState = store.getState();
-    // id represents planid
-    const { geoJSONData, plan } = this.props;
+    const { jurisdiction, plan, goals } = this.props;
+
+    if (!goals || !jurisdiction || !plan) {
+      return <Loading />;
+    }
 
     return (
       <div>
-        <h2 className="page-title mt-4 mb-5">
-          Map View: {plan && plan.jurisdiction_name ? plan.jurisdiction_name : null}
+        <h2 className="page-title mt-4 mb-4">
+          {FOCUS_INVESTIGATION}: {plan && plan.jurisdiction_name ? plan.jurisdiction_name : null}
         </h2>
-        <div className="map">
-          <GisidaWrapper
-            handlers={this.buildHandlers()}
-            geoData={geoJSONData}
-            // Location= {this.props.result.geoJSONData}
-            // onInit={() => {console.log('map init')}}
-          />
+        <div className="row no-gutters">
+          <div className="col-9">
+            <div className="map">
+              <GisidaWrapper handlers={this.buildHandlers()} geoData={jurisdiction} />
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="mapSidebar">
+              <h5>Responses</h5>
+              <hr />
+              {goals &&
+                goals.map((item: Goal) => {
+                  return (
+                    <div className="responseItem" key={item.goal_id}>
+                      <h6>{item.action_code}</h6>
+                      <div className="targetItem">
+                        <p>Measure: {item.measure}</p>
+                        <p>
+                          Target: {item.task_count} of {item.goal_value}
+                        </p>
+                      </div>
+                    </div>
+                  );
+                })}
+            </div>
+          </div>
         </div>
       </div>
     );
-  }
-  // not sure where this should be called
-  private loadLayerFunc(
-    resObj: FlexObject,
-    layerId: number,
-    mapId: string,
-    dispatch: (action: AnyAction) => void
-  ): void {
-    const layerObj = { ...resObj };
-    layerObj.id = layerId;
-    layerObj.loaded = false;
-    dispatch(Actions.addLayer(mapId, layerObj));
-    if (layerObj.visible && !layerObj.loaded) {
-      prepareLayer(mapId, layerObj, dispatch);
-    }
   }
 
   private buildHandlers() {
@@ -98,9 +135,6 @@ class SingleActiveFIMap extends React.Component<
     const handlers = [
       {
         method: function drillDownClick(e: any) {
-          // if (e.originalEvent.shiftKey) {
-          //   return false;
-          // }
           const features = e.target.queryRenderedFeatures(e.point);
           if (features[0] && Number(features[0].id) !== Number(self.props.match.params.id)) {
             self.props.history.push(`/focus-investigation/map/${features[0].id}`);
@@ -109,15 +143,6 @@ class SingleActiveFIMap extends React.Component<
         name: 'drillDownClick',
         type: 'click',
       },
-      // {
-      //   method: function selectionClick(e: any) {
-      //     if (!e.originalEvent.shiftKey) {
-      //       return false;
-      //     }
-      //   },
-      //   name: 'selectionClick',
-      //   type: 'click',
-      // },
     ];
     return handlers;
   }
@@ -126,18 +151,25 @@ class SingleActiveFIMap extends React.Component<
 const mapStateToProps = (state: Partial<Store>, ownProps: any) => {
   // pass in the plan id to get plan the get the jurisdicytion_id from the plan
   const plan = getPlanById(state, ownProps.match.params.id);
-  let geoJSONData = null;
+  let goals = null;
+  let jurisdiction = null;
   if (plan) {
-    geoJSONData = getJurisdictionById(state, plan.jurisdiction_id);
+    jurisdiction = getJurisdictionById(state, plan.jurisdiction_id);
+    goals = getGoalsArrayByPlanId(state, plan.plan_id);
   }
   return {
-    geoJSONData,
+    goals,
+    jurisdiction,
     plan,
+    plansArray: getPlansArray(state),
+    plansIdArray: getPlansIdArray(state),
   };
 };
 
 const mapDispatchToProps = {
+  fetchGoalsActionCreator: fetchGoals,
   fetchJurisdictionsActionCreator: fetchJurisdictions,
+  fetchPlansActionCreator: fetchPlans,
 };
 const ConnectedMapSingleFI = connect(
   mapStateToProps,
