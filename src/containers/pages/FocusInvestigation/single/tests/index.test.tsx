1aebb77286b174ebdee22fac9c49045b0d15461c
p-netm <pmuriuki98@gmail.com>
2019-05-24 18:30:21 +0300

    Test singleFi works with store

--- src/containers/pages/FocusInvestigation/single/tests/index.test.tsx
+++ src/containers/pages/FocusInvestigation/single/tests/index.test.tsx
@@ -2,8 +2,12 @@ import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import { createBrowserHistory } from 'history';
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router';
 import { FI_SINGLE_URL } from '../../../../../constants';
+import store from '../../../../../store';
+import { fetchGoals } from '../../../../../store/ducks/goals';
+import { fetchPlans } from '../../../../../store/ducks/plans';
 import * as fixtures from '../../../../../store/ducks/tests/fixtures';
 import { SingleFI } from '../../single';
 
@@ -57,6 +61,31 @@ describe('containers/pages/SingleFI', () => {
       <Router history={history}>
         <SingleFI {...props} />
       </Router>
+    );
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.unmount();
+  });
+
+  it('It works with the Redux store', () => {
+    const mock: any = jest.fn();
+    store.dispatch(fetchPlans(fixtures.plans));
+    store.dispatch(fetchGoals(fixtures.goals));
+    const props = {
+      history,
+      location: mock,
+      match: {
+        isExact: true,
+        params: { id: fixtures.plan1.plan_id },
+        path: `${FI_SINGLE_URL}/:id`,
+        url: `${FI_SINGLE_URL}/16`,
+      },
+    };
+    const wrapper = mount(
+      <Provider store={store}>
+        <Router history={history}>
+          <SingleFI {...props} />
+        </Router>
+      </Provider>
     );
     expect(toJson(wrapper)).toMatchSnapshot();
     wrapper.unmount();
