7c36991992d2f4953ce3646cd1d13192f4dc5aa0
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-05-28 15:59:23 +0300

    Add tests for goal selectors
        - getGoalsByPlanId
        - getGoalsByGoalId
        - getGoalsByJurisdictionId

--- src/store/ducks/tests/goals.test.ts
+++ src/store/ducks/tests/goals.test.ts
@@ -2,7 +2,16 @@ import reducerRegistry from '@onaio/redux-reducer-registry';
 import { keyBy, keys, values } from 'lodash';
 import { FlushThunks } from 'redux-testkit';
 import store from '../../index';
-import reducer, { fetchGoals, getGoalById, getGoalsById, Goal, reducerName } from '../goals';
+import reducer, {
+  fetchGoals,
+  getGoalById,
+  getGoalsByGoalId,
+  getGoalsById,
+  getGoalsByJurisdictionId,
+  getGoalsByPlanId,
+  Goal,
+  reducerName,
+} from '../goals';
 import * as fixtures from './fixtures';
 
 reducerRegistry.register(reducerName, reducer);
@@ -18,6 +27,9 @@ describe('reducers/goals', () => {
   it('should have initial state', () => {
     expect(getGoalsById(store.getState())).toEqual({});
     expect(getGoalById(store.getState(), 'someId')).toEqual(null);
+    expect(getGoalsByPlanId(store.getState(), 'someId')).toEqual([]);
+    expect(getGoalsByGoalId(store.getState(), 'someId')).toEqual([]);
+    expect(getGoalsByJurisdictionId(store.getState(), 'someId')).toEqual([]);
   });
 
   it('should fetch goals', () => {
@@ -27,5 +39,13 @@ describe('reducers/goals', () => {
     expect(getGoalById(store.getState(), '5a27ec10-7a5f-563c-ba11-4de150b336af')).toEqual(
       fixtures.goal1
     );
+    expect(getGoalsByPlanId(store.getState(), '10f9e9fa-ce34-4b27-a961-72fab5206ab6')).toEqual([
+      fixtures.goal1,
+      fixtures.goal2,
+    ]);
+    expect(getGoalsByGoalId(store.getState(), 'Case_Confirmation')).toEqual([fixtures.goal1]);
+    expect(
+      getGoalsByJurisdictionId(store.getState(), '450fc15b-5bd2-468a-927a-49cb10d3bcac')
+    ).toEqual([fixtures.goal1, fixtures.goal2]);
   });
 });
