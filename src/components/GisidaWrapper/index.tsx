1093b5e3ecb1531feac699b7919ba1fc2683d26d
kahama <phares663@gmail.com>
2019-05-30 12:13:21 +0300

    wip tasks fixes

--- src/components/GisidaWrapper/index.tsx
+++ src/components/GisidaWrapper/index.tsx
@@ -208,6 +208,8 @@ class GisidaWrapper extends React.Component<FlexObject, GisidaState> {
             symbolLayers.push(fillLayer);
           });
         }
+      } else {
+        alert('no tasks to show');
       }
     }
     const { geoData } = this.props;
@@ -272,7 +274,6 @@ class GisidaWrapper extends React.Component<FlexObject, GisidaState> {
       GISIDA_ONADATA_API_TOKEN,
       LayerStore
     );
-
     this.setState({ doRenderMap: true }, () => {
       // 4. Initialize Gisida stores
       store.dispatch(Actions.initApp(config.APP));
