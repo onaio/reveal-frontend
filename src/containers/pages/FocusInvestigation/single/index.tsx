d2f002136b837b9a2bd953c966ad7988d401369a
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-06-12 16:49:14 +0300

    Add map area

--- src/containers/pages/FocusInvestigation/single/index.tsx
+++ src/containers/pages/FocusInvestigation/single/index.tsx
@@ -112,7 +112,7 @@ class SingleFI extends React.Component<RouteComponentProps<RouteParams> & Single
             <h4 className="mb-4">{FOCUS_AREA_INFO}</h4>
             <div style={{ background: 'lightgrey', height: '200px' }} />
             <div className="map-area">
-              <GisidaWrapper geoData={jurisdiction} />
+              <GisidaWrapper geoData={jurisdiction} minHeight="200px" />
             </div>
             <dl className="row mt-3">
               <dt className="col-5">{DISTRICT}</dt>
