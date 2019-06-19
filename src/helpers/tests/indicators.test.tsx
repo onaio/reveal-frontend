f76216eb4b1e7382711b9ff1c74ed29d9eef45ab
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-06-19 18:06:55 +0300

    Add test for reducing goal

--- src/helpers/tests/indicators.test.tsx
+++ src/helpers/tests/indicators.test.tsx
@@ -12,4 +12,22 @@ describe('helpers/indicators', () => {
         fixtures.goal5.goal_value
     );
   });
+
+  it('goalPercentAchieved works for reducing goals', () => {
+    const goal = {
+      action_code: 'Blood Screening',
+      completed_task_count: 100,
+      goal_comparator: '<',
+      goal_id: 'RACD_blood_screening_1km_radius',
+      goal_unit: 'pints',
+      goal_value: 30,
+      id: '19b86421-3cb2-5698-9f11-c1bdafbe5e6d',
+      jurisdiction_id: '1337',
+      measure: 'Percent of registered people tested',
+      plan_id: '1337',
+      task_business_status_map: {},
+      task_count: 100,
+    };
+    expect(goalPercentAchieved(goal)).toEqual(0);
+  });
 });
