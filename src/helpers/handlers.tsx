8e7c68ad42e6afa6ffa2b9b2876c636f13e27793
kahama <phares663@gmail.com>
2019-06-20 15:11:13 +0300

    show popups on fill layer type

--- src/helpers/handlers.tsx
+++ src/helpers/handlers.tsx
@@ -8,7 +8,7 @@ declare global {
   const mapboxgl: typeof mapboxgl;
 }
 
-/** something */
+/** Having features as any type is not most desirable this has been qued up as part of technical debt payment */
 export function popupHandler(event: EventData) {
   const features = event.target.queryRenderedFeatures(event.point) as any;
 
@@ -19,14 +19,16 @@ export function popupHandler(event: EventData) {
     features[0].geometry.coordinates &&
     features[0].properties &&
     features[0].properties.action_code &&
-    features[0].layer.type !== 'fill'
+    features[0].layer.type !== 'line'
   ) {
-    const coordinates = features[0].geometry.coordinates.slice();
+    const coordinates =
+      features[0].layer.type === 'fill' ? event.lngLat : features[0].geometry.coordinates.slice();
     const description = features[0].properties.action_code;
     /** Ensure that if the map is zoomed out such that multiple
      * copies of the feature are visible,
      * the popup appears over the copy being pointed to
      */
+
     while (Math.abs(event.lngLat.lng - coordinates[0]) > 180) {
       coordinates[0] += event.lngLat.lng > coordinates[0] ? 360 : -360;
     }
@@ -34,5 +36,6 @@ export function popupHandler(event: EventData) {
       .setLngLat(coordinates)
       .setHTML(description)
       .addTo(window.maps[0]);
+    // Change the cursor to a pointer when the mouse is over the states layer.
   }
 }
