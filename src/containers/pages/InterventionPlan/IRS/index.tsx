0870f36c6523b6efda19d8264086d3b3077a374f
cKellyDesign <ckelly@ona.io>
2019-06-21 09:25:01 -0700

    IRS Plan page as a type of Intervention Plan page

---
+++ src/containers/pages/InterventionPlan/IRS/index.tsx
@@ -0,0 +1,18 @@
+// this is the IRS page component
+import * as React from 'react';
+
+class IRS extends React.Component<{}, {}> {
+  constructor(props: {}) {
+    super(props);
+  }
+
+  public render() {
+    return (
+      <div>
+        <h1>IRS</h1>
+      </div>
+    );
+  }
+}
+
+export default IRS;
