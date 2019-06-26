99ed51d3fb31893e63139ac80ba5c614778fb535
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-06-26 14:25:08 +0300

    Mock more environment variables
    - GISIDA_ONADATA_API_TOKEN
    - GISIDA_MAPBOX_TOKEN

--- src/configs/__mocks__/env.ts
+++ src/configs/__mocks__/env.ts
@@ -7,5 +7,11 @@
 export const SUPERSET_PLANS_SLICE = 0;
 export type SUPERSET_PLANS_SLICE = typeof SUPERSET_PLANS_SLICE;
 
+export const GISIDA_MAPBOX_TOKEN = 'hunter2';
+export type GISIDA_MAPBOX_TOKEN = typeof GISIDA_MAPBOX_TOKEN;
+
+export const GISIDA_ONADATA_API_TOKEN = 'hunter2';
+export type GISIDA_ONADATA_API_TOKEN = typeof GISIDA_ONADATA_API_TOKEN;
+
 export const DIGITAL_GLOBE_CONNECT_ID = 'hunter2';
 export type DIGITAL_GLOBE_CONNECT_ID = typeof DIGITAL_GLOBE_CONNECT_ID;
