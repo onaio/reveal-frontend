76012a42e3b88ecbabee61bb8e588bad2911e526
kahama <phares663@gmail.com>
2019-07-01 17:07:54 +0300

    Show popup when a goal has been chosen

--- src/helpers/handlers/index.ts
+++ src/helpers/handlers/index.ts
@@ -1,6 +1,8 @@
 import { GisidaMap } from 'gisida';
 import { LngLat, Map } from 'mapbox-gl';
 import { MAP_ID } from '../../constants';
+import store from '../../store';
+import { getCurrentGoal } from '../../store/ducks/goals';
 import { EventData } from '../mapbox';
 import './handlers.css';
 
@@ -14,40 +16,46 @@ declare global {
 
 /** Having features as any type is not most desirable this has been qued up as part of technical debt payment */
 export function popupHandler(event: EventData) {
-  const features = event.target.queryRenderedFeatures(event.point) as any[];
-  let description: string = '';
-  features.forEach((feature: any) => {
-    if (
-      feature &&
-      feature.geometry &&
-      feature.geometry.coordinates &&
-      feature.properties &&
-      feature.properties.action_code &&
-      feature.layer.type !== 'line' &&
-      feature.layer.id
-    ) {
-      description += `<p class="heading"> ${feature.properties.action_code}</b></p> <p> ${
-        feature.properties.task_business_status
-      }</p><br/><br/>`;
-    }
-  });
-  if (description.length) {
-    description = '<div>' + description + '</div>';
-    const coordinates: LngLat = event.lngLat;
-    /** Ensure that if the map is zoomed out such that multiple
-     * copies of the feature are visible,
-     * the popup appears over the copy being pointed to
-     */
+  const currentState = store.getState();
+  const currentGoal = getCurrentGoal(currentState);
+  if (currentGoal) {
+    const features = event.target.queryRenderedFeatures(event.point) as any[];
+    let description: string = '';
+    features.forEach((feature: any) => {
+      if (
+        feature &&
+        feature.geometry &&
+        feature.geometry.coordinates &&
+        feature.properties &&
+        feature.properties.action_code &&
+        feature.layer.type !== 'line' &&
+        feature.layer.id
+      ) {
+        description += `<p class="heading"> ${feature.properties.action_code}</b></p> <p> ${
+          feature.properties.task_business_status
+        }</p><br/><br/>`;
+      }
+    });
+    if (description.length) {
+      description = '<div>' + description + '</div>';
+      const coordinates: LngLat = event.lngLat;
+      /** Ensure that if the map is zoomed out such that multiple
+       * copies of the feature are visible,
+       * the popup appears over the copy being pointed to
+       */
 
-    while (Math.abs(event.lngLat.lng - coordinates.lng) > 180) {
-      coordinates.lng += event.lngLat.lng > coordinates.lng ? 360 : -360;
+      while (Math.abs(event.lngLat.lng - coordinates.lng) > 180) {
+        coordinates.lng += event.lngLat.lng > coordinates.lng ? 360 : -360;
+      }
+      // to do map container_id should come from props incase we have many maps to show
+      const loadedMap =
+        window.maps[
+          window.maps.map((map: Map) => (map as GisidaMap)._container.id).indexOf(MAP_ID)
+        ];
+      new mapboxgl.Popup()
+        .setLngLat(coordinates)
+        .setHTML(description)
+        .addTo(loadedMap);
     }
-    // to do map container_id should come from props incase we have many maps to show
-    const loadedMap =
-      window.maps[window.maps.map((map: Map) => (map as GisidaMap)._container.id).indexOf(MAP_ID)];
-    new mapboxgl.Popup()
-      .setLngLat(coordinates)
-      .setHTML(description)
-      .addTo(loadedMap);
   }
 }
