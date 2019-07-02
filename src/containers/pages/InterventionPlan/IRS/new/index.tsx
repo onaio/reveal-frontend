51204fe3cc79e4f8ad16328ff99c4dd7650456d2
cKellyDesign <ckelly@ona.io>
2019-07-02 13:26:35 -0700

    Add breadcrumbs to IRS Plan pages

--- src/containers/pages/InterventionPlan/IRS/new/index.tsx
+++ src/containers/pages/InterventionPlan/IRS/new/index.tsx
@@ -1,14 +1,37 @@
 // this is the IRS NEW view page component
 import * as React from 'react';
 
+import { HOME, HOME_URL, INTERVENTION_IRS_URL } from '../../../../../constants';
+
+import HeaderBreadcrumbs, {
+  BreadCrumbProps,
+} from '../../../../../components/page/HeaderBreadcrumb/HeaderBreadcrumb';
+
 class IrsNewPlan extends React.Component<{}, {}> {
   constructor(props: {}) {
     super(props);
   }
 
   public render() {
+    const homePage = {
+      label: HOME,
+      url: HOME_URL,
+    };
+    const basePage = {
+      label: 'IRS',
+      url: INTERVENTION_IRS_URL,
+    };
+    const breadCrumbProps: BreadCrumbProps = {
+      currentPage: {
+        label: 'New Plan',
+        url: `${INTERVENTION_IRS_URL}/new`,
+      },
+      pages: [homePage, basePage],
+    };
+
     return (
-      <div>
+      <div className="mb-5">
+        <HeaderBreadcrumbs {...breadCrumbProps} />
         <h2 className="page-title">IRS: New Plan</h2>
       </div>
     );
