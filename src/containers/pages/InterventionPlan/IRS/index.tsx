a212a515e9d2543915219d8335358efd751fb642
cKellyDesign <ckelly@ona.io>
2019-07-03 10:15:35 -0700

    Connect IRS Plans container to router and store

--- src/containers/pages/InterventionPlan/IRS/index.tsx
+++ src/containers/pages/InterventionPlan/IRS/index.tsx
@@ -1,14 +1,30 @@
 // this is the IRS LIST view page component
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { RouteComponentProps } from 'react-router';
+import { Store } from 'redux';
 
 import { HOME, HOME_URL, INTERVENTION_IRS_URL } from '../../../../constants';
+
+import { RouteParams } from '../../../../helpers/utils';
+import { getPlansArray, getPlansIdArray, Plan } from '../../../../store/ducks/plans';
 
 import HeaderBreadcrumbs, {
   BreadCrumbProps,
 } from '../../../../components/page/HeaderBreadcrumb/HeaderBreadcrumb';
 
-class IrsPlans extends React.Component<{}, {}> {
-  constructor(props: {}) {
+export interface IrsPlansProps {
+  plansArray: Plan[];
+  plansIdArray: string[];
+}
+
+export const defaultIrsPlansProps: IrsPlansProps = {
+  plansArray: [],
+  plansIdArray: [],
+};
+
+class IrsPlans extends React.Component<RouteComponentProps<RouteParams> & IrsPlansProps, {}> {
+  constructor(props: RouteComponentProps<RouteParams> & IrsPlansProps) {
     super(props);
   }
 
@@ -34,4 +50,29 @@ class IrsPlans extends React.Component<{}, {}> {
   }
 }
 
-export default IrsPlans;
+export { IrsPlans };
+
+interface DispatchedStateProps {
+  plansArray: Plan[];
+  plansIdArray: string[];
+}
+
+const mapStateToProps = (state: Partial<Store>, ownProps: any): DispatchedStateProps => {
+  const props = {
+    plansArray: [],
+    plansIdArray: [],
+    ...ownProps,
+  };
+
+  if (false) {
+    // todo - get plans from store
+    props.plansArray = getPlansArray(state);
+    props.plansIdArray = getPlansIdArray(state);
+  }
+
+  return props;
+};
+
+const ConnectedIrsPlans = connect(mapStateToProps)(IrsPlans);
+
+export default ConnectedIrsPlans;
--- src/containers/pages/InterventionPlan/tests/IRS.test.tsx
+++ src/containers/pages/InterventionPlan/tests/IRS.test.tsx
@@ -2,8 +2,15 @@ import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import { createBrowserHistory } from 'history';
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router';
-import IRS from '../IRS';
+import { FI_SINGLE_URL } from '../../../../constants';
+import store from '../../../../store';
+import { Plan } from '../../../../store/ducks/plans';
+import * as fixtures from '../../../../store/ducks/tests/fixtures';
+import ConnectedIrsPlans, { IrsPlans } from '../IRS';
+
+jest.mock('../../../../configs/env');
 
 const history = createBrowserHistory();
 
@@ -15,16 +22,31 @@ describe('containers/pages/IRS', () => {
   it('renders without crashing', () => {
     shallow(
       <Router history={history}>
-        <IRS />
+        <IrsPlans />
       </Router>
     );
   });
 
-  it('renders IRS correctly', () => {
+  it('renders plans list correctly', () => {
+    const mock: any = jest.fn();
+    const props = {
+      history,
+      location: mock,
+      match: {
+        isExact: true,
+        path: FI_SINGLE_URL,
+        url: FI_SINGLE_URL,
+      },
+      plansArray: fixtures.plans as Plan[],
+      plansIdArray: fixtures.plansIdArray,
+    };
+
     const wrapper = mount(
-      <Router history={history}>
-        <IRS />
-      </Router>
+      <Provider store={store}>
+        <Router history={history}>
+          <ConnectedIrsPlans {...props} />
+        </Router>
+      </Provider>
     );
     expect(toJson(wrapper)).toMatchSnapshot();
     wrapper.unmount();
