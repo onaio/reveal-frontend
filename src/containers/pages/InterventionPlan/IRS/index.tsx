ea4debe2fdb2c0bc8642fe15cd6c15758c10c74c
cKellyDesign <ckelly@ona.io>
2019-07-08 09:35:18 -0700

    Fetch IRS plans when IRS page mounts

--- src/containers/pages/InterventionPlan/IRS/index.tsx
+++ src/containers/pages/InterventionPlan/IRS/index.tsx
@@ -4,28 +4,49 @@ import { connect } from 'react-redux';
 import { RouteComponentProps } from 'react-router';
 import { Store } from 'redux';
 
+import reducerRegistry from '@onaio/redux-reducer-registry';
+
+import { SUPERSET_PLANS_SLICE } from '../../../../configs/env';
 import { HOME, HOME_URL, INTERVENTION_IRS_URL } from '../../../../constants';
 
 import { RouteParams } from '../../../../helpers/utils';
-import { getPlansArray, getPlansIdArray, Plan } from '../../../../store/ducks/plans';
+import supersetFetch from '../../../../services/superset';
+import plansReducer, {
+  fetchPlans,
+  getPlansArray,
+  InterventionType,
+  Plan,
+  reducerName as plansReducerName,
+} from '../../../../store/ducks/plans';
 
 import HeaderBreadcrumbs, {
   BreadCrumbProps,
 } from '../../../../components/page/HeaderBreadcrumb/HeaderBreadcrumb';
 
+/** register the plans reducer */
+reducerRegistry.register(plansReducerName, plansReducer);
+
 export interface IrsPlansProps {
+  fetchPlansActionCreator: typeof fetchPlans;
   plansArray: Plan[];
-  plansIdArray: string[];
+  supersetService: typeof supersetFetch;
 }
 
 export const defaultIrsPlansProps: IrsPlansProps = {
+  fetchPlansActionCreator: fetchPlans,
   plansArray: [],
-  plansIdArray: [],
+  supersetService: supersetFetch,
 };
 
-class IrsPlans extends React.Component<RouteComponentProps<RouteParams> & IrsPlansProps, {}> {
+class IrsPlans extends React.Component<IrsPlansProps & RouteComponentProps<RouteParams>, {}> {
+  public static defaultProps: IrsPlansProps = defaultIrsPlansProps;
   constructor(props: RouteComponentProps<RouteParams> & IrsPlansProps) {
     super(props);
+  }
+
+  public componentDidMount() {
+    const { fetchPlansActionCreator, supersetService } = this.props;
+    supersetService(SUPERSET_PLANS_SLICE).then((result: Plan[]) => fetchPlansActionCreator(result));
   }
 
   public render() {
@@ -54,25 +75,25 @@ export { IrsPlans };
 
 interface DispatchedStateProps {
   plansArray: Plan[];
-  plansIdArray: string[];
 }
 
 const mapStateToProps = (state: Partial<Store>, ownProps: any): DispatchedStateProps => {
   const props = {
     plansArray: [],
-    plansIdArray: [],
     ...ownProps,
   };
 
-  if (false) {
-    // todo - get plans from store
-    props.plansArray = getPlansArray(state);
-    props.plansIdArray = getPlansIdArray(state);
+  if (state) {
+    props.plansArray = getPlansArray(state, InterventionType.IRS);
   }
-
   return props;
 };
 
-const ConnectedIrsPlans = connect(mapStateToProps)(IrsPlans);
+const mapDispatchToProps = { fetchPlansActionCreator: fetchPlans };
+
+const ConnectedIrsPlans = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(IrsPlans);
 
 export default ConnectedIrsPlans;
