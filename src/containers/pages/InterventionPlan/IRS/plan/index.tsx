aadb21c60e92ebdc63feb00bb7d773aba785dc6d
cKellyDesign <ckelly@ona.io>
2019-07-08 11:14:38 -0700

    Fetch IRS Plan by Id when Plan mounts

--- src/containers/pages/InterventionPlan/IRS/plan/index.tsx
+++ src/containers/pages/InterventionPlan/IRS/plan/index.tsx
@@ -1,37 +1,59 @@
 // this is the IRS Plan page component
+import reducerRegistry from '@onaio/redux-reducer-registry';
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { RouteComponentProps } from 'react-router';
 import { Store } from 'redux';
 
+import { SUPERSET_PLANS_SLICE } from '../../../../../configs/env';
 import { HOME, HOME_URL, INTERVENTION_IRS_URL } from '../../../../../constants';
 import { RouteParams } from '../../../../../helpers/utils';
 
-import { getPlanById, Plan } from '../../../../../store/ducks/plans';
+import supersetFetch from '../../../../../services/superset';
+
+import plansReducer, {
+  fetchPlans,
+  getPlanById,
+  Plan,
+  reducerName as plansReducerName,
+} from '../../../../../store/ducks/plans';
 
 import HeaderBreadcrumbs, {
   BreadCrumbProps,
 } from '../../../../../components/page/HeaderBreadcrumb/HeaderBreadcrumb';
 
+/** register the plans reducer */
+reducerRegistry.register(plansReducerName, plansReducer);
+
 export interface IrsPlanProps {
-  planId: string | null;
-  plan?: Plan | null;
-  isNewPlan?: boolean;
+  fetchPlansActionCreator: typeof fetchPlans;
   isDraftPlan?: boolean;
   isFinalizedPlan?: boolean;
+  isNewPlan?: boolean;
+  planById?: Plan | null;
+  planId: string | null;
+  supersetService: typeof supersetFetch;
 }
 
 export const defaultIrsPlanProps: IrsPlanProps = {
+  fetchPlansActionCreator: fetchPlans,
   isDraftPlan: false,
   isFinalizedPlan: false,
   isNewPlan: false,
-  plan: null,
+  planById: null,
   planId: null,
+  supersetService: supersetFetch,
 };
 
 class IrsPlan extends React.Component<RouteComponentProps<RouteParams> & IrsPlanProps, {}> {
+  public static defaultProps = defaultIrsPlanProps;
   constructor(props: RouteComponentProps<RouteParams> & IrsPlanProps) {
     super(props);
+  }
+
+  public componentDidMount() {
+    const { supersetService, fetchPlansActionCreator } = this.props;
+    supersetService(SUPERSET_PLANS_SLICE).then((result: Plan[]) => fetchPlansActionCreator(result));
   }
 
   public render() {
@@ -71,18 +93,28 @@ export { IrsPlan };
 
 const mapStateToProps = (state: Partial<Store>, ownProps: any): DispatchedStateProps => {
   const planId = ownProps.match.params.id || null;
+  const isNewPlan = planId === null;
+  const plan = getPlanById(state, planId);
+  const isDraftPlan = plan && plan.plan_status !== 'active';
+  const isFinalizedPlan = plan && plan.plan_status === 'active';
   const props = {
-    isNewPlan: planId === null,
+    isDraftPlan,
+    isFinalizedPlan,
+    isNewPlan,
+    planById: plan,
     planId,
     ...ownProps,
   };
-  if (planId && false) {
-    // todo - actually fetch the plans first
-    props.plan = getPlanById(state, planId);
-  }
   return props;
 };
 
-const ConnectedIrsPlan = connect(mapStateToProps)(IrsPlan);
+const mapDispatchToProps = {
+  fetchPlansActionCreator: fetchPlans,
+};
+
+const ConnectedIrsPlan = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(IrsPlan);
 
 export default ConnectedIrsPlan;
