f5df974f7edffe696f1a8f89e394814c2cd32cd9
p-netm <pmuriuki98@gmail.com>
2019-07-16 13:28:04 +0300

    Test title is displayed for HOME component

--- src/containers/pages/Home/tests/Home.test.tsx
+++ src/containers/pages/Home/tests/Home.test.tsx
@@ -2,7 +2,9 @@ import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import { createBrowserHistory } from 'history';
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { Router } from 'react-router';
+import { HOME_TITLE } from '../../../../constants';
 import Home from '../Home';
 
 const history = createBrowserHistory();
@@ -20,12 +22,15 @@ describe('containers/pages/Home', () => {
     );
   });
 
-  it('renders Home correctly', () => {
+  it('renders Home correctly & changes Title of page', () => {
     const wrapper = mount(
       <Router history={history}>
         <Home />
       </Router>
     );
+
+    const helmet = Helmet.peek();
+    expect(helmet.title).toEqual(HOME_TITLE);
     expect(toJson(wrapper)).toMatchSnapshot();
     wrapper.unmount();
   });
