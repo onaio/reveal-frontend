94004904070e9b9e6f2ace0477fd7e3b5c828311
kahama <phares663@gmail.com>
2019-07-17 17:27:50 +0300

    Add symbol layer logic to both polygons and points geometries

--- src/components/GisidaWrapper/index.tsx
+++ src/components/GisidaWrapper/index.tsx
@@ -350,15 +350,51 @@ class GisidaWrapper extends React.Component<GisidaProps, GisidaState> {
     }
     // handle Point layer types
     if (pointFeatureCollection) {
+      /** Build symbol layers to render on top of circle layers */
+      // const goalsWithSymbols = ['Mosquito_Collection_Min_3_Traps', 'Larval_Dipping_Min_3_Sites', 'Case_Confirmation'];
+
       let mosquitoCollectionSymbolLayer;
+      let larvalDippingSymbolLayer;
+      let caseConfirmation;
       if (this.props.currentGoal === 'Mosquito_Collection_Min_3_Traps') {
-        // build symbol layers to render on top of circle layers
         mosquitoCollectionSymbolLayer = {
           ...symbolLayerConfig,
           id: `${this.props.currentGoal}-symbol`,
           layout: {
             'icon-image': 'mosquito',
-            'icon-size': 0.25,
+            'icon-size': 0.05,
+          },
+          source: {
+            ...symbolLayerConfig.source,
+            data: {
+              ...symbolLayerConfig.source.data,
+              data: JSON.stringify(pointFeatureCollection),
+            },
+          },
+        };
+      } else if (this.props.currentGoal === 'Larval_Dipping_Min_3_Sites') {
+        larvalDippingSymbolLayer = {
+          ...symbolLayerConfig,
+          id: `${this.props.currentGoal}-symbol`,
+          layout: {
+            'icon-image': 'larval',
+            'icon-size': 0.05,
+          },
+          source: {
+            ...symbolLayerConfig.source,
+            data: {
+              ...symbolLayerConfig.source.data,
+              data: JSON.stringify(pointFeatureCollection),
+            },
+          },
+        };
+      } else if (this.props.currentGoal === 'Case_Confirmation') {
+        caseConfirmation = {
+          ...symbolLayerConfig,
+          id: `${this.props.currentGoal}-symbol`,
+          layout: {
+            'icon-image': 'case-confirmation',
+            'icon-size': 0.05,
           },
           source: {
             ...symbolLayerConfig.source,
@@ -389,9 +425,79 @@ class GisidaWrapper extends React.Component<GisidaProps, GisidaState> {
       if (mosquitoCollectionSymbolLayer) {
         builtGeometriesContainer.push(mosquitoCollectionSymbolLayer);
       }
+      if (larvalDippingSymbolLayer) {
+        builtGeometriesContainer.push(larvalDippingSymbolLayer);
+      }
+      if (caseConfirmation) {
+        builtGeometriesContainer.push(caseConfirmation);
+      }
     }
     // Handle fill layers
     if (polygonFeatureCollection) {
+      /** Build symbol layers to render on top of circle layers */
+      // const goalsWithSymbols = ['Mosquito_Collection_Min_3_Traps', 'Larval_Dipping_Min_3_Sites', 'Case_Confirmation'];
+
+      let mosquitoCollectionSymbolLayer;
+      let larvalDippingSymbolLayer;
+      let caseConfirmation;
+      if (this.props.currentGoal === 'Mosquito_Collection_Min_3_Traps') {
+        mosquitoCollectionSymbolLayer = {
+          ...symbolLayerConfig,
+          id: `${this.props.currentGoal}-symbol`,
+          layout: {
+            'icon-image': 'mosquito',
+            'icon-size': 0.05,
+          },
+          source: {
+            ...symbolLayerConfig.source,
+            data: {
+              ...symbolLayerConfig.source.data,
+              data: JSON.stringify(polygonFeatureCollection),
+            },
+          },
+        };
+      } else if (this.props.currentGoal === 'Larval_Dipping_Min_3_Sites') {
+        larvalDippingSymbolLayer = {
+          ...symbolLayerConfig,
+          id: `${this.props.currentGoal}-symbol`,
+          layout: {
+            'icon-image': 'larval',
+            'icon-size': 0.05,
+          },
+          source: {
+            ...symbolLayerConfig.source,
+            data: {
+              ...symbolLayerConfig.source.data,
+              data: JSON.stringify(polygonFeatureCollection),
+            },
+          },
+        };
+      } else if (this.props.currentGoal === 'Case_Confirmation') {
+        caseConfirmation = {
+          ...symbolLayerConfig,
+          id: `${this.props.currentGoal}-symbol`,
+          layout: {
+            'icon-image': 'case-confirmation',
+            'icon-size': 0.05,
+          },
+          source: {
+            ...symbolLayerConfig.source,
+            data: {
+              ...symbolLayerConfig.source.data,
+              data: JSON.stringify(polygonFeatureCollection),
+            },
+          },
+        };
+      }
+      if (mosquitoCollectionSymbolLayer) {
+        builtGeometriesContainer.push(mosquitoCollectionSymbolLayer);
+      }
+      if (larvalDippingSymbolLayer) {
+        builtGeometriesContainer.push(larvalDippingSymbolLayer);
+      }
+      if (caseConfirmation) {
+        builtGeometriesContainer.push(caseConfirmation);
+      }
       builtGeometriesContainer.push(
         {
           ...fillLayerConfig,
