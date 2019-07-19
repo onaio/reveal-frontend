b610331b26a6fde4fdd18347e10b4eb19a1920c2
p-netm <pmuriuki98@gmail.com>
2019-07-19 17:28:00 +0300

    Update string literal constants

--- src/constants.tsx
+++ src/constants.tsx
@@ -81,6 +81,10 @@ export const STRUCTURE_LAYER = 'structure-layer';
 export type STRUCTURE_LAYER = typeof STRUCTURE_LAYER;
 export const PROGRESS = 'Progress';
 export type PROGRESS = typeof PROGRESS;
+export const STRUCTURES = 'structure(s)';
+export type STRUCTURES = typeof STRUCTURES;
+export const PERSONS = 'person(s)';
+export type PERSON = typeof PERSONS;
 
 // internal urls
 export const LOGIN_URL = '/login';
