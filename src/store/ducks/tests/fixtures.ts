dde77c7c5b6b79282130b9b5999f6a70d3ab5861
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-07-19 14:15:35 +0300

    Add plan 99 fixture

--- src/store/ducks/tests/fixtures.ts
+++ src/store/ducks/tests/fixtures.ts
@@ -95,6 +95,37 @@ export const plan3 = {
   plan_title: 'A1-Tha Luang Village 1 Focus 01',
 };
 
+export const plan99 = {
+  id: '236ca3fb-1b74-5028-a0c8-ab954bb28044',
+  jurisdiction_depth: 4,
+  jurisdiction_id: '4550a8ab-b310-4881-8c76-1b6a817ea63a',
+  jurisdiction_name: 'Tha Sen 8',
+  jurisdiction_name_path: [
+    'Trat 2',
+    'Mueng Trat District 2',
+    'Laem Klat Canton 2',
+    'Laem Klat Moo 8',
+  ],
+  jurisdiction_parent_id: 'a14c6253-2d3d-4bcc-8c49-35f9e44c66e1',
+  jurisdiction_path: [
+    '61707fc2 - c6ac - 4112 - a8d6 - 2a4861958396',
+    '8a8e4987- 95a9-4b2e - b746 - 3c9c3eec15c5',
+    '735a94f9 - 6ed2 - 4bcd - b242 - 8c2b5f714bee',
+    'a14c6253 - 2d3d - 4bcc - 8c49 - 35f9e44c66e1',
+  ],
+  plan_date: '2019-07-03',
+  plan_effective_period_end: '2019-07-30',
+  plan_effective_period_start: '2019-06-18',
+  plan_fi_reason: 'Case-triggered',
+  plan_fi_status: 'A1',
+  plan_id: '5ad62ca5-6b9b-4c5e-bd72-c9824074c385',
+  plan_intervention_type: 'FI',
+  plan_name: 'A1-ThaSen8_01',
+  plan_status: 'active',
+  plan_title: 'A1 - Tha Sen 8- Focus 01',
+  plan_version: '1',
+};
+
 export const plans = [plan1, plan2];
 
 export const plansIdArray = [plan1.plan_id];
