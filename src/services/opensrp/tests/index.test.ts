00bd69e2401fa40a770e9465e35756dd3513302a
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-07-22 12:57:53 +0300

    Add test for OpenSRPService constructor

--- src/services/opensrp/tests/index.test.ts
+++ src/services/opensrp/tests/index.test.ts
@@ -1,8 +1,10 @@
 import { getOpenSRPUserInfo } from '@onaio/gatekeeper';
 import { authenticateUser } from '@onaio/session-reducer';
 import store from '../../../store';
-import { getDefaultHeaders } from '../index';
+import { getDefaultHeaders, OpenSRPService } from '../index';
 import { OpenSRPAPIResponse } from './fixtures/session';
+
+jest.mock('../../../configs/env');
 
 describe('services/OpenSRP', () => {
   beforeEach(() => {
@@ -10,7 +12,6 @@ describe('services/OpenSRP', () => {
   });
 
   it('getDefaultHeaders works', async () => {
-    // call action to log in
     const { authenticated, user, extraData } = getOpenSRPUserInfo(OpenSRPAPIResponse);
     await store.dispatch(authenticateUser(authenticated, user, extraData));
     expect(getDefaultHeaders()).toEqual({
@@ -18,5 +19,12 @@ describe('services/OpenSRP', () => {
       authorization: 'Token hunter2',
       'content-type': 'application/json',
     });
+  });
+
+  it('OpenSRPService constructor works', async () => {
+    const planService = new OpenSRPService('plans');
+    expect(planService.baseURL).toEqual('https://test.smartregister.org/opensrp/rest/');
+    expect(planService.endpoint).toEqual('plans');
+    expect(planService.generalURL).toEqual('https://test.smartregister.org/opensrp/rest/plans');
   });
 });
