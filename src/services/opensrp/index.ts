b4276b96b9959de500f41f12309a7f23eee09ff6
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-07-22 12:33:02 +0300

    Use function to get default headers

--- src/services/opensrp/index.ts
+++ src/services/opensrp/index.ts
@@ -2,9 +2,11 @@ import { IncomingHttpHeaders } from 'http';
 import store from '../../store';
 import { getAccessToken } from '../../store/selectors';
 
-/** default HTTP headers for OpenSRP service */
-export const defaultHeaders: IncomingHttpHeaders = {
-  accept: 'application/json',
-  authorization: `Token ${getAccessToken(store.getState())}`,
-  'content-type': 'application/json',
-};
+/** get default HTTP headers for OpenSRP service */
+export function getDefaultHeaders(): IncomingHttpHeaders {
+  return {
+    accept: 'application/json',
+    authorization: `Token ${getAccessToken(store.getState())}`,
+    'content-type': 'application/json',
+  };
+}
