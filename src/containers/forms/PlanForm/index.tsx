9b7c512fe44a05ca3e365d7abf4b90a4c6d81229
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-07-24 19:07:27 +0300

    cleanup initial form

--- src/containers/forms/PlanForm/index.tsx
+++ src/containers/forms/PlanForm/index.tsx
@@ -1,27 +1,55 @@
-import { Field, FieldArray, Formik } from 'formik';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 import React from 'react';
-import { Alert, Button, Col, Form, FormGroup, FormText, Input, Label, Row } from 'reactstrap';
+import { Button, FormGroup, Label } from 'reactstrap';
 import { InterventionType } from '../../../store/ducks/plans';
-
-/** HTML element for plan form */
-const FormElement = () => {
-  return (
-    <Form>
-      <FormGroup className="row">
-        <Label for="interventionType">Intervention Type</Label>
-        <Input type="select" name="interventionType" id="interventionType">
-          <option value="{InterventionType.FI}">FI</option>
-        </Input>
-      </FormGroup>
-    </Form>
-  );
-};
 
 /** Plan Form component */
 const PlanForm = () => {
   return (
     <div className="form-container">
-      <Formik render={FormElement} />
+      <Formik
+        initialValues={{ interventionType: InterventionType.FI }}
+        /* tslint:disable-next-line jsx-no-lambda */
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <FormGroup>
+              <Label for="interventionType">Intervention Type</Label>
+              <Field
+                component="select"
+                name="interventionType"
+                id="interventionType"
+                className="form-control"
+              >
+                <option value="{InterventionType.FI}">FI</option>
+                <option value="{InterventionType.IRS}">IRS</option>
+              </Field>
+              <ErrorMessage
+                name="interventionType"
+                component="small"
+                className="form-text text-danger"
+              />
+            </FormGroup>
+            <Button
+              type="submit"
+              className="btn btn-block"
+              color="primary"
+              aria-label="Save Plan"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? 'Saving' : 'Save Plan'}
+            </Button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
+
+export default PlanForm;
