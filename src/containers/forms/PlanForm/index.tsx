d6581a4611654c9d26fa15f5e2e5178a7955d616
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-07-24 18:10:56 +0300

    Add sample form

--- src/containers/forms/PlanForm/index.tsx
+++ src/containers/forms/PlanForm/index.tsx
@@ -3,11 +3,31 @@ import React from 'react';
 import { Alert, Button, Col, Form, FormGroup, FormText, Input, Label, Row } from 'reactstrap';
 import { InterventionType } from '../../../store/ducks/plans';
 
+/** HTML element for plan form */
+const XForm = () => {
+  return (
+    <Form>
+      <label htmlFor="firstName">First Name</label>
+      <Field id="firstName" name="firstName" placeholder="John" type="text" />
+
+      <label htmlFor="lastName">Last Name</label>
+      <Field id="lastName" name="lastName" placeholder="Doe" type="text" />
+
+      <label htmlFor="email">Email</label>
+      <Field id="email" name="email" placeholder="john@acme.com" type="email" />
+
+      <button type="submit" style={{ display: 'block' }}>
+        Submit
+      </button>
+    </Form>
+  );
+};
+
 /** Plan Form component */
 const PlanForm = () => {
   return (
     <div className="form-container">
-      <Formik />
+      <Formik render={XForm} />
     </div>
   );
 };
