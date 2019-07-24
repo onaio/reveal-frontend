11a0a15204ca860a37bd83e256ac996fe9013810
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-07-24 18:07:42 +0300

    Add basic plan form

---
+++ src/containers/forms/PlanForm/index.tsx
@@ -0,0 +1,9 @@
+import { Field, FieldArray, Formik } from 'formik';
+import React from 'react';
+import { Alert, Button, Col, Form, FormGroup, FormText, Input, Label, Row } from 'reactstrap';
+import { InterventionType } from '../../../store/ducks/plans';
+
+/** Logout component */
+const PlanForm = () => {
+  return <div className="form-container" />;
+};
