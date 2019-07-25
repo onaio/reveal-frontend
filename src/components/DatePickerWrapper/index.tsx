a59efb6a48796a9888188895c3ceb207450f8a5c
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-07-25 14:57:32 +0300

    Pass props down to DatePickerWrapper

--- src/components/DatePickerWrapper/index.tsx
+++ src/components/DatePickerWrapper/index.tsx
@@ -4,12 +4,13 @@ import DatePicker, { ReactDatePickerProps } from 'react-datepicker';
 
 import 'react-datepicker/dist/react-datepicker.css';
 
+/** DatePickerWrapper */
 const DatePickerWrapper = (props: ReactDatePickerProps & FieldProps) => {
   const handleChange = (date: any) => {
     props.form.setFieldValue(props.field.name, date);
   };
 
-  return <DatePicker selected={props.field.value} onChange={handleChange} isClearable={true} />;
+  return <DatePicker selected={props.field.value} onChange={handleChange} {...props} />;
 };
 
 export default DatePickerWrapper;
