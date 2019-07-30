07169347efbaccdca11f13d85cd79d286ddbefde
kahama <phares663@gmail.com>
2019-07-30 17:16:02 +0300

    Add options to react select component

--- src/components/SelectPlan/index.tsx
+++ src/components/SelectPlan/index.tsx
@@ -1,13 +1,31 @@
 import * as React from 'react';
 import Select from 'react-select';
+import { Plan } from '../../store/ducks/plans';
 
-class SelectPlan extends React.Component<{}, {}> {
-  constructor(props: {}) {
+export interface SelectPlanProps {
+  plansArray: Plan[];
+}
+class SelectPlan extends React.Component<SelectPlanProps, {}> {
+  constructor(props: SelectPlanProps) {
     super(props);
   }
-
   public render() {
-    return <Select placeholder="Other area Investigations" />;
+    const { plansArray } = this.props;
+    /** Sort plans by plan_date and build value label key value pairs to populate the select */
+    let options;
+    options = plansArray.sort((a, b) => {
+      if (a.plan_date > b.plan_date) {
+        return -1;
+      }
+      if (a.plan_date < b.plan_date) {
+        return 1;
+      }
+      return 0;
+    });
+    options = options.map(element => {
+      return { value: element.plan_id, label: element.plan_title };
+    });
+    return <Select defaultOptions={true} options={options} />;
   }
 }
 
