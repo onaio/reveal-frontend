68b0cfead58b7416cd2fac76a0ca32bd457967f1
FrankApiyo <franklineapiyo@gmail.com>
2019-08-02 13:41:24 +0300

    Make LinkToNewPlans use props and use arrow funtion

--- src/components/LinkToNewPlans/index.tsx
+++ src/components/LinkToNewPlans/index.tsx
@@ -2,12 +2,18 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { NEW_PLAN_URL } from '../../constants';
 
-function LinkToNewPlans() {
+export interface Props {
+  classProp: string;
+}
+
+const LinkToNewPlans = ({
+  classProp = 'focus-investigation btn btn-primary float-right mt-5',
+}: Props) => {
   return (
-    <Link to={NEW_PLAN_URL} className="focus-investigation btn btn-primary float-right mt-5">
+    <Link to={NEW_PLAN_URL} className={classProp}>
       Add Focus Investigation
     </Link>
   );
-}
+};
 
 export default LinkToNewPlans;
