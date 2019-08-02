9107a50a3ebdc940364899c98cfdc43992883438
FrankApiyo <franklineapiyo@gmail.com>
2019-08-02 18:51:52 +0300

    Correct docstring

--- src/components/LinkToNewPlans/index.tsx
+++ src/components/LinkToNewPlans/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { NEW_PLAN_URL } from '../../constants';
 
-/** interface for LinkToNewPlans component */
+/** interface for LinkToNewPlans component props */
 export interface Props {
   classProp: string;
 }
