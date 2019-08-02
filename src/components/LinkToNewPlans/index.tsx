70682374a148459fcb7a684bd3c7fc658e70ef0b
FrankApiyo <franklineapiyo@gmail.com>
2019-08-02 18:24:49 +0300

    Set default props using alternative method

--- src/components/LinkToNewPlans/index.tsx
+++ src/components/LinkToNewPlans/index.tsx
@@ -3,17 +3,18 @@ import { Link } from 'react-router-dom';
 import { NEW_PLAN_URL } from '../../constants';
 
 export interface Props {
-  classProp?: string;
+  classProp: string;
 }
 
-const LinkToNewPlans = ({
-  classProp = 'focus-investigation btn btn-primary float-right mt-5',
-}: Props) => {
+const LinkToNewPlans = (props: Props) => {
   return (
-    <Link to={NEW_PLAN_URL} className={classProp}>
+    <Link to={NEW_PLAN_URL} className={props.classProp}>
       Add Focus Investigation
     </Link>
   );
 };
+
+const defaultProps: Props = { classProp: 'focus-investigation btn btn-primary float-right mt-5' };
+LinkToNewPlans.defaultProps = defaultProps;
 
 export default LinkToNewPlans;
