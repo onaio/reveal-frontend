36f4806e317b924df706119a4906adb0b83a7376
FrankApiyo <franklineapiyo@gmail.com>
2019-08-05 15:14:22 +0300

    Add test: 'onHandle change is called when date is chosen'

--- src/components/DatePickerWrapper/tests/index.test.tsx
+++ src/components/DatePickerWrapper/tests/index.test.tsx
@@ -19,4 +19,20 @@ describe('components/DatePickerWrapper', () => {
     const handleChange = () => jest.fn();
     shallow(<DatePickerWrapper selected={new Date()} onChange={handleChange} {...props} />);
   });
+
+  it('onHandle change is called when date is chosen', () => {
+    const handleChange = jest.fn();
+    const wrapper = mount(
+      <DatePickerWrapper selected={new Date()} onChange={handleChange} {...props} />
+    );
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper
+      .find('.react-datepicker__input-container > input')
+      .simulate('change', { target: { value: '2018-01-01' } });
+    expect(handleChange.mock.calls.length).toBe(1);
+    wrapper
+      .find('.react-datepicker__input-container > input')
+      .simulate('change', { target: { value: '2018-01-01' } });
+    expect(handleChange.mock.calls.length).toBe(2);
+  });
 });
