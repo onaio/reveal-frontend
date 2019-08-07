e92dfcaf82c768d9b67615596d8943531a66bbbc
FrankApiyo <franklineapiyo@gmail.com>
2019-08-07 09:41:02 +0300

    Add expect statement to test: 'check extractActivityForForm returns the correct value'

--- src/containers/forms/PlanForm/tests/helpers.test.ts
+++ src/containers/forms/PlanForm/tests/helpers.test.ts
@@ -1,8 +1,16 @@
 import { extractActivityForForm } from '../helpers';
-import { bccTestPlanActivity, expectedActivity } from './fixtures';
+import {
+  bccTestPlanActivity,
+  bccTestPlanActivityWithEmptyfields,
+  expectedActivity,
+  expectedActivity2,
+} from './fixtures';
 
 describe('PlanForm/helpers', () => {
   it('check extractActivityForForm returns the correct value', () => {
     expect(extractActivityForForm(bccTestPlanActivity.BCC)).toEqual(expectedActivity);
+    expect(extractActivityForForm(bccTestPlanActivityWithEmptyfields.BCC)).toEqual(
+      expectedActivity2
+    );
   });
 });
