fedf709c31aaae405fb0af0bc7b2a2abd2850415
FrankApiyo <franklineapiyo@gmail.com>
2019-08-07 11:43:36 +0300

    Add test: check getFormActivities returns the correct value

--- src/containers/forms/PlanForm/tests/helpers.test.ts
+++ src/containers/forms/PlanForm/tests/helpers.test.ts
@@ -1,9 +1,11 @@
-import { extractActivityForForm } from '../helpers';
+import { extractActivityForForm, getFormActivities } from '../helpers';
 import {
   bccTestPlanActivity,
   bccTestPlanActivityWithEmptyfields,
   expectedActivity,
   expectedActivity2,
+  extractedActivitiesFromForms,
+  planActivities,
 } from './fixtures';
 
 describe('PlanForm/helpers', () => {
@@ -12,5 +14,9 @@ describe('PlanForm/helpers', () => {
     expect(extractActivityForForm(bccTestPlanActivityWithEmptyfields.BCC)).toEqual(
       expectedActivity2
     );
+  });
+
+  it('check getFormActivities returns the correct value', () => {
+    expect(getFormActivities(planActivities)).toBe(extractedActivitiesFromForms);
   });
 });
