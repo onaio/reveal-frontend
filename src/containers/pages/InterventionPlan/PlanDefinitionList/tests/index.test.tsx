6b0bd0d9a512be2f8b07b48f9410fbc8b429b86a
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-08-08 15:38:34 +0300

    Add test for ConnectedPlanDefinitionList

--- src/containers/pages/InterventionPlan/PlanDefinitionList/tests/index.test.tsx
+++ src/containers/pages/InterventionPlan/PlanDefinitionList/tests/index.test.tsx
@@ -2,9 +2,14 @@ import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import { createBrowserHistory } from 'history';
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router';
-import { PlanDefinitionList } from '../';
+import ConnectedPlanDefinitionList, { PlanDefinitionList } from '../';
+import store from '../../../../../store';
 import * as fixtures from '../../../../../store/ducks/opensrp/PlanDefinition/tests/fixtures';
+
+/* tslint:disable-next-line no-var-requires */
+const fetch = require('jest-fetch-mock');
 
 const history = createBrowserHistory();
 
@@ -25,6 +30,7 @@ describe('components/InterventionPlan/PlanDefinitionList', () => {
   });
 
   it('renders plan definition list correctly', () => {
+    fetch.mockResponseOnce(fixtures.plansJSON);
     const props = {
       plans: fixtures.plans,
     };
@@ -32,6 +38,19 @@ describe('components/InterventionPlan/PlanDefinitionList', () => {
       <Router history={history}>
         <PlanDefinitionList {...props} />
       </Router>
+    );
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.unmount();
+  });
+
+  it('works when connected to redux store', () => {
+    fetch.mockResponseOnce(fixtures.plansJSON);
+    const wrapper = mount(
+      <Provider store={store}>
+        <Router history={history}>
+          <ConnectedPlanDefinitionList />
+        </Router>
+      </Provider>
     );
     expect(toJson(wrapper)).toMatchSnapshot();
     wrapper.unmount();
