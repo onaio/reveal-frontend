d213a7b36838e76c18f16735fa9a7005ddbac896
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-08-14 12:13:23 +0300

    Add shallow render test

--- src/components/NewRecordBadge/tests/index.test.tsx
+++ src/components/NewRecordBadge/tests/index.test.tsx
@@ -1,8 +1,13 @@
 import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
+import NewRecordBadge from '..';
 
 describe('components/NewRecordBadge', () => {
   beforeEach(() => {
     jest.resetAllMocks();
+  });
+
+  it('renders without crashing', () => {
+    shallow(<NewRecordBadge />);
   });
 });
