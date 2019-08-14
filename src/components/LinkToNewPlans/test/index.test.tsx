d1653bad1a1ed2903bb3513c80ccfe1ad5000c51
FrankApiyo <franklineapiyo@gmail.com>
2019-08-14 09:58:38 +0300

    Add test: renders without crashing

---
+++ src/components/LinkToNewPlans/test/index.test.tsx
@@ -0,0 +1,14 @@
+import { mount, shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import { createBrowserHistory } from 'history';
+import React from 'react';
+import { Router } from 'react-router';
+import LinkToNewPlans from '..';
+
+const history = createBrowserHistory();
+
+describe('components/LinkToNewPlans', () => {
+  it('renders without crashing', () => {
+    shallow(<LinkToNewPlans />);
+  });
+});
