502df3425513f857006ba4290cf7e91072f567b2
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-08-15 17:30:08 +0300

    Uncomment tests

--- src/containers/forms/PlanForm/tests/helpers.test.ts
+++ src/containers/forms/PlanForm/tests/helpers.test.ts
@@ -30,122 +30,122 @@ import {
 } from './fixtures';
 
 describe('containers/forms/PlanForm/helpers', () => {
-  // it('check extractActivityForForm returns the correct value for BCC', () => {
-  //   expect(extractActivityForForm(planActivities.BCC)).toEqual(expectedActivity.BCC);
-  //   expect(extractActivityForForm(planActivityWithEmptyfields.BCC)).toEqual(
-  //     expectedActivityEmptyField.BCC
-  //   );
-  // });
+  it('check extractActivityForForm returns the correct value for BCC', () => {
+    expect(extractActivityForForm(planActivities.BCC)).toEqual(expectedActivity.BCC);
+    expect(extractActivityForForm(planActivityWithEmptyfields.BCC)).toEqual(
+      expectedActivityEmptyField.BCC
+    );
+  });
 
-  // it('check extractActivityForForm returns the correct value for IRS', () => {
-  //   expect(extractActivityForForm(planActivities.bednetDistribution)).toEqual(
-  //     expectedActivity.bednetDistribution
-  //   );
-  //   expect(extractActivityForForm(planActivityWithEmptyfields.bednetDistribution)).toEqual(
-  //     expectedActivityEmptyField.bednetDistribution
-  //   );
-  // });
+  it('check extractActivityForForm returns the correct value for IRS', () => {
+    expect(extractActivityForForm(planActivities.bednetDistribution)).toEqual(
+      expectedActivity.bednetDistribution
+    );
+    expect(extractActivityForForm(planActivityWithEmptyfields.bednetDistribution)).toEqual(
+      expectedActivityEmptyField.bednetDistribution
+    );
+  });
 
-  // it('check extractActivityForForm returns the correct value for bloodScreening', () => {
-  //   expect(extractActivityForForm(planActivities.bloodScreening)).toEqual(
-  //     expectedActivity.bloodScreening
-  //   );
-  //   expect(extractActivityForForm(planActivityWithEmptyfields.bloodScreening)).toEqual(
-  //     expectedActivityEmptyField.bloodScreening
-  //   );
-  // });
+  it('check extractActivityForForm returns the correct value for bloodScreening', () => {
+    expect(extractActivityForForm(planActivities.bloodScreening)).toEqual(
+      expectedActivity.bloodScreening
+    );
+    expect(extractActivityForForm(planActivityWithEmptyfields.bloodScreening)).toEqual(
+      expectedActivityEmptyField.bloodScreening
+    );
+  });
 
-  // it('check extractActivityForForm returns the correct value for caseConfirmation', () => {
-  //   expect(extractActivityForForm(planActivities.caseConfirmation)).toEqual(
-  //     expectedActivity.caseConfirmation
-  //   );
-  //   expect(extractActivityForForm(planActivityWithEmptyfields.caseConfirmation)).toEqual(
-  //     expectedActivityEmptyField.caseConfirmation
-  //   );
-  // });
+  it('check extractActivityForForm returns the correct value for caseConfirmation', () => {
+    expect(extractActivityForForm(planActivities.caseConfirmation)).toEqual(
+      expectedActivity.caseConfirmation
+    );
+    expect(extractActivityForForm(planActivityWithEmptyfields.caseConfirmation)).toEqual(
+      expectedActivityEmptyField.caseConfirmation
+    );
+  });
 
-  // it('check extractActivityForForm returns the correct value for larvalDipping', () => {
-  //   expect(extractActivityForForm(planActivities.larvalDipping)).toEqual(
-  //     expectedActivity.larvalDipping
-  //   );
-  //   expect(extractActivityForForm(planActivityWithEmptyfields.larvalDipping)).toEqual(
-  //     expectedActivityEmptyField.larvalDipping
-  //   );
-  // });
+  it('check extractActivityForForm returns the correct value for larvalDipping', () => {
+    expect(extractActivityForForm(planActivities.larvalDipping)).toEqual(
+      expectedActivity.larvalDipping
+    );
+    expect(extractActivityForForm(planActivityWithEmptyfields.larvalDipping)).toEqual(
+      expectedActivityEmptyField.larvalDipping
+    );
+  });
 
-  // it('check extractActivityForForm returns the correct value for familyRegistration', () => {
-  //   expect(extractActivityForForm(planActivities.familyRegistration)).toEqual(
-  //     expectedActivity.familyRegistration
-  //   );
-  // });
+  it('check extractActivityForForm returns the correct value for familyRegistration', () => {
+    expect(extractActivityForForm(planActivities.familyRegistration)).toEqual(
+      expectedActivity.familyRegistration
+    );
+  });
 
-  // it('check extractActivityForForm returns the correct value for mosquitoCollection', () => {
-  //   expect(extractActivityForForm(planActivities.mosquitoCollection)).toEqual(
-  //     expectedActivity.mosquitoCollection
-  //   );
-  //   expect(extractActivityForForm(planActivityWithEmptyfields.mosquitoCollection)).toEqual(
-  //     expectedActivityEmptyField.mosquitoCollection
-  //   );
-  // });
+  it('check extractActivityForForm returns the correct value for mosquitoCollection', () => {
+    expect(extractActivityForForm(planActivities.mosquitoCollection)).toEqual(
+      expectedActivity.mosquitoCollection
+    );
+    expect(extractActivityForForm(planActivityWithEmptyfields.mosquitoCollection)).toEqual(
+      expectedActivityEmptyField.mosquitoCollection
+    );
+  });
 
-  // it('check getFormActivities returns the correct value', () => {
-  //   expect(JSON.stringify(getFormActivities(planActivities))).toEqual(
-  //     JSON.stringify(extractedActivitiesFromForms)
-  //   );
-  // });
+  it('check getFormActivities returns the correct value', () => {
+    expect(JSON.stringify(getFormActivities(planActivities))).toEqual(
+      JSON.stringify(extractedActivitiesFromForms)
+    );
+  });
 
-  // it('check doesFieldHaveErrors returns the correct value', () => {
-  //   let errors = [
-  //     {
-  //       id: 'Required',
-  //     },
-  //   ];
-  //   let field = 'id';
-  //   let index = 0;
-  //   expect(doesFieldHaveErrors(field, index, errors)).toBe(true);
-  //   field = '';
-  //   index = NaN;
-  //   errors = [];
-  //   expect(doesFieldHaveErrors(field, index, errors)).toBe(false);
-  // });
+  it('check doesFieldHaveErrors returns the correct value', () => {
+    let errors = [
+      {
+        id: 'Required',
+      },
+    ];
+    let field = 'id';
+    let index = 0;
+    expect(doesFieldHaveErrors(field, index, errors)).toBe(true);
+    field = '';
+    index = NaN;
+    errors = [];
+    expect(doesFieldHaveErrors(field, index, errors)).toBe(false);
+  });
 
-  // it('check extractActivitiesFromPlanForm returns the correct value', () => {
-  //   MockDate.set('1/30/2000', 0);
-  //   expect(extractActivitiesFromPlanForm(activities)).toEqual(
-  //     expectedExtractActivityFromPlanformResult
-  //   );
-  //   MockDate.reset();
-  // });
+  it('check extractActivitiesFromPlanForm returns the correct value', () => {
+    MockDate.set('1/30/2000', 0);
+    expect(extractActivitiesFromPlanForm(activities)).toEqual(
+      expectedExtractActivityFromPlanformResult
+    );
+    MockDate.reset();
+  });
 
-  // it('check getNameTitle returns the correct value when Focus Investigation(FI) is selected', () => {
-  //   expect(getNameTitle(event, values)).toEqual(['A1--2019-08-09', 'A1  2019-08-09']);
-  //   expect(getNameTitle(event, valuesWithJurisdiction)).toEqual([
-  //     'A1-TLv2_01-2019-08-09',
-  //     'A1 TLv2_01 2019-08-09',
-  //   ]);
-  // });
+  it('check getNameTitle returns the correct value when Focus Investigation(FI) is selected', () => {
+    expect(getNameTitle(event, values)).toEqual(['A1--2019-08-09', 'A1  2019-08-09']);
+    expect(getNameTitle(event, valuesWithJurisdiction)).toEqual([
+      'A1-TLv2_01-2019-08-09',
+      'A1 TLv2_01 2019-08-09',
+    ]);
+  });
 
-  // it('check getNameTitle returns the correct value when IRS is selected', () => {
-  //   expect(getNameTitle(event2, values)).toEqual(['IRS-2019-08-09', 'IRS 2019-08-09']);
-  //   expect(getNameTitle(event2, valuesWithJurisdiction)).toEqual([
-  //     'IRS-2019-08-09',
-  //     'IRS 2019-08-09',
-  //   ]);
-  // });
+  it('check getNameTitle returns the correct value when IRS is selected', () => {
+    expect(getNameTitle(event2, values)).toEqual(['IRS-2019-08-09', 'IRS 2019-08-09']);
+    expect(getNameTitle(event2, valuesWithJurisdiction)).toEqual([
+      'IRS-2019-08-09',
+      'IRS 2019-08-09',
+    ]);
+  });
 
-  // it('check getNameTitle returns the correct value when nothing is selected', () => {
-  //   expect(getNameTitle(event3, values)).toEqual(['IRS-2019-08-09', 'IRS 2019-08-09']);
-  //   expect(getNameTitle(event3, valuesWithJurisdiction)).toEqual([
-  //     'IRS-2019-08-09',
-  //     'IRS 2019-08-09',
-  //   ]);
-  // });
+  it('check getNameTitle returns the correct value when nothing is selected', () => {
+    expect(getNameTitle(event3, values)).toEqual(['IRS-2019-08-09', 'IRS 2019-08-09']);
+    expect(getNameTitle(event3, valuesWithJurisdiction)).toEqual([
+      'IRS-2019-08-09',
+      'IRS 2019-08-09',
+    ]);
+  });
 
-  // it('check generatePlanDefinition returns the correct value', () => {
-  //   MockDate.set('1/30/2000', 0);
-  //   expect(generatePlanDefinition(values2)).toEqual(expectedPlanDefinition);
-  //   MockDate.reset();
-  // });
+  it('check generatePlanDefinition returns the correct value', () => {
+    MockDate.set('1/30/2000', 0);
+    expect(generatePlanDefinition(values2)).toEqual(expectedPlanDefinition);
+    MockDate.reset();
+  });
 
   it('getPlanFormValues can get original planForm', () => {
     const planForm = planFormValues as PlanFormFields;
