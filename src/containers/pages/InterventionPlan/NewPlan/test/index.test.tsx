194d7f8930c8324ca18ba82bf78126bc5a07a3ae
FrankApiyo <franklineapiyo@gmail.com>
2019-08-19 14:06:26 +0300

    Add snapshot test

--- src/containers/pages/InterventionPlan/NewPlan/test/index.test.tsx
+++ src/containers/pages/InterventionPlan/NewPlan/test/index.test.tsx
@@ -1,6 +1,11 @@
 import { mount, shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import { createBrowserHistory } from 'history';
 import React from 'react';
+import { Router } from 'react-router';
 import NewPlan from '../index';
+
+const history = createBrowserHistory();
 
 describe('containers/pages/NewPlan', () => {
   beforeEach(() => {
@@ -9,5 +14,15 @@ describe('containers/pages/NewPlan', () => {
 
   it('renders without crashing', () => {
     shallow(<NewPlan />);
+  });
+
+  it('renders correctly', () => {
+    const wrapper = mount(
+      <Router history={history}>
+        <NewPlan />
+      </Router>
+    );
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.unmount();
   });
 });
