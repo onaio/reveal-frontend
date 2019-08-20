46377466d2876a6ce8bc753dca6290f72d39b7a2
FrankApiyo <franklineapiyo@gmail.com>
2019-08-20 17:33:03 +0300

    Add tests that use fixtures instead of snapshot

--- src/containers/pages/InterventionPlan/NewPlan/test/index.test.tsx
+++ src/containers/pages/InterventionPlan/NewPlan/test/index.test.tsx
@@ -4,6 +4,7 @@ import { createBrowserHistory } from 'history';
 import React from 'react';
 import { Router } from 'react-router';
 import NewPlan from '../index';
+import { someObject } from './fixtures';
 
 const history = createBrowserHistory();
 
@@ -27,8 +28,7 @@ describe('containers/pages/NewPlan', () => {
     expect(toJson(wrapper.find('h3.mb-3.page-title'))).toMatchSnapshot('page title');
 
     // check that planform receives the correct props
-    expect(wrapper.find('PlanForm').props()).toMatchSnapshot('plan form props');
-
+    expect(JSON.stringify(wrapper.find('PlanForm').props())).toEqual(JSON.stringify(someObject));
     // check that there's a Row that nests a Col that nests a Planform
     expect(wrapper.find('Row')).toHaveLength(1);
     expect(wrapper.find('Row').find('Col')).toHaveLength(1);
