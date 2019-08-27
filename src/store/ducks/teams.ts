075282d7d87f004043083224307ae5f948383c24
p-netm <pmuriuki98@gmail.com>
2019-08-27 14:47:53 +0300

    Refactor team dux store to organization dux store

--- src/store/ducks/teams.ts
+++ src/store/ducks/teams.ts
@@ -1,36 +1,48 @@
-/** Teams redux module */
+/** Organizations redux module */
 import { get } from 'lodash';
 import { Store } from 'redux';
 import { AnyAction } from 'redux';
 import SeamlessImmutable from 'seamless-immutable';
 
 /** The reducer name */
-export const reducerName = 'teams';
+export const reducerName = 'organizations';
 
-/** interface for a Team object */
-export interface Team {
-  id: string;
+/** interface for an organization's address object */
+interface OrganizationAddress {
+  [key: string]: string;
+}
+
+/** interface for a Organization object */
+export interface Organization {
+  identifier: string;
+  // is organization record still active
+  active: boolean;
+  // kind of organization
+  type: string;
   name: string;
+  alias: string[];
+  address: OrganizationAddress[];
 }
 
-/** interface for Teams state in store */
-interface TeamState {
-  teamsById: { [key: string]: Team };
+/** interface for Organizations state in store */
+interface OrganizationState {
+  organizationsById: { [key: string]: Organization };
 }
 
-/** initial state for Teams records in store */
-const initialTeamState: ImmutableTeamState = SeamlessImmutable({
-  teamsById: {},
+/** initial state for Organizations records in store */
+const initialOrganizationState: ImmutableOrganizationState = SeamlessImmutable({
+  organizationsById: {},
 });
 
 /** single type for all action types */
-type TeamActionTypes = AnyAction;
+type OrganizationActionTypes = AnyAction;
 
-// immutable team state in dux
-export type ImmutableTeamState = TeamState & SeamlessImmutable.ImmutableObject<TeamState>;
+// immutable organization state in dux
+export type ImmutableOrganizationState = OrganizationState &
+  SeamlessImmutable.ImmutableObject<OrganizationState>;
 
-/** the Team reducer function */
-export default function reducer(state = initialTeamState, action: TeamActionTypes) {
+/** the Organization reducer function */
+export default function reducer(state = initialOrganizationState, action: OrganizationActionTypes) {
   switch (action.type) {
     default:
       return state;
@@ -39,12 +51,15 @@ export default function reducer(state = initialTeamState, action: TeamActionType
 
 // selectors
 
-/** Get single Team by the given id
+/** Get single Organization by the given id
  * @param {Partial<Store>} state - Part of the redux store
- * @param {string} teamId - filters team data to be returned based on this id
+ * @param {string} organizationId - filters organization data to be returned based on this id
  *
- * @return {Team | null} - A team object if found else null
+ * @return {Organization | null} - A organization object if found else null
  */
-export function getTeamById(state: Partial<Store>, teamId: string): Team | null {
-  return get((state as any)[reducerName].teamsById, teamId) || null;
+export function getOrganizationById(
+  state: Partial<Store>,
+  organizationId: string
+): Organization | null {
+  return get((state as any)[reducerName].organizationsById, organizationId) || null;
 }
