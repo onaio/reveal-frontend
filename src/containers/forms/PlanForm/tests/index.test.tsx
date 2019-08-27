32dfb7fddf8a4870ac74abc0576e10bf84543eb8
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-08-27 18:47:55 +0300

    Add test for title error message

--- src/containers/forms/PlanForm/tests/index.test.tsx
+++ src/containers/forms/PlanForm/tests/index.test.tsx
@@ -279,8 +279,8 @@ describe('containers/forms/PlanForm', () => {
 
     // name is required
     expect(wrapper.find('.non-field-errors p.name-error').text()).toEqual('Name is Required');
-    // // title is required
-    // expect(wrapper.find('.non-field-errors p.name-error').text()).toEqual('Name is Required');
+    // title is required
+    expect(wrapper.find('small.title-error').text()).toEqual('Required');
     // jurisdiction is required
     expect(wrapper.find('small.jurisdictions-error').text()).toEqual('An Error Ocurred');
   });
