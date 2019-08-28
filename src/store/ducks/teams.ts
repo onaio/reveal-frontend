be04a3847f960892370070f0626b08b37fee9b9e
p-netm <pmuriuki98@gmail.com>
2019-08-28 14:29:54 +0300

    Add actions, the creators and selectors

--- src/store/ducks/teams.ts
+++ src/store/ducks/teams.ts
@@ -1,65 +1,121 @@
-/** Organizations redux module */
-import { get } from 'lodash';
+/** Teams redux module */
+import { get, keyBy, values } from 'lodash';
 import { Store } from 'redux';
 import { AnyAction } from 'redux';
 import SeamlessImmutable from 'seamless-immutable';
 
 /** The reducer name */
-export const reducerName = 'organizations';
+export const reducerName = 'teams';
 
-/** interface for an organization's address object */
-interface OrganizationAddress {
-  [key: string]: string;
-}
-
-/** interface for a Organization object */
-export interface Organization {
+/** interface for a Team object */
+export interface Team {
   identifier: string;
-  // is organization record still active
-  active: boolean;
-  // kind of organization
-  type: string;
   name: string;
-  alias: string[];
-  address: OrganizationAddress[];
+  jurisdictions: string[];
 }
 
-/** interface for Organizations state in store */
-interface OrganizationState {
-  organizationsById: { [key: string]: Organization };
+// action interfaces
+
+/** action type for action that adds Teams to store */
+export const TEAMS_FETCHED = 'src/store/ducks/teams/reducer/TEAM_FETCHED';
+/** action type for REMOVE_TEAMS action */
+export const REMOVE_TEAMS = 'src/store/ducks/teams/reducer/REMOVE_TEAMS';
+
+/** interface for teams fetched action */
+interface FetchTeamsAction extends AnyAction {
+  teamsById: { [key: string]: Team };
+  type: typeof TEAMS_FETCHED;
 }
 
-/** initial state for Organizations records in store */
-const initialOrganizationState: ImmutableOrganizationState = SeamlessImmutable({
-  organizationsById: {},
+/** interface for action that removes teams from store */
+interface RemoveTeamsAction extends AnyAction {
+  teamsById: { [key: string]: Team };
+  type: typeof REMOVE_TEAMS;
+}
+
+/** interface for Teams state in store */
+interface TeamState {
+  teamsById: { [key: string]: Team };
+}
+
+/** initial state for Teams records in store */
+const initialTeamState: ImmutableTeamState = SeamlessImmutable({
+  teamsById: {},
 });
 
 /** single type for all action types */
-type OrganizationActionTypes = AnyAction;
+type TeamActionTypes = FetchTeamsAction | RemoveTeamsAction | AnyAction;
 
-// immutable organization state in dux
-export type ImmutableOrganizationState = OrganizationState &
-  SeamlessImmutable.ImmutableObject<OrganizationState>;
+// immutable team state in dux
+export type ImmutableTeamState = TeamState & SeamlessImmutable.ImmutableObject<TeamState>;
 
-/** the Organization reducer function */
-export default function reducer(state = initialOrganizationState, action: OrganizationActionTypes) {
+/** the Team reducer function */
+export default function reducer(state = initialTeamState, action: TeamActionTypes) {
   switch (action.type) {
+    case TEAMS_FETCHED:
+      return SeamlessImmutable({
+        ...state,
+        teamsById: { ...state.teamsById, ...action.teamsById },
+      });
+    case REMOVE_TEAMS:
+      return SeamlessImmutable({
+        ...state,
+        teamsById: action.teamsById,
+      });
     default:
       return state;
   }
 }
 
+// actions
+
+/** action to remove teams form store */
+export const removeTeamsAction: RemoveTeamsAction = {
+  teamsById: {},
+  type: REMOVE_TEAMS,
+};
+
+// action creators
+
+/** creates action to add fetched teams to store
+ * @param {Team []} teamsList - array of teams to be added to store
+ *
+ * @returns {FetchTeamsAction} - action with teams payload that is added to store
+ */
+export const fetchTeams = (teamsList: Team[]): FetchTeamsAction => {
+  return {
+    teamsById: keyBy(teamsList, team => team.identifier),
+    type: TEAMS_FETCHED,
+  };
+};
+
 // selectors
 
-/** Get single Organization by the given id
- * @param {Partial<Store>} state - Part of the redux store
- * @param {string} organizationId - filters organization data to be returned based on this id
+/** get teams as an object where their ids are the keys and the objects
+ * the values
+ * @param {Partial<Store>} state - Portion of the store
  *
- * @return {Organization | null} - A organization object if found else null
+ * @return {[key: string]: Team}
  */
-export function getOrganizationById(
-  state: Partial<Store>,
-  organizationId: string
-): Organization | null {
-  return get((state as any)[reducerName].organizationsById, organizationId) || null;
+export function getTeamsById(state: Partial<Store>): { [key: string]: Team } {
+  return (state as any)[reducerName].teamsById;
+}
+
+/** Get single Team by the given id
+ * @param {Partial<Store>} state - Part of the redux store
+ * @param {string} teamId - filters team data to be returned based on this id
+ *
+ * @return {Team | null} - A team object if found else null
+ */
+export function getTeamById(state: Partial<Store>, teamId: string): Team | null {
+  return get(getTeamsById(state), teamId) || null;
+}
+
+/** Get all teams as an array
+ * @param {Partial<Store>} state - Part of the redux store
+ *
+ * @return {Team []} - all teams in store as an array
+ */
+export function getTeamsArray(state: Partial<Store>): Team[] {
+  return values(getTeamsById(state));
 }
