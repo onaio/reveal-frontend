f07b6a1f4d2622ea4c44502d259d61d0675c6aa5
cKellyDesign <ckelly@ona.io>
2019-08-29 09:04:02 -0700

    Use correct URL in IRS Plan tests

--- src/containers/pages/InterventionPlan/IRS/plan/tests/index.test.tsx
+++ src/containers/pages/InterventionPlan/IRS/plan/tests/index.test.tsx
@@ -4,7 +4,7 @@ import { createBrowserHistory } from 'history';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router';
-import { FI_SINGLE_URL } from '../../../../../../constants';
+import { INTERVENTION_IRS_URL } from '../../../../../../constants';
 import store from '../../../../../../store';
 import * as fixtures from '../../../../../../store/ducks/tests/fixtures';
 
@@ -26,8 +26,8 @@ describe('containers/pages/IRS/plan', () => {
       match: {
         isExact: true,
         params: { id: '1234' },
-        path: `${FI_SINGLE_URL}/plan/:id`,
-        url: `${FI_SINGLE_URL}/plan/1234`,
+        path: `${INTERVENTION_IRS_URL}/plan/:id`,
+        url: `${INTERVENTION_IRS_URL}/plan/1234`,
       },
       planId: '1234',
     };
@@ -47,8 +47,8 @@ describe('containers/pages/IRS/plan', () => {
       match: {
         isExact: true,
         params: { id },
-        path: `${FI_SINGLE_URL}/plan/:id`,
-        url: `${FI_SINGLE_URL}/plan/${id}`,
+        path: `${INTERVENTION_IRS_URL}/plan/:id`,
+        url: `${INTERVENTION_IRS_URL}/plan/${id}`,
       },
     };
     const wrapper = mount(
