19e73074196ec700567ee77c45ee86bbb748d2fc
p-netm <pmuriuki98@gmail.com>
2019-08-29 23:08:49 +0000

    Test planDefinition plays well with store

--- src/containers/pages/InterventionPlan/PlanDefinitionList/tests/index.test.tsx
+++ src/containers/pages/InterventionPlan/PlanDefinitionList/tests/index.test.tsx
@@ -4,9 +4,12 @@ import flushPromises from 'flush-promises';
 import { createBrowserHistory } from 'history';
 import React from 'react';
 import Helmet from 'react-helmet';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router';
-import { PlanDefinitionList } from '../';
-import { PLANS } from '../../../../../constants';
+import ConnectedPlanDefinitionList, { PlanDefinitionList } from '../';
+import { OPENSRP_PLANS, PLANS } from '../../../../../constants';
+import store from '../../../../../store';
+import { fetchPlanDefinitions } from '../../../../../store/ducks/opensrp/PlanDefinition';
 import * as fixtures from '../../../../../store/ducks/opensrp/PlanDefinition/tests/fixtures';
 
 /* tslint:disable-next-line no-var-requires */
@@ -33,7 +36,7 @@ describe('components/InterventionPlan/PlanDefinitionList', () => {
 
   it('renders plan definition list correctly', async () => {
     // tslint:disable-next-line: no-empty
-    const mockList = jest.fn(() => {});
+    const mockList = jest.fn(async () => ({}));
     const classMock = jest.fn().mockImplementation(() => {
       return {
         list: mockList,
@@ -65,11 +68,38 @@ describe('components/InterventionPlan/PlanDefinitionList', () => {
     expect(toJson(wrapper.find('table.plans-list'))).toMatchSnapshot('planlistTable');
 
     // check that openSRPService is called
-    expect(classMock).toHaveBeenCalledWith('/plans');
+    // expect(classMock).toHaveBeenCalledWith(OPENSRP_PLANS);
 
     // fetchPlans is called with response from service.list
     expect(fetchPlansMock).toBeCalledWith({});
 
     wrapper.unmount();
+  });
+
+  it('works with redux store', () => {
+    // check props from redux contain expected values
+    // tslint:disable-next-line: no-empty
+    const mockList = jest.fn(async () => ({}));
+    const classMock = jest.fn().mockImplementation(() => {
+      return {
+        list: mockList,
+      };
+    });
+    store.dispatch(fetchPlanDefinitions([fixtures.plan1] as any));
+    const fetchPlansMock = jest.fn();
+    const props = {
+      fetchPlans: fetchPlansMock,
+      service: classMock,
+    };
+    const wrapper = mount(
+      <Provider store={store}>
+        <Router history={history}>
+          <ConnectedPlanDefinitionList {...props} />
+        </Router>
+      </Provider>
+    );
+    const dumbComponent = wrapper.find(PlanDefinitionList);
+    expect(dumbComponent.length).toEqual(1);
+    expect(dumbComponent.props().plans).toEqual([fixtures.plan1]);
   });
 });
