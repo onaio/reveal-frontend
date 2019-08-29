1d11da106095c64ecdf3b1b0e7545dac392ed700
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-08-29 14:41:39 +0300

    Add the ability to change link text
    This is so that you can have different words on the link based on props.

--- src/components/LinkToNewPlans/index.tsx
+++ src/components/LinkToNewPlans/index.tsx
@@ -5,18 +5,23 @@ import { NEW_PLAN_URL } from '../../constants';
 /** interface for LinkToNewPlans component props */
 export interface Props {
   classProp: string;
+  text: string;
 }
 
 /** LinkToNewPlans component */
 const LinkToNewPlans = (props: Props) => {
+  const { text } = props;
   return (
     <Link to={NEW_PLAN_URL} className={props.classProp}>
-      Add Focus Investigation
+      {text}
     </Link>
   );
 };
 
-const defaultProps: Props = { classProp: 'focus-investigation btn btn-primary float-right mt-5' };
+const defaultProps: Props = {
+  classProp: 'focus-investigation btn btn-primary float-right mt-5',
+  text: 'Add Focus Investigation',
+};
 LinkToNewPlans.defaultProps = defaultProps;
 
 export default LinkToNewPlans;
