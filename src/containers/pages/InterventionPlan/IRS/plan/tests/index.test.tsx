17a98355e914a8e9443f3ccd5edfe5d408899c7a
cKellyDesign <ckelly@ona.io>
2019-09-04 09:51:42 -0700

    Remove commented code from IRS plan tests

--- src/containers/pages/InterventionPlan/IRS/plan/tests/index.test.tsx
+++ src/containers/pages/InterventionPlan/IRS/plan/tests/index.test.tsx
@@ -9,15 +9,12 @@ import store from '../../../../../../store';
 import * as fixtures from '../../../../../../store/ducks/tests/fixtures';
 import ConnectedIrsPlan, { IrsPlan } from './..';
 
-// const fetch = require('jest-fetch-mock');
-
 jest.mock('../../../../../../configs/env');
 const history = createBrowserHistory();
 
 describe('containers/pages/IRS/plan', () => {
   beforeEach(() => {
     jest.resetAllMocks();
-    // fetch.resetMocks();
   });
 
   it('renders without crashing', () => {
@@ -64,31 +61,4 @@ describe('containers/pages/IRS/plan', () => {
     expect(toJson(wrapper)).toMatchSnapshot();
     wrapper.unmount();
   });
-
-  // it('loads planById from Plans store', () => {});
-  // it('loads entire location tree', () => {});
-  // it('fetches jurisdiction geojson when necessary', () => {});
-  // it('defines child jursidictions of all parent jurisdictions', () => {});
-  // it('renders a gisida Map with correct layers', () => {});
-  // it('renders a drilldown table with correct hierarchy', () => {});
 });
-
-// describe('containers/pages/IRS/plan - load draft plan', () => {
-//   beforeEach(() => {
-//     jest.resetAllMocks();
-//     fetch.resetMocks();
-//   });
-//   it ('uses tilesets as gisida layer sources', () => {});
-//   it('fetches the correct level 0 jurisdiction from opensrp', () => {});
-//   it('determines which jurisdictions are selected in the plan', () => {});
-// });
-
-// describe('containers/pages/IRS/plan - jurisdiction hierarchy', () => {
-//   beforeEach(() => {
-//     jest.resetAllMocks();
-//     fetch.resetMocks();
-//   });
-//   it('gets correct descendant jurisdictions', () => {});
-//   it('gets correct ancestor jurisdictions', () => {});
-//   it('correctly toggles jurisdiction selection', () => {});
-// });
