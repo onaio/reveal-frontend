5641546d0aeb3b76e64c312a21a5d83d9acc0689
cKellyDesign <ckelly@ona.io>
2019-09-10 14:05:27 -0700

    Add connection tests for IRS reporting table view

--- src/containers/pages/InterventionPlan/IRS/report/tests/index.test.tsx
+++ src/containers/pages/InterventionPlan/IRS/report/tests/index.test.tsx
@@ -1,17 +1,31 @@
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import { createBrowserHistory } from 'history';
 import MockDate from 'mockdate';
 import React from 'react';
 import { Router } from 'react-router';
-import { irsPlanDefinition1, irsPlanRecord1 } from '../../tests/fixtures';
+import {
+  childrenByParentId,
+  irsPlanDefinition1,
+  irsPlanRecord1,
+  jurisdictionIdsByPlanId,
+  jurisdictionsArray,
+  jurisdictionsById,
+} from '../../tests/fixtures';
 
+import { Provider } from 'react-redux';
 import { REPORT_IRS_PLAN_URL } from '../../../../../../constants';
+import store from '../../../../../../store';
+import {
+  fetchChildrenByParentId,
+  fetchJurisdictionIdsByPlanId,
+  fetchJurisdictions,
+} from '../../../../../../store/ducks/jurisdictions';
 import {
   extractPlanRecordFromPlanPayload,
   PlanPayload,
   PlanRecord,
 } from '../../../../../../store/ducks/plans';
-import { defaultIrsReportProps, IrsReport } from '../../report';
+import ConnectedIrsReport, { defaultIrsReportProps, IrsReport, IrsReportProps } from '../../report';
 
 const history = createBrowserHistory();
 jest.mock('../../../../../../configs/env');
@@ -53,5 +67,51 @@ describe('containers/pages/IrsReportList', () => {
         <IrsReport {...routeProps} {...ownProps} />
       </Router>
     );
+  });
+
+  it('renders the IRS Reporting Drilldown Table correctly', () => {
+    const mock: any = jest.fn();
+    // mock.mockImplementation(() => Promise.resolve(jurisdictionsArray));
+
+    const routeProps = {
+      history,
+      location: mock,
+      match: {
+        isExact: true,
+        params: { id: irsPlanDefinition1.identifier },
+        path: `${REPORT_IRS_PLAN_URL}/:id`,
+        url: `${REPORT_IRS_PLAN_URL}/0230f9e8-1f30-5e91-8693-4c993661785e`,
+      },
+    };
+
+    const planById: PlanRecord | null = extractPlanRecordFromPlanPayload(
+      irsPlanDefinition1 as PlanPayload
+    );
+    const ownProps: IrsReportProps = {
+      ...defaultIrsReportProps,
+      childrenByParentId: { ...childrenByParentId },
+      jurisdictionIdsByPlanId: { ...jurisdictionIdsByPlanId },
+      jurisdictionsById: { ...jurisdictionsById },
+      planById,
+      planId: irsPlanDefinition1.identifier,
+    };
+
+    store.dispatch(fetchJurisdictions(jurisdictionsArray));
+    store.dispatch(fetchChildrenByParentId(childrenByParentId));
+    store.dispatch(fetchJurisdictionIdsByPlanId(jurisdictionIdsByPlanId));
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <Router history={history}>
+          <IrsReport {...routeProps} {...ownProps} />
+        </Router>
+      </Provider>
+    );
+
+    expect(wrapper.find('h2.page-title')).toMatchSnapshot();
+    expect(wrapper.find('nav.reveal-breadcrumb')).toMatchSnapshot();
+    expect(wrapper.find('.table-bread-crumbs')).toMatchSnapshot();
+    expect(wrapper.find('.ReactTable .rt-thead.-headerGroups')).toMatchSnapshot();
+    expect(wrapper.find('.ReactTable .rt-tbody')).toMatchSnapshot();
   });
 });
