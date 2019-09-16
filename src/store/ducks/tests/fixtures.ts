df1b92c346af8c747b3a05f179122f81c2fbdf3e
p-netm <pmuriuki98@gmail.com>
2019-09-16 10:39:53 +0300

    Refactor teams fixture data

--- src/store/ducks/tests/fixtures.ts
+++ src/store/ducks/tests/fixtures.ts
@@ -1176,15 +1176,43 @@ export const CompleteRoutineTableProps = {
 };
 
 export const team1: Team = {
-  identifier: '1',
-  jurisdictions: [],
-  name: 'User_45',
+  active: true,
+  id: 1,
+  identifier: 'fcc19470-d599-11e9-bb65-2a2ae2dbcce4',
+  name: 'The Luang',
+  type: {
+    coding: [
+      {
+        code: 'team',
+        display: 'Team',
+        system: 'http://terminology.hl7.org/CodeSystem/organization-type',
+      },
+    ],
+  },
 };
 
 export const team2: Team = {
-  identifier: '2',
-  jurisdictions: [],
-  name: 'South',
+  active: true,
+  id: 3,
+  identifier: '4c506c98-d3a9-11e9-bb65-2a2ae2dbcce4',
+  name: 'Demo Team',
+};
+
+export const team3: Team = {
+  active: true,
+  id: 2,
+  identifier: 'd23f7350-d406-11e9-bb65-2a2ae2dbcce4',
+  name: 'Takang 1',
+  partOf: 1,
+  type: {
+    coding: [
+      {
+        code: 'team',
+        display: 'Team',
+        system: 'http://terminology.hl7.org/CodeSystem/organization-type',
+      },
+    ],
+  },
 };
 
 export const teams: Team[] = [team1, team2];
