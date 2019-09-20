913fff2af315ac77fbd703bb3fc69872fbc1b383
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-09-20 18:30:39 +0300

    Start playing with example data

--- src/containers/pages/IRS/JurisdictionsReport/index.tsx
+++ src/containers/pages/IRS/JurisdictionsReport/index.tsx
@@ -1,5 +1,6 @@
 // this is the FocusInvestigation "historical" page component
 import DrillDownTable from '@onaio/drill-down-table';
+import superset from '@onaio/superset-connector';
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
 import { RouteComponentProps } from 'react-router';
@@ -15,18 +16,43 @@ import { HOME, HOME_URL, IRS_PLANS, IRS_REPORTS_URL } from '../../../../constant
 import { getFIAdherenceIndicator, renderClassificationRow } from '../../../../helpers/indicators';
 import '../../../../helpers/tables.css';
 import { FlexObject, percentage, RouteParams } from '../../../../helpers/utils';
+import supersetFetch from '../../../../services/superset';
+import * as fixtures from './tests/fixtures';
 
 export interface IRSJurisdictionProps {
-  country: string;
+  service: typeof supersetFetch;
 }
+
+const data = superset.processData(JSON.parse(fixtures.supersetJSON)) || [];
 
 /** Renders IRS Jurisdictions reports */
 const IRSJurisdictions = (props: IRSJurisdictionProps & RouteComponentProps<RouteParams>) => {
-  return <div>xx</div>;
+  const id = null;
+
+  const tableProps = {
+    CellComponent: DrillDownTableLinkedCell,
+    data,
+    extraCellProps: { urlPath: IRS_REPORTS_URL },
+    identifierField: 'jurisdiction_name',
+    linkerField: 'jurisdiction_name',
+    minRows: 0,
+    parentIdentifierField: 'jurisdiction_parent_id',
+    rootParentId: id || null,
+    shouldUseEffect: true,
+    showPageSizeOptions: true,
+    showPagination: true,
+    useDrillDownTrProps: false,
+  };
+
+  return (
+    <div>
+      <DrillDownTable {...tableProps} />
+    </div>
+  );
 };
 
 const defaultProps: IRSJurisdictionProps = {
-  country: 'Namibia',
+  service: supersetFetch,
 };
 
 IRSJurisdictions.defaultProps = defaultProps;
