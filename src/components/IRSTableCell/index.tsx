deeed9ae31227303c450ec4dd1e414db7d3ff0f5
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-09-24 21:17:10 +0300

    Render link and map icon correctly

--- src/components/IRSTableCell/index.tsx
+++ src/components/IRSTableCell/index.tsx
@@ -1,6 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { DropDownCellProps } from '@onaio/drill-down-table';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { MAP } from '../../constants';
 
 /** Interface for linked cell props */
 export interface LinkedCellProps extends DropDownCellProps {
@@ -11,8 +13,22 @@ export interface LinkedCellProps extends DropDownCellProps {
  * that moves you to the next hierarchical level.
  */
 const IRSTableCell: React.ElementType<LinkedCellProps> = (props: LinkedCellProps) => {
-  const { cellValue, hasChildren, urlPath } = props;
-  return <div>{hasChildren ? <Link to={urlPath || ''}>{cellValue}</Link> : cellValue}</div>;
+  const { cell, cellValue, hasChildren, urlPath } = props;
+  const url = urlPath ? `${urlPath}/${cell.original.jurisdiction_id}` : '';
+  const val = hasChildren ? (
+    cellValue
+  ) : (
+    <span>
+      <span className="plan-jurisdiction-name btn-link">
+        <Link to={''}>
+          <FontAwesomeIcon icon={['fas', MAP]} />
+        </Link>
+        &nbsp;&nbsp;
+      </span>
+      {cellValue}
+    </span>
+  );
+  return <div>{hasChildren ? <Link to={url}>{cellValue}</Link> : val}</div>;
 };
 
 export default IRSTableCell;
