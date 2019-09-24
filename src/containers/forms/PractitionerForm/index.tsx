e3af542d1fd6373c2e015bdef5b45cb0e09317e0
p-netm <pmuriuki98@gmail.com>
2019-09-24 15:12:10 +0300

    Add Practitioner form

---
+++ src/containers/forms/PractitionerForm/index.tsx
@@ -0,0 +1,185 @@
+import { faFileDownload } from '@fortawesome/free-solid-svg-icons';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router';
+import { valueContainerCSS } from 'react-select/src/components/containers';
+import { Button, Label } from 'reactstrap';
+import { FormGroup } from 'reactstrap';
+import * as Yup from 'yup';
+import {
+  ACTIVE,
+  ID,
+  NAME,
+  OPENSRP_PRACTITIONER_ENDPOINT,
+  PRACTITIONER,
+  REQUIRED,
+  SAVE,
+  SAVING,
+} from '../../../constants';
+import { OpenSRPService } from '../../../services/opensrp';
+
+export interface PractitionerFormFields {
+  identifier: string;
+  active: boolean;
+  name: string;
+  // userUuid: string; //TODO - add userid select field
+}
+
+/** Initial values for practitioner's form */
+export const defaultInitialValues = {
+  identifier: '',
+  name: '',
+};
+
+export const PractitionerSchema = Yup.object().shape({
+  active: Yup.boolean(),
+  identifier: Yup.string(),
+  name: Yup.string().required(REQUIRED),
+});
+
+/** interface for props to Practitioner form */
+export interface PractitionerFormProps {
+  initialValues: PractitionerFormFields;
+  disabledFields: string[];
+  redirectAfterAction: string;
+}
+
+const PractitionerForm = (props: PractitionerFormProps) => {
+  /** track when redirection from this form page should occur */
+  const [ifDoneHere, setIfDoneHere] = useState<boolean>(false);
+  const { initialValues, redirectAfterAction, disabledFields } = props;
+  const [globalError, setGlobalError] = useState<string>('');
+
+  /** edit mode set to true if we are updating a practitioner data. */
+  const editMode: boolean = initialValues.identifier !== '';
+
+  return (
+    <div className="form-container">
+      {ifDoneHere && <Redirect to={redirectAfterAction} />}
+      <Formik
+        initialValues={initialValues}
+        validationSchema={PractitionerSchema}
+        // tslint:disable-next-line: jsx-no-lambda
+        onSubmit={(values, { setSubmitting }) => {
+          const apiService = new OpenSRPService(OPENSRP_PRACTITIONER_ENDPOINT);
+
+          if (editMode) {
+            apiService
+              .update(values)
+              .then(() => {
+                setSubmitting(false);
+                setIfDoneHere(true);
+              })
+              .catch((e: Error) => {
+                setGlobalError(e.message);
+                setSubmitting(false);
+              });
+          } else {
+            apiService
+              .create(values)
+              .then(() => {
+                setSubmitting(false);
+                setIfDoneHere(true);
+              })
+              .catch((e: Error) => {
+                setGlobalError(e.message);
+                setSubmitting(false);
+              });
+          }
+        }}
+      >
+        {({ errors, isSubmitting, values, setFieldValue }) => (
+          <Form className="mb-5">
+            <FormGroup className="non-field-errors">
+              {globalError !== '' && <p className="form-text text-danger">{globalError}</p>}
+            </FormGroup>
+            <FormGroup>
+              <Label>{`${PRACTITIONER} ${ID}`}</Label>
+              <Field
+                type="text"
+                name="identifier"
+                id="identifier"
+                disabled={disabledFields.includes('identifier')}
+                className={errors.identifier ? `form-control is-invalid` : `form-control`}
+              />
+              <ErrorMessage
+                component="small"
+                name="identifier"
+                className="form-text text-danger identifier-error"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label>{`${NAME}`}</Label>
+              <Field
+                type="text"
+                name="name"
+                id="name"
+                disabled={disabledFields.includes('name')}
+                className={errors.name ? `form-control is-invalid` : `form-control`}
+              />
+              <ErrorMessage
+                name="name"
+                component="small"
+                className="form-text text-danger name-error"
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <Label>{`${ACTIVE}`}</Label>
+              <div className="btn-group btn-group-toggle" data-toggle="buttons">
+                <Field name="active">
+                  <Label class="btn btn-outline-primary">
+                    <input
+                      type="radio"
+                      name="active"
+                      id="yes"
+                      checked={values.active === true}
+                      // tslint:disable-next-line: jsx-no-lambda
+                      onChange={() => setFieldValue('active', true)}
+                    />{' '}
+                    yes
+                  </Label>
+                  <Label class="btn btn-outline-primary">
+                    <input
+                      type="radio"
+                      name="active"
+                      id="no"
+                      checked={values.active === true}
+                      // tslint:disable-next-line: jsx-no-lambda
+                      onChange={() => setFieldValue('active', false)}
+                    />{' '}
+                    no
+                  </Label>
+                </Field>
+              </div>
+
+              <Field
+                type="text"
+                name="name"
+                id="name"
+                disabled={disabledFields.includes('name')}
+                className={errors.name ? `form-control is-invalid` : `form-control`}
+              />
+              <ErrorMessage
+                name="active"
+                component="small"
+                className="form-text text-danger name-error"
+              />
+            </FormGroup>
+
+            <hr className="mb-2" />
+            <Button
+              type="submit"
+              id="practitioner-form-submit-button"
+              className="btn btn-block btn btn-primary"
+              aria-Label="Save Practitioner"
+              disabled={isSubmitting || Object.keys(errors).length > 0}
+            >
+              {isSubmitting ? SAVING : `${SAVE} ${PRACTITIONER}`}
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
