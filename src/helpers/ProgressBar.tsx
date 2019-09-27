561dbbe323cad9ac1c604cfc1edcb2ba91d05dfe
p-netm <pmuriuki98@gmail.com>
2019-09-27 15:33:51 +0300

    Add marker html and css to progressbar

--- src/helpers/ProgressBar.tsx
+++ src/helpers/ProgressBar.tsx
@@ -1,6 +1,15 @@
+import { Color } from 'csstype';
 import React from 'react';
 import * as colors from '../colors';
 import { GREEN_THRESHOLD, ORANGE_THRESHOLD, YELLOW_THRESHOLD, ZERO } from '../configs/settings';
+
+/** Marker? - a small vertical line on the progress bar that will visually inform of a certain limit */
+interface Marker {
+  height?: string /** customizable css style height */;
+  width?: string /** how fat do you want it to be */;
+  markAt: string /** percentage value as string; from left of progressbar that the marker should appear */;
+  color?: Color /** shat color should the marker be */;
+}
 
 /** Props for ProgressBar */
 interface ProgressBarProps {
@@ -9,6 +18,7 @@ interface ProgressBarProps {
   min: number;
   max: number;
   value: number;
+  marker: Marker | null;
 }
 
 /** default props for ProgressBar */
@@ -21,7 +31,7 @@ const defaultProgressBarProps: Partial<ProgressBarProps> = {
 
 /** Displays configurable progress bar */
 const ProgressBar = (props: ProgressBarProps) => {
-  const { decimalPoints, height, value } = props;
+  const { decimalPoints, height, value, marker } = props;
   const max = props.max || 100;
   const min = props.min || 0;
   let range = max - min;
@@ -31,8 +41,24 @@ const ProgressBar = (props: ProgressBarProps) => {
   const decimalValue = value / range;
   const percentValue = decimalValue * 100;
   const percentValueString = percentValue.toFixed(decimalPoints);
+
   return (
-    <div className="progress" style={{ height, marginBottom: '15px' }}>
+    <div className="progress" style={{ height, marginBottom: '15px', position: 'relative' }}>
+      {!!marker && (
+        <div
+          className="marker"
+          id="marker"
+          style={{
+            backgroundColor: marker.color ? marker.color : '#F00',
+            height: marker.height ? marker.height : 'inherit',
+            left: marker.markAt,
+            position: 'absolute',
+            width: marker.width ? marker.width : '5px',
+          }}
+        >
+          &nbsp;
+        </div>
+      )}
       <div
         className={`progress-bar`}
         style={{
