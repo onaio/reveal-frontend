d5b3da170e941b54aaf86817ae058aee139331e3
p-netm <pmuriuki98@gmail.com>
2019-09-27 16:05:18 +0300

    Refactor marker implementation so it can render above bar

--- src/helpers/ProgressBar.tsx
+++ src/helpers/ProgressBar.tsx
@@ -9,6 +9,7 @@ interface Marker {
   width?: string /** how fat do you want it to be */;
   markAt: string /** percentage value as string; from left of progressbar that the marker should appear */;
   color?: Color /** shat color should the marker be */;
+  top?: string /** how far up the marker should be rendered relative to top of progressbar */;
 }
 
 /** Props for ProgressBar */
@@ -43,40 +44,44 @@ const ProgressBar = (props: ProgressBarProps) => {
   const percentValueString = percentValue.toFixed(decimalPoints);
 
   return (
-    <div className="progress" style={{ height, marginBottom: '15px', position: 'relative' }}>
+    <div className="marked-progress-bar" style={{ position: 'relative' }}>
       {!!marker && (
         <div
           className="marker"
           id="marker"
           style={{
             backgroundColor: marker.color ? marker.color : '#F00',
-            height: marker.height ? marker.height : 'inherit',
+            height: marker.height ? marker.height : '25px',
             left: marker.markAt,
             position: 'absolute',
+            top: marker.top ? marker.top : '-5px',
             width: marker.width ? marker.width : '5px',
+            zIndex: 1,
           }}
         >
           &nbsp;
         </div>
       )}
-      <div
-        className={`progress-bar`}
-        style={{
-          backgroundColor:
-            decimalValue >= GREEN_THRESHOLD
-              ? colors.GREEN
-              : decimalValue >= ORANGE_THRESHOLD
-              ? colors.ORANGE
-              : decimalValue >= YELLOW_THRESHOLD
-              ? colors.RED
-              : colors.YELLOW,
-          width: `${percentValueString}%`,
-        }}
-        role="progressbar"
-        aria-valuenow={percentValue}
-        aria-valuemin={min}
-        aria-valuemax={max}
-      />
+      <div className="progress" style={{ height, marginBottom: '15px', position: 'relative' }}>
+        <div
+          className={`progress-bar`}
+          style={{
+            backgroundColor:
+              decimalValue >= GREEN_THRESHOLD
+                ? colors.GREEN
+                : decimalValue >= ORANGE_THRESHOLD
+                ? colors.ORANGE
+                : decimalValue >= YELLOW_THRESHOLD
+                ? colors.RED
+                : colors.YELLOW,
+            width: `${percentValueString}%`,
+          }}
+          role="progressbar"
+          aria-valuenow={percentValue}
+          aria-valuemin={min}
+          aria-valuemax={max}
+        />
+      </div>
     </div>
   );
 };
