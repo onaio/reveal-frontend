799e9549d70001624a518d527683098a82808f8d
p-netm <pmuriuki98@gmail.com>
2019-09-29 20:32:25 +0000

    Add inlineSearchform component

---
+++ src/components/InlineSearchForm/index.tsx
@@ -0,0 +1,33 @@
+/** renders a form inline, containing search field and search button */
+import React, { FormEvent } from 'react';
+import { Button, Form, FormGroup, Input } from 'reactstrap';
+import { SEARCH } from '../../constants';
+
+export interface Props {
+  handleSubmit?: (e: FormEvent) => void;
+  inputId: string;
+  inputPlaceholder: string;
+}
+
+const defaultProps: Props = {
+  inputId: 'search',
+  inputPlaceholder: 'Search',
+};
+
+const InlineSearchForm = (props: Props) => {
+  const { handleSubmit, inputPlaceholder, inputId } = props;
+  return (
+    <Form inline={true} onSubmit={handleSubmit}>
+      <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+        <Input type="text" name="search" id={inputId} placeholder={inputPlaceholder} />
+      </FormGroup>
+      <Button type="submit" outline={true} color="success">
+        {SEARCH}
+      </Button>
+    </Form>
+  );
+};
+
+InlineSearchForm.defaultProps = defaultProps;
+
+export default InlineSearchForm;
