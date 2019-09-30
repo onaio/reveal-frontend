17384a03d32241ec9b7c1e73bd23ba89f068e477
p-netm <pmuriuki98@gmail.com>
2019-09-30 12:06:50 +0300

    Add local fixtures file for the organizationListview component

---
+++ src/containers/pages/OrganizationViews/OrganizationListView/tests/fixtures.tsx
@@ -0,0 +1,20 @@
+import { organization1, organization2 } from '../../../../../store/ducks/tests/fixtures';
+
+export const state = {
+  gatekeeper: { result: {}, success: null },
+  organizations: {
+    organizationsById: {
+      '4c506c98-d3a9-11e9-bb65-2a2ae2dbcce4': organization2,
+      'fcc19470-d599-11e9-bb65-2a2ae2dbcce4': organization1,
+    },
+  },
+  router: {
+    action: 'POP',
+    location: { hash: '', pathname: '/', search: '', state: undefined },
+  },
+  session: {
+    authenticated: false,
+    extraData: {},
+    user: { email: '', gravatar: '', name: '', username: '' },
+  },
+};
