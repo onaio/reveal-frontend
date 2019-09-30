a0f0a4605c2893348ca277908d530ae3850156d7
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-09-30 15:36:22 +0300

    Add tests for drilling down

--- src/containers/pages/IRS/JurisdictionsReport/tests/index.test.tsx
+++ src/containers/pages/IRS/JurisdictionsReport/tests/index.test.tsx
@@ -8,7 +8,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { Router } from 'react-router';
 import { JurisdictionReport } from '../';
-import { INTERVENTION_IRS_URL, IRS_REPORTING_TITLE, MAP } from '../../../../../constants';
+import { REPORT_IRS_PLAN_URL } from '../../../../../constants';
 import store from '../../../../../store';
 import GenericJurisdictionsReducer, {
   fetchGenericJurisdictions,
@@ -86,8 +86,8 @@ describe('components/IRS Reports/JurisdictionReport', () => {
         params: {
           planId: (plans[0] as IRSPlan).plan_id,
         },
-        path: `${INTERVENTION_IRS_URL}/:planId`,
-        url: `${INTERVENTION_IRS_URL}/${(plans[0] as IRSPlan).plan_id}`,
+        path: `${REPORT_IRS_PLAN_URL}/:planId`,
+        url: `${REPORT_IRS_PLAN_URL}/${(plans[0] as IRSPlan).plan_id}`,
       },
       plan: plans[0] as IRSPlan,
     };
@@ -128,8 +128,8 @@ describe('components/IRS Reports/JurisdictionReport', () => {
         params: {
           planId: (plans[0] as IRSPlan).plan_id,
         },
-        path: `${INTERVENTION_IRS_URL}/:planId`,
-        url: `${INTERVENTION_IRS_URL}/${(plans[0] as IRSPlan).plan_id}`,
+        path: `${REPORT_IRS_PLAN_URL}/:planId`,
+        url: `${REPORT_IRS_PLAN_URL}/${(plans[0] as IRSPlan).plan_id}`,
       },
       plan: plans[0] as IRSPlan,
       service: supersetServiceMock,
@@ -200,5 +200,178 @@ describe('components/IRS Reports/JurisdictionReport', () => {
     ]);
     expect(supersetServiceMock).toHaveBeenCalledTimes(3);
     wrapper.unmount();
+  });
+
+  it('drills down correctly', async () => {
+    fetch.mockResponseOnce(JSON.stringify({}));
+    const mock: any = jest.fn();
+
+    const supersetServiceMock: any = jest.fn();
+    supersetServiceMock.mockImplementation(async () => []);
+
+    let jurisdictions =
+      getGenericJurisdictionsArray(
+        store.getState(),
+        'zm-jurisdictions',
+        '9f1e0cfa-5313-49ff-af2c-f7dbf4fbdb9d'
+      ) || [];
+    jurisdictions = jurisdictions.concat(
+      getGenericJurisdictionsArray(
+        store.getState(),
+        'zm-focusAreas',
+        '9f1e0cfa-5313-49ff-af2c-f7dbf4fbdb9d'
+      )
+    );
+
+    const props = {
+      history,
+      jurisdictions,
+      location: mock,
+      match: {
+        isExact: true,
+        params: {
+          planId: (plans[0] as IRSPlan).plan_id,
+        },
+        path: `${REPORT_IRS_PLAN_URL}/:planId`,
+        url: `${REPORT_IRS_PLAN_URL}/${(plans[0] as IRSPlan).plan_id}`,
+      },
+      plan: plans[0] as IRSPlan,
+      service: supersetServiceMock,
+    };
+
+    const wrapper = mount(
+      <Router history={history}>
+        <JurisdictionReport {...props} />
+      </Router>
+    );
+
+    const baseURL = `${REPORT_IRS_PLAN_URL}/727c3d40-e118-564a-b231-aac633e6abce`;
+
+    expect(wrapper.find('HeaderBreadcrumb li').length).toEqual(3);
+    expect(
+      wrapper
+        .find('HeaderBreadcrumb li')
+        .first()
+        .text()
+    ).toEqual('Home');
+    expect(
+      wrapper
+        .find('HeaderBreadcrumb a')
+        .last()
+        .text()
+    ).toEqual('IRS Reporting');
+    expect(
+      wrapper
+        .find('HeaderBreadcrumb li')
+        .last()
+        .text()
+    ).toEqual('IRS 2019-09-05 TEST');
+    expect(wrapper.find('HeaderBreadcrumb a').length).toEqual(2);
+    expect(toJson(wrapper.find('h3.page-title'))).toMatchSnapshot();
+
+    // Zambia URL
+    expect(
+      wrapper.find(`a[href$="${baseURL}/22bc44dd-752d-4c20-8761-617361b4f1e7"]`).length
+    ).toEqual(1);
+    wrapper
+      .find(`a[href$="${baseURL}/22bc44dd-752d-4c20-8761-617361b4f1e7"]`)
+      .first()
+      .simulate('click', { button: 0 });
+    expect(wrapper.find('HeaderBreadcrumb li').length).toEqual(4);
+    expect(wrapper.find('HeaderBreadcrumb a').length).toEqual(3);
+    expect(
+      wrapper
+        .find('HeaderBreadcrumb a')
+        .last()
+        .text()
+    ).toEqual('IRS 2019-09-05 TEST');
+    expect(
+      wrapper
+        .find('HeaderBreadcrumb li')
+        .last()
+        .text()
+    ).toEqual('Zambia');
+
+    // Lusaka URL
+    expect(
+      wrapper.find(`a[href$="${baseURL}/fd5a22ba-fa0e-4fdc-ae1c-1db8965b7b43"]`).length
+    ).toEqual(1);
+    wrapper
+      .find(`a[href$="${baseURL}/fd5a22ba-fa0e-4fdc-ae1c-1db8965b7b43"]`)
+      .first()
+      .simulate('click', { button: 0 });
+    expect(wrapper.find('HeaderBreadcrumb li').length).toEqual(5);
+    expect(wrapper.find('HeaderBreadcrumb a').length).toEqual(4);
+    expect(
+      wrapper
+        .find('HeaderBreadcrumb a')
+        .last()
+        .text()
+    ).toEqual('Zambia');
+    expect(
+      wrapper
+        .find('HeaderBreadcrumb li')
+        .last()
+        .text()
+    ).toEqual('Lusaka');
+
+    // Lusaka Test
+    expect(
+      wrapper.find(`a[href$="${baseURL}/8f1fd4cf-264d-4ed6-8838-e538c5845d46"]`).length
+    ).toEqual(1);
+    wrapper
+      .find(`a[href$="${baseURL}/8f1fd4cf-264d-4ed6-8838-e538c5845d46"]`)
+      .first()
+      .simulate('click', { button: 0 });
+    expect(wrapper.find('HeaderBreadcrumb li').length).toEqual(6);
+    expect(wrapper.find('HeaderBreadcrumb a').length).toEqual(5);
+    expect(
+      wrapper
+        .find('HeaderBreadcrumb a')
+        .last()
+        .text()
+    ).toEqual('Lusaka');
+    expect(
+      wrapper
+        .find('HeaderBreadcrumb li')
+        .last()
+        .text()
+    ).toEqual('Lusaka_Test');
+
+    // Lusaka HFC
+    expect(
+      wrapper.find(`a[href$="${baseURL}/2bf9915d-8725-4061-983d-5938802ac0f0"]`).length
+    ).toEqual(1);
+    wrapper
+      .find(`a[href$="${baseURL}/2bf9915d-8725-4061-983d-5938802ac0f0"]`)
+      .first()
+      .simulate('click', { button: 0 });
+    expect(wrapper.find('HeaderBreadcrumb li').length).toEqual(7);
+    expect(wrapper.find('HeaderBreadcrumb a').length).toEqual(6);
+    expect(
+      wrapper
+        .find('HeaderBreadcrumb a')
+        .last()
+        .text()
+    ).toEqual('Lusaka_Test');
+    expect(
+      wrapper
+        .find('HeaderBreadcrumb li')
+        .last()
+        .text()
+    ).toEqual('Lusaka HFC');
+
+    expect(wrapper.find('DrillDownTable a').length).toEqual(7);
+    expect(
+      wrapper.find('DrillDownTable .plan-jurisdiction-name.main-span').map(e => e.text())
+    ).toEqual([
+      '  Akros 3',
+      '  Akros_2',
+      '  Ibex_1',
+      '  Kalikiliki Settlement',
+      '  Akros_1',
+      '  Bauleni Settlement',
+      '  Helen Kaunda Settlement',
+    ]);
   });
 });
