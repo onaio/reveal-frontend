214b226c6e4988fbb91a023837c4b0000d194c8f
p-netm <pmuriuki98@gmail.com>
2019-10-06 13:18:09 +0300

    Add EDIT to constants

--- src/constants.tsx
+++ src/constants.tsx
@@ -196,6 +196,7 @@ export const EDIT = 'Edit';
 export type EDIT = typeof EDIT;
 export const ADD = 'Add';
 export type ADD = typeof ADD;
+
 // TODO ? - do the below 2 belong here or in a settings file
 export const ORGANIZATION_LABEL = TEAM;
 export type ORGANIZATION_LABEL = typeof ORGANIZATION_LABEL;
