ddd272eba5b7bb7b6341c4516184102b43aef008
p-netm <pmuriuki98@gmail.com>
2019-10-07 15:49:08 +0300

    Add route to assign Practitioners view

--- src/constants.tsx
+++ src/constants.tsx
@@ -302,6 +302,8 @@ export const ASSIGN_ORGANIZATION_URL = '/plans/teamAssignment';
 export type ASSIGN_ORGANIZATION_URL = typeof ASSIGN_ORGANIZATION_URL;
 export const PRACTITIONERS_LIST_URL = '/practitioners';
 export type PRACTITIONERS_LIST_URL = typeof PRACTITIONERS_LIST_URL;
+export const ASSIGN_PRACTITIONERS_URL = '/teams/assignPractitioners';
+export type ASSIGN_PRACTITIONERS_URL = typeof ASSIGN_ORGANIZATION_URL;
 
 // OpenSRP API strings
 export const OPENSRP_PRACTITIONER_ENDPOINT = 'practitioner';
