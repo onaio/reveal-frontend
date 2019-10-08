9ac7803333966212f2067a0150a725a744eaad96
p-netm <pmuriuki98@gmail.com>
2019-10-08 12:52:02 +0300

    Move async select styles tweaking object to utils

---
+++ src/containers/pages/OrganizationViews/AssignPractitioners/utils.ts
@@ -0,0 +1,14 @@
+/** custom styling for fixed options */
+export const styles = {
+  multiValue: (base: any, state: any) => {
+    return state.data.isFixed ? { ...base, backgroundColor: 'gray' } : base;
+  },
+  multiValueLabel: (base: any, state: any) => {
+    return state.data.isFixed
+      ? { ...base, fontWeight: 'bold', color: 'white', paddingRight: 6 }
+      : base;
+  },
+  multiValueRemove: (base: any, state: any) => {
+    return state.data.isFixed ? { ...base, display: 'none' } : base;
+  },
+};
