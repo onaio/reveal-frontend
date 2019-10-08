002f307fe52733f5ea358ea1ab4db98c183a053b
cKellyDesign <ckelly@ona.io>
2019-10-07 18:47:59 -0700

    Add buttons to save and clear assignments in the store

--- src/components/AssignTeamTableCell/index.tsx
+++ src/components/AssignTeamTableCell/index.tsx
@@ -9,6 +9,7 @@ import OrganizationSelect from '../../containers/forms/OrganizationSelect';
 import { stopPropagationAndPreventDefault } from '../../helpers/utils';
 import assignmentReducer, {
   Assignment,
+  fetchAssignments,
   getAssignmentsArrayByPlanIdByJurisdictionId,
   reducerName as assignmentReducerName,
 } from '../../store/ducks/opensrp/assignments';
@@ -25,6 +26,7 @@ reducerRegistry.register(organizationsReducerName, organizationsReducer);
 /** Interface for Assign Teams cell props */
 export interface AssignTeamCellProps {
   assignments: Assignment[];
+  fetchAssignmentsActionCreator: typeof fetchAssignments;
   jurisdictionId: string;
   organizationsById: { [key: string]: Organization } | null;
   planId: string;
@@ -34,10 +36,29 @@ export interface AssignTeamCellProps {
  * showing the button and card in the Assign Teams column
  */
 const AssignTeamTableCell = (props: AssignTeamCellProps) => {
-  const { assignments, jurisdictionId, organizationsById, planId } = props;
+  const {
+    assignments,
+    fetchAssignmentsActionCreator,
+    jurisdictionId,
+    organizationsById,
+    planId,
+  } = props;
   const [isActive, setIsActive] = useState<boolean>(false);
 
   const onPlanAssignmentButtonClick = (e: MouseEvent) => {
+    stopPropagationAndPreventDefault(e);
+    setIsActive(!isActive);
+  };
+
+  const onClearAssignmentsButtonClick = (e: MouseEvent) => {
+    stopPropagationAndPreventDefault(e);
+    const nextAssignments = assignments.filter(
+      (a: Assignment) => a.jurisdiction !== jurisdictionId
+    );
+    fetchAssignmentsActionCreator(nextAssignments);
+  };
+
+  const onSaveAssignmentsButtonClick = (e: MouseEvent) => {
     stopPropagationAndPreventDefault(e);
     setIsActive(!isActive);
   };
@@ -63,7 +84,6 @@ const AssignTeamTableCell = (props: AssignTeamCellProps) => {
     jurisdictionId,
     name: getFormName(jurisdictionId),
     planId,
-    values: assignments.map((a: Assignment) => a.organization),
   };
 
   const popoverProps = {
@@ -72,14 +92,20 @@ const AssignTeamTableCell = (props: AssignTeamCellProps) => {
     target: getButtonId(jurisdictionId),
     toggle: () => setIsActive(!isActive),
   };
+
   const AssignPopover = (
     <Popover {...popoverProps}>
       <PopoverHeader>Select Teams to Assign</PopoverHeader>
       <PopoverBody>
-        <h1>Here be Teams!</h1>
         {organizationsArray ? (
           <Form name={getFormName(jurisdictionId)}>
             <OrganizationSelect {...organizationSelectProps} />
+            <Button color="default" onClick={onClearAssignmentsButtonClick} size="xs">
+              Clear
+            </Button>
+            <Button color="primary" onClick={onSaveAssignmentsButtonClick} size="xs">
+              Save
+            </Button>
           </Form>
         ) : (
           <p>No teams loaded...</p>
@@ -121,7 +147,9 @@ const mapStateToProps = (state: Partial<Store>, ownProps: any): AssignTeamCellPr
 };
 
 /** map props to actions that may be dispatched by component */
-const mapDispatchToProps = {};
+const mapDispatchToProps = {
+  fetchAssignmentsActionCreator: fetchAssignments,
+};
 
 /** Create Connected AssignTeamTableCell */
 const ConnectedAssignTeamTableCell = connect(
