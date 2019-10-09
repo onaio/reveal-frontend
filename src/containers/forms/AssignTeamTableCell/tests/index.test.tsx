a8f769a0751b2d8d0ca826e09c4f67d849946b75
cKellyDesign <ckelly@ona.io>
2019-10-09 10:13:38 -0700

    Add basic tests for AssignTeamTableCell

---
+++ src/containers/forms/AssignTeamTableCell/tests/index.test.tsx
@@ -0,0 +1,47 @@
+import reducerRegistry from '@onaio/redux-reducer-registry';
+import { mount, shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import { createBrowserHistory } from 'history';
+import React from 'react';
+import { Provider } from 'react-redux';
+import AssignTeamTableCell from '..';
+import store from '../../../../store';
+import assignmentReducer, {
+  reducerName as assignmentReducerName,
+} from '../../../../store/ducks/opensrp/assignments';
+import organizationsReducer, {
+  reducerName as organizationsReducerName,
+} from '../../../../store/ducks/opensrp/organizations';
+
+jest.mock('../../../../configs/env');
+
+reducerRegistry.register(assignmentReducerName, assignmentReducer);
+reducerRegistry.register(organizationsReducerName, organizationsReducer);
+
+describe('/containers/forms/AssignTeamTableCell', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('renders without crashing', () => {
+    shallow(<AssignTeamTableCell />);
+  });
+
+  it('renders correctly', () => {
+    const props = {
+      assignPopover: <div id="custom-popover" />,
+      jurisdictionId: 'outpost-number-one',
+      planId: 'alpha',
+    };
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <AssignTeamTableCell {...props} />
+      </Provider>
+    );
+    expect(toJson(wrapper.find('button#plan-assignment-outpost-number-one'))).toMatchSnapshot(
+      'AssignTeamButton'
+    );
+    expect(toJson(wrapper.find('span.assignment-count-text'))).toMatchSnapshot('Team Count Text');
+  });
+});
