ee51f948e0eb658c97f58066246d5e9b7dfb7138
cKellyDesign <ckelly@ona.io>
2019-10-11 07:32:59 -0700

    Remove commented out test, moved to #517

--- src/components/AssignTeamPopover/tests/index.test.tsx
+++ src/components/AssignTeamPopover/tests/index.test.tsx
@@ -23,32 +23,4 @@ describe('/components/AssignTeamPopover', () => {
     };
     shallow(<AssignTeamPopover {...props} />);
   });
-
-  // todo - fix this test!!!
-  // it('renders correctly', () => {
-  //   const mock: any = jest.fn();
-  //   const props:AssignTeamPopoverProps = {
-  //     formName: 'plan-assignment-form-outpost-number-one',
-  //     isActive: true,
-  //     jurisdictionId: 'outpost-number-one',
-  //     onClearAssignmentsButtonClick: mock,
-  //     onSaveAssignmentsButtonClick: mock,
-  //     onToggle: mock,
-  //     organizationsById: null,
-  //     planId: 'alpha',
-  //     target: 'plan-assignment-outpost-number-one',
-  //   }
-
-  //   // attempted fix from https://github.com/reactstrap/reactstrap/issues/773#issuecomment-373451256
-  //   var div = document.createElement('div');
-  //   document.body.appendChild(div);
-
-  //   // this is breaking on mount because the underlying Popover component can't find the button
-  //   const wrapper = mount((
-  //     <div>
-  //       <button id="plan-assignment-outpost-number-one">Target Button</button>
-  //       <AssignTeamPopover {...props} />
-  //     </div>
-  //   ), { attachTo: div });
-  // })
 });
