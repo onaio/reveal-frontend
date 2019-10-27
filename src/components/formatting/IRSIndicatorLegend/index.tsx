e48c54b4166ee4d024b54194923f07e705f7c55c
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-10-27 13:07:36 +0300

    Add IRSIndicatorLegend component

---
+++ src/components/formatting/IRSIndicatorLegend/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Table } from 'reactstrap';
+import {
+  IndicatorThresholdItem,
+  IndicatorThresholds,
+  indicatorThresholdsNA,
+} from '../../../configs/settings';
+import { percentage } from '../../../helpers/utils';
+
+interface Props {
+  indicatorThresholds: IndicatorThresholds;
+}
+
+const defaultProps: Props = {
+  indicatorThresholds: indicatorThresholdsNA,
+};
+
+const IRSIndicatorLegend = (props: Props) => {
+  const { indicatorThresholds } = props;
+
+  return (
+    <div>
+      <h5 className="mt-4 mb-3">IRS Conditional Formatting</h5>
+      <div className="row">
+        <div className="col-3">
+          <Table className="text-center">
+            <tbody>
+              {Object.values(indicatorThresholds)
+                .sort((a: IndicatorThresholdItem, b: IndicatorThresholdItem) =>
+                  a.value > b.value ? 1 : -1
+                )
+                .map((item, index) => {
+                  return (
+                    <tr key={index} style={{ background: item.color }}>
+                      <td>{item.name}</td>
+                      <td>>=</td>
+                      <td>{percentage(item.value)}</td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </Table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+IRSIndicatorLegend.defaultProps = defaultProps;
+
+export default IRSIndicatorLegend;
