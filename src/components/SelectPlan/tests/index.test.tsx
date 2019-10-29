0fca3b3bdeed5bb521cfaaa33250144fc10ad5c3
cKellyDesign <ckelly@ona.io>
2019-10-29 14:44:31 +0300

    Add optional param for placholder text to SelectPlan for Select

--- src/components/SelectPlan/tests/index.test.tsx
+++ src/components/SelectPlan/tests/index.test.tsx
@@ -4,6 +4,7 @@ import { createBrowserHistory } from 'history';
 import React from 'react';
 import { Router } from 'react-router';
 import SelectComponent from '../';
+import { PLAN_SELECT_PLACEHOLDER } from '../../../constants';
 import * as fixtures from '../../../store/ducks/tests/fixtures';
 
 const history = createBrowserHistory();
@@ -39,6 +40,20 @@ describe('components/page/Header', () => {
 
   it('renders header correctly when authenticated', () => {
     const props = {
+      plansArray: [fixtures.plan1, fixtures.plan2],
+    };
+    const wrapper = mount(
+      <Router history={history}>
+        <SelectComponent {...props} />
+      </Router>
+    );
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.unmount();
+  });
+
+  it('renders a placeholder correctly if provided', () => {
+    const props = {
+      placeholder: PLAN_SELECT_PLACEHOLDER,
       plansArray: [fixtures.plan1, fixtures.plan2],
     };
     const wrapper = mount(
