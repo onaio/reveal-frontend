73694eaf394b59640be1168e9c5120de4ad08e27
cKellyDesign <ckelly@ona.io>
2019-12-05 16:30:01 -0800

    Fix initial lang test by passing in reference

--- src/helpers/tests/lang.test.tsx
+++ src/helpers/tests/lang.test.tsx
@@ -1,15 +1,15 @@
-import { keys } from 'lodash';
-import * as lang from '../../configs/lang';
 import { translate } from '../../configs/lang';
 
+import strings from '../../configs/strings';
 jest.mock('../../configs/env');
 
 describe('configs/lang', () => {
   beforeEach(() => {
     jest.resetAllMocks();
   });
+
   it('translates display strings', () => {
-    expect(translate('WELCOME_TO_REVEAL', 'foo baz')).toBe('Welcome to Reveal');
+    expect(translate('WELCOME_TO_REVEAL', 'foo baz', strings.en)).toBe('Welcome to Reveal');
   });
 
   it('falls back to english translations', () => {
