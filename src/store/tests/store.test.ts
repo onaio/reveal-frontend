17248053f4e9e38bd3a13d58b17ce853fedeb215
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-12-13 22:10:27 +0300

    Add test that preloaded state works

--- src/store/tests/store.test.ts
+++ src/store/tests/store.test.ts
@@ -3,12 +3,14 @@ import { authenticateUser, getUser, isAuthenticated, logOutUser } from '@onaio/s
 import { FlushThunks } from 'redux-testkit';
 import store from '..';
 import messages, { selectAllMessages, sendMessage } from './ducks/messages';
+import random, { reducerName as randomReducer } from './ducks/random';
 
 describe('store', () => {
   let flushThunks;
 
   beforeEach(() => {
     flushThunks = FlushThunks.createMiddleware();
+    reducerRegistry.register(randomReducer, random);
     jest.resetAllMocks();
   });
 
@@ -53,5 +55,9 @@ describe('store', () => {
     expect(store.getState().messages).toEqual({ messages: [{ user: 'bob', message: 'hello' }] });
     // retrieving data should work
     expect(selectAllMessages(store.getState())).toEqual([{ message: 'hello', user: 'bob' }]);
+  });
+
+  it('should work with preloaded state', () => {
+    expect(store.getState().random).toEqual('Preloaded state, baby!');
   });
 });
