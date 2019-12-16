a2f8474decbdc32387fb30a2a72aa3bb62b18ffd
cKellyDesign <ckelly@ona.io>
2019-12-16 11:56:37 -0800

    Add username dimension to GA for RVL-216

--- src/components/page/WithGATracker/index.tsx
+++ src/components/page/WithGATracker/index.tsx
@@ -1,16 +1,31 @@
 /* eslint-disable react/prop-types */
+import { getUser } from '@onaio/session-reducer';
 import React, { Component } from 'react';
 import GoogleAnalytics from 'react-ga';
 import { RouteComponentProps } from 'react-router';
 import { GA_CODE, GA_ENV } from '../../../configs/env';
 import { RouteParams } from '../../../helpers/utils';
+import store from '../../../store';
 
 type Props = RouteComponentProps<RouteParams>;
+let username = (getUser(store.getState()) || {}).username || '';
+
+/**
+ * helper function to set the Google Analytics dimension for username
+ */
+export const setGAusername = (): void => {
+  const user = getUser(store.getState()) || {};
+  username = user.username || '';
+  GoogleAnalytics.set({ username });
+};
+
+export const getGAusername = (): string => username;
 
 if (GA_CODE.length) {
   GoogleAnalytics.initialize(GA_CODE);
   GoogleAnalytics.set({
     env: GA_ENV,
+    username,
   });
 }
 
@@ -26,6 +41,11 @@ const WithGATracker = (WrappedComponent: any, options: any = {}) => {
   // eslint-disable-next-line
   const HOC = class extends Component<Props> {
     public componentDidMount() {
+      const user = getUser(store.getState()) || {};
+      if (getGAusername() !== user.username) {
+        setGAusername();
+      }
+
       if (GA_CODE.length) {
         // eslint-disable-next-line
         const page = this.props.location.pathname + this.props.location.search;
