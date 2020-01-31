b053cc45fbe09cfb616ccb09c97773d3811ccad5
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-01-31 20:48:55 +0300

    Display link to focus area when adding new FI plan
    - Pass a handler function to PlanForm that is used to "lift" up form values into
      the new plan component
    - Use form values to render a link to the focus area when a new FI plan's
      jurisdiction has been selected

--- src/containers/pages/InterventionPlan/NewPlan/General/index.tsx
+++ src/containers/pages/InterventionPlan/NewPlan/General/index.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
 import { Col, Row } from 'reactstrap';
+import { format } from 'util';
 import HeaderBreadcrumb from '../../../../../components/page/HeaderBreadcrumb/HeaderBreadcrumb';
-import { CREATE_NEW_PLAN, HOME, PLANS } from '../../../../../configs/lang';
-import { HOME_URL, NEW_PLAN_URL, PLAN_LIST_URL } from '../../../../../constants';
-import PlanForm from '../../../../forms/PlanForm';
+import { CREATE_NEW_PLAN, FIS_IN_JURISDICTION, HOME, PLANS } from '../../../../../configs/lang';
+import { FI_SINGLE_URL, HOME_URL, NEW_PLAN_URL, PLAN_LIST_URL } from '../../../../../constants';
+import { InterventionType } from '../../../../../store/ducks/plans';
+import PlanForm, { defaultInitialValues } from '../../../../forms/PlanForm';
 
 /** Simple component that loads the new plan form and allows you to create a new plan */
 const NewPlan = () => {
+  const [formValues, setFormValues] = useState(defaultInitialValues);
+  const formValuesHandler = (curr: any, next: any) => {
+    setFormValues(next.values);
+  };
+
   const pageTitle: string = CREATE_NEW_PLAN;
 
   const breadcrumbProps = {
@@ -36,7 +44,15 @@ const NewPlan = () => {
       <h3 className="mb-3 page-title">{pageTitle}</h3>
       <Row>
         <Col md={8}>
-          <PlanForm />
+          <PlanForm {...{ formHandler: formValuesHandler }} />
+        </Col>
+        <Col md={4}>
+          {formValues.interventionType === InterventionType.FI &&
+            formValues.jurisdictions[0].id !== '' && (
+              <Link to={`${FI_SINGLE_URL}/${formValues.jurisdictions[0].id}`}>
+                {format(FIS_IN_JURISDICTION, formValues.jurisdictions[0].name)}
+              </Link>
+            )}
         </Col>
       </Row>
     </div>
