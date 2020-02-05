3b01e7987f5878add552a52097ad2a0ed05cb4e0
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-02-05 10:34:06 +0300

    Add FIJurisdiction component

---
+++ src/containers/pages/FocusInvestigation/jurisdiction/index.tsx
@@ -0,0 +1,8 @@
+import DrillDownTable from '@onaio/drill-down-table';
+import * as React from 'react';
+
+const FIJurisdiction = () => {
+  return <div>xxxxxxxxxxxxxxxxxxxxxxxxxxxxx</div>;
+};
+
+export default FIJurisdiction;
