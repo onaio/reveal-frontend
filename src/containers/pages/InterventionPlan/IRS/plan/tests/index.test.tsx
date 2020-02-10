bae8d8b3daab8674d14c9e388a2044c14cb47435
cKellyDesign <ckelly@ona.io>
2020-02-10 11:29:41 -0800

    Init better tests for IRS Planning pages

--- src/containers/pages/InterventionPlan/IRS/plan/tests/index.test.tsx
+++ src/containers/pages/InterventionPlan/IRS/plan/tests/index.test.tsx
@@ -6,17 +6,24 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router';
 import { INTERVENTION_IRS_URL } from '../../../../../../constants';
+import { FlexObject } from '../../../../../../helpers/utils';
 import store from '../../../../../../store';
 import jurisdictionReducer, {
   reducerName as jurisdictionReducerName,
 } from '../../../../../../store/ducks/jurisdictions';
+import { Organization } from '../../../../../../store/ducks/opensrp/organizations';
 import plansReducer, {
   fetchPlanRecords,
   PlanRecordResponse,
   reducerName as plansReducerName,
 } from '../../../../../../store/ducks/plans';
+import { authorizeSuperset } from '../../../../../../store/ducks/superset';
 import * as fixtures from '../../../../../../store/ducks/tests/fixtures';
-import { irsPlanRecordResponse1, jurisdictionsById } from '../../tests/fixtures';
+import {
+  irsPlanRecordResponse1,
+  jurisdictionsArray,
+  jurisdictionsById,
+} from '../../tests/fixtures';
 import ConnectedIrsPlan, { IrsPlan } from './..';
 
 reducerRegistry.register(jurisdictionReducerName, jurisdictionReducer);
@@ -31,6 +38,7 @@ const fetch = require('jest-fetch-mock');
 describe('containers/pages/IRS/plan', () => {
   beforeEach(() => {
     jest.resetAllMocks();
+    store.dispatch(authorizeSuperset(true));
   });
 
   it('renders without crashing', () => {
@@ -80,6 +88,7 @@ describe('containers/pages/IRS/plan', () => {
 
   it('renders without crashing when loading plans with invlaid jurisdiction ids', () => {
     store.dispatch(fetchPlanRecords([irsPlanRecordResponse1 as PlanRecordResponse]));
+    // store.dispatch()
 
     fetch.mockResponseOnce(JSON.stringify({}));
     const mock: any = jest.fn();
@@ -114,6 +123,50 @@ describe('containers/pages/IRS/plan', () => {
         </Router>
       </Provider>
     );
+
+    wrapper.unmount();
+  });
+
+  it('calls the correct API endpoints to load map and table', () => {
+    store.dispatch(fetchPlanRecords([irsPlanRecordResponse1 as PlanRecordResponse])); // negates: opensrp (1) - get plan
+
+    const mock: any = jest.fn();
+    const { identifier: id } = irsPlanRecordResponse1;
+    // const supersetServiceMock: any = jest.fn();
+    // supersetServiceMock.mockImplementation(async () => jurisdictionsArray as FlexObject[]);
+
+    fetch
+      .mockResponseOnce(JSON.stringify([] as Organization[])) // opensrp (2) - get organizations: the only successful mock response
+      .mockResponseOnce(JSON.stringify(jurisdictionsArray as FlexObject[])) // superset (3) - get all jurisdictions
+      .mockResponseOnce(JSON.stringify([jurisdictionsArray[0]])); // opensrp (4) - get level 0 jurisdiction
+
+    const props = {
+      history,
+      location: mock,
+      match: {
+        isExact: true,
+        params: { id },
+        path: `${INTERVENTION_IRS_URL}/plan/:id`,
+        url: `${INTERVENTION_IRS_URL}/plan/${id}`,
+      },
+    };
+    const wrapper = mount(
+      <Provider store={store}>
+        <Router history={history}>
+          <ConnectedIrsPlan {...props} />
+        </Router>
+      </Provider>
+    );
+
+    // check that the page renders more than just a spinner
+    // todo - when more than the spinner loads, replace snapshot with better tests
+    expect(toJson(wrapper.find('IrsPlan'))).toMatchSnapshot('IRS PLAN');
+
+    // todo - mock click save buttons
+    // const saveDraftButton = wrapper.find('.save-plan-as-draft-btn');
+    // expect(saveDraftButton.length).toBe(1);
+    // const saveFinalButton = wrapper.find('.save-as-finalized-plan-btn');
+    // expect(saveDraftButton.length).toBe(1);
 
     wrapper.unmount();
   });
