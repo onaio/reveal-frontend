39e6b90c827da7c382fb9f219f9c9c660a60c184
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-02-12 19:06:45 +0300

    Cleanup and refactor

--- src/helpers/cbv.tsx
+++ src/helpers/cbv.tsx
@@ -1,30 +1,81 @@
 import reducerRegistry from '@onaio/redux-reducer-registry';
+import * as React from 'react';
 import { connect } from 'react-redux';
 import { Store } from 'redux';
-import reducer, { selectAllMessages, sendMessage } from '../store/tests/ducks/messages';
+import store from '../store';
+import reducer, {
+  reducerName,
+  selectAllMessages,
+  sendMessage,
+} from '../store/tests/ducks/messages';
 
-reducerRegistry.register('messages', reducer);
+reducerRegistry.register(reducerName, reducer);
+
+interface ObjectListOptions {
+  listPropName: string;
+  dispatchPropName: string;
+}
+
+interface ObjectListProps {
+  actionCreator: any;
+  objectList: any;
+}
 
 export class ObjectList {
   public Component: any;
+  public options: ObjectListOptions;
 
-  constructor(component: any) {
+  constructor(component: any, options: ObjectListOptions) {
     this.Component = component;
+    this.options = options;
   }
 
-  public render() {
-    // map dispatch to props
-    const mapDispatchToProps = { dispatchObjects: sendMessage };
-    /** map state to props */
-    const mapStateToProps = (state: Partial<Store>): any => {
-      return {
-        messages: selectAllMessages(state),
+  public loadData() {
+    store.dispatch(sendMessage({ user: 'bob', message: 'hello' }));
+    store.dispatch(sendMessage({ user: 'brayo', message: 'sasa' }));
+    store.dispatch(sendMessage({ user: 'Swale', message: 'habari' }));
+    store.dispatch(sendMessage({ user: 'ras', message: 'yeiya' }));
+  }
+
+  public getHOC() {
+    const NewComponent = (props: ObjectListProps) => {
+      const { actionCreator, objectList } = props;
+      const propsToPass = {
+        [this.options.dispatchPropName]: actionCreator,
+        [this.options.listPropName]: objectList,
       };
+      return <this.Component {...propsToPass} />;
     };
 
+    return NewComponent;
+  }
+
+  public getMapDispatchToProps() {
+    return { actionCreator: sendMessage };
+  }
+
+  public getMapStateToProps() {
+    return (state: Partial<Store>): any => {
+      return {
+        objectList: selectAllMessages(state),
+      };
+    };
+  }
+
+  public getConnectedHOC() {
+    /** map dispatch to props */
+    const mapDispatchToProps = this.getMapDispatchToProps();
+    /** map state to props */
+    const mapStateToProps = this.getMapStateToProps();
+    /** connect to store */
     return connect(
       mapStateToProps,
       mapDispatchToProps
-    )(this.Component);
+    )(this.getHOC());
+  }
+
+  public render() {
+    this.loadData();
+    return this.getConnectedHOC();
   }
 }
