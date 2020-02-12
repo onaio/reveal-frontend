09f0c2ef1830b382303c7c8bfef0841320921b3b
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-02-12 17:57:30 +0300

    Use basic working version of ObjectList

--- src/containers/pages/FocusInvestigation/jurisdiction/index.tsx
+++ src/containers/pages/FocusInvestigation/jurisdiction/index.tsx
@@ -1,8 +1,42 @@
 import * as React from 'react';
 import { ObjectList } from '../../../../helpers/cbv';
+import { Message, sendMessage } from '../../../../store/tests/ducks/messages';
 
-const FIJurisdiction = () => {
-  return <div>xxxxxxxxxxxxxxxxxxxxxxxxxxxxx</div>;
+interface Props {
+  dispatchObjects: typeof sendMessage;
+  messages: Message[];
+}
+
+const FIJurisdiction = (props: Props) => {
+  const { dispatchObjects, messages } = props;
+  const messageList =
+    messages.length > 0 ? (
+      <ul>
+        {messages.map(e => (
+          <li>
+            {e.user}: {e.message}
+          </li>
+        ))}
+      </ul>
+    ) : (
+      <div>nothing</div>
+    );
+
+  const xxx = () => {
+    dispatchObjects({ user: 'bob', message: 'hello' });
+  };
+
+  return (
+    <div>
+      {messageList}
+      <hr />
+      <span onClick={xxx}>Add Message</span>
+    </div>
+  );
+};
+
+FIJurisdiction.defaultProps = {
+  messages: [] as Message[],
 };
 
 const objectList = new ObjectList(FIJurisdiction);
