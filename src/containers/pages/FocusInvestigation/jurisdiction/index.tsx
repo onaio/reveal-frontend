3777d85ec46f263705e5d68fca696582cda941e8
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-02-17 18:40:16 +0300

    Add functionality for fetching plans from Superset
    - Add the necessary props
    - Add the superset fetch function that calls the action creator

--- src/containers/pages/FocusInvestigation/jurisdiction/index.tsx
+++ src/containers/pages/FocusInvestigation/jurisdiction/index.tsx
@@ -1,8 +1,17 @@
 import reducerRegistry from '@onaio/redux-reducer-registry';
+import superset from '@onaio/superset-connector';
 import React, { useEffect } from 'react';
+import { RouteComponentProps } from 'react-router';
+import { SUPERSET_PLANS_SLICE } from '../../../../configs/env';
 import { ObjectList } from '../../../../helpers/cbv';
 import { displayError } from '../../../../helpers/errors';
-import { getURL } from '../../../../services/opensrp';
+import supersetFetch from '../../../../services/superset';
+import plansReducer, {
+  fetchPlans,
+  InterventionType,
+  Plan,
+  reducerName as plansReducerName,
+} from '../../../../store/ducks/plans';
 import reducer, {
   Message,
   reducerName,
@@ -12,42 +21,51 @@ import reducer, {
 } from '../../../../store/tests/ducks/messages';
 
 reducerRegistry.register(reducerName, reducer);
+reducerRegistry.register(plansReducerName, plansReducer);
 
 /** Interface for props that come from the URL */
 export interface RouteParams {
   jurisdiction_id: string;
 }
 
+/** Interface for FIJurisdiction component props */
 export interface FIJurisdictionProps {
+  caseTriggeredPlans: Plan[] | null;
+  fetchPlansActionCreator: typeof fetchPlans;
   loadMessages: typeof sendMessage;
   messages: Message[];
+  routinePlans: Plan[] | null;
+  supersetService: typeof supersetFetch;
 }
 
-const options = {
-  actionCreator: sendMessage,
-  dispatchPropName: 'loadMessages',
-  listPropName: 'messages',
-  selector: selectAllMessages,
+/** default props for ActiveFI component */
+export const defaultActiveFIProps: FIJurisdictionProps = {
+  caseTriggeredPlans: null,
+  fetchPlansActionCreator: fetchPlans,
+  loadMessages: sendMessage,
+  messages: [] as Message[],
+  routinePlans: null,
+  supersetService: supersetFetch,
 };
 
 /** This component is meant to display a list of focus investigations for a
  * given jurisdiction.  It is meant to display for the focus area i.e. the
  * lowest level jurisdiction
  */
-const FIJurisdiction = (props: FIJurisdictionProps) => {
-  const { loadMessages, messages } = props;
+const FIJurisdiction = (props: FIJurisdictionProps & RouteComponentProps<RouteParams>) => {
+  const { fetchPlansActionCreator, messages, supersetService } = props;
+
+  const jurisdiction_id = props.match.params.jurisdiction_id;
+
+  // this gets FI plans for the current jurisdiction
+  const supersetParams = superset.getFormData(2000, [
+    { comparator: jurisdiction_id, operator: '==', subject: 'jurisdiction_id' },
+    { comparator: InterventionType.FI, operator: '==', subject: 'plan_intervention_type' },
+  ]);
 
   useEffect(() => {
-    const requestOptions: RequestInit = {
-      method: 'GET',
-      mode: 'no-cors',
-    };
-    const params = { user: 'jaya', message: 'hello' };
-    const url = getURL('https://postman-echo.com/get', params);
-    fetch(url, requestOptions)
-      .then(result => {
-        loadMessages({ message: result.ok, user: result.type });
-      })
+    supersetService(SUPERSET_PLANS_SLICE, supersetParams)
+      .then((result: Plan[]) => fetchPlansActionCreator(result))
       .catch(err => displayError(err));
   }, []);
 
@@ -72,14 +90,19 @@ const FIJurisdiction = (props: FIJurisdictionProps) => {
   );
 };
 
-FIJurisdiction.defaultProps = {
-  loadMessages: sendMessage,
-  messages: [] as Message[],
+FIJurisdiction.defaultProps = defaultActiveFIProps;
+
+/** ObjectList options */
+const objectListOptions = {
+  actionCreator: sendMessage,
+  dispatchPropName: 'loadMessages',
+  listPropName: 'messages',
+  selector: selectAllMessages,
 };
 
 const cbv = new ObjectList<Message, SendMessageAction, typeof selectAllMessages>(
   FIJurisdiction,
-  options
+  objectListOptions
 );
 
 /** This represents a fully redux-connected component that fetches data from
