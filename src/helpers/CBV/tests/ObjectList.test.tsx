5d414646ab2239d98f7a24191bfe7c0539cdf95a
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-02-25 14:36:57 +0300

    Add start of ObjectList tests

---
+++ src/helpers/CBV/tests/ObjectList.test.tsx
@@ -0,0 +1,65 @@
+import reducerRegistry from '@onaio/redux-reducer-registry';
+import React from 'react';
+import store from '../../../store';
+import reducer, {
+  Message,
+  reducerName,
+  selectAllMessages,
+  sendMessage,
+  SendMessageAction,
+} from '../../../store/tests/ducks/messages';
+import { ObjectList } from '../ObjectList';
+
+/** register reducers */
+reducerRegistry.register(reducerName, reducer);
+
+interface TestProps {
+  actionCreator: typeof sendMessage;
+  callbackFunc?: () => void;
+  messages: Message[];
+}
+
+const TestComponent = (_: TestProps) => <div>mosh</div>;
+
+/** ObjectList options */
+const objectListOptions = {
+  actionCreator: sendMessage,
+  dispatchPropName: 'actionCreator',
+  returnPropName: 'messages',
+  selector: selectAllMessages,
+};
+
+const ClassBasedView = new ObjectList<
+  Message,
+  SendMessageAction,
+  typeof selectAllMessages,
+  TestProps
+>(TestComponent, objectListOptions);
+
+describe('cbv/ObjectList', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+  it('getMapStateToProps works as expected', () => {
+    const mapStateToProps = ClassBasedView.getMapStateToProps();
+
+    expect(typeof mapStateToProps === 'function').toBeTruthy();
+
+    expect(mapStateToProps(store.getState(), {} as any)).toEqual({ messages: [] });
+
+    store.dispatch(sendMessage({ user: 'bob', message: 'hello' }));
+    store.dispatch(sendMessage({ user: 'bobbie', message: 'hello hello' }));
+
+    expect(mapStateToProps(store.getState(), {} as any)).toEqual({
+      messages: [{ user: 'bob', message: 'hello' }, { user: 'bobbie', message: 'hello hello' }],
+    });
+  });
+  // it('getMapDispatchToProps works as expected', () => {
+  // });
+  // it('getHOC works as expected', () => {
+  // });
+  // it('getConnectedHOC works as expected', () => {
+  // });
+  // it('render works as expected', () => {
+  // });
+});
