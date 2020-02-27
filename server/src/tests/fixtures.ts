2414689a666020147f8c9fd79f92f7db22287642
p-netm <pmuriuki98@gmail.com>
2020-02-27 12:46:34 +0300

    Add preliminary tests

---
+++ server/src/tests/fixtures.ts
@@ -0,0 +1,6 @@
+// tslint:disable: object-literal-sort-keys
+export const parsedApiResponse = {
+  userName: 'superset-user',
+  roles: ['Provider'],
+  preferredName: 'Superset User',
+};
---
+++ server/src/tests/index.test.ts
@@ -0,0 +1,170 @@
+import ClientOauth2 from 'client-oauth2';
+import { json } from 'express';
+import nock from 'nock';
+import request from 'supertest';
+import { EXPRESS_SESSION_LOGIN_URL, FRONTEND_OPENSRP_CALLBACK_URL } from '../configs/envs';
+import server from '../index';
+import { parsedApiResponse } from './fixtures';
+
+const authorizationUri = 'https://reveal-stage.smartregister.org/opensrp/oauth/';
+const oauthCallbackUri = '/oauth/callback/OpenSRP/?code=Boi4Wz&state=opensrp';
+
+const panic = (err: Error, done: jest.DoneCallback) => {
+  if (err) {
+    done(err);
+  }
+};
+
+jest.mock('../configs/envs');
+// jest.mock('request', () => {
+//   return {
+//     get(url: string, options: any, callback: Function) {
+//       callback();
+//     },
+//   };
+// });
+
+jest.mock('client-oauth2', () => {
+  class CodeFlow {
+    private client: ClientOauth2;
+    public constructor(client: ClientOauth2) {
+      this.client = client;
+    }
+
+    public getUri() {
+      return authorizationUri;
+    }
+
+    public async getToken() {
+      return this.client.token;
+    }
+  }
+  // tslint:disable-next-line: max-classes-per-file
+  class TokenFlow {
+    public data = (() => {
+      return {
+        access_token: '64dc9918-fa1c-435d-9a97-ddb4aa1a8316',
+        expires_in: 3221,
+        refresh_token: '808f060c-be93-459e-bd56-3074d9b96229',
+        scope: 'read write',
+        token_type: 'bearer',
+      };
+    })();
+
+    private client: ClientOauth2;
+    public constructor(client: ClientOauth2) {
+      this.client = client;
+    }
+
+    public sign(options: any) {
+      return { url: 'http://someUrl.com' };
+    }
+  }
+
+  // tslint:disable-next-line: max-classes-per-file
+  return class ClientOAuth2 {
+    public code = (() => {
+      return new CodeFlow(this as any);
+    })();
+    public token = (() => {
+      return new TokenFlow(this as any);
+    })();
+    private options: ClientOauth2.Options;
+    private request: ClientOauth2.Request;
+    public constructor(options: ClientOauth2.Options, request: ClientOauth2.Request) {
+      this.options = options;
+      this.request = request;
+    }
+  };
+});
+
+describe('src/index.ts', () => {
+  const actualJsonParse = JSON.parse;
+
+  afterEach(() => {
+    JSON.parse = actualJsonParse;
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+
+  it('serves the build.index.html file', done => {
+    request(server)
+      .get('/')
+      .expect(200)
+      .expect('Do you mind')
+      .end((err: Error, res: Express.Response) => {
+        if (err) {
+          throw err;
+        }
+        done();
+      });
+  });
+
+  it('oauth/opensrp redirects to auth-server', done => {
+    request(server)
+      .get('/oauth/opensrp')
+      .expect(302)
+      // .expect('Do you mind')
+      .end((err: Error, res: any) => {
+        panic(err, done);
+        expect(res.headers.location).toEqual(authorizationUri);
+        expect(res.notFound).toBeFalsy();
+        expect(res.redirect).toBeTruthy();
+        done();
+      });
+  });
+
+  it('E2E: oauth/opensrp/callback works correctly', done => {
+    JSON.parse = body => {
+      if (body === '{}') {
+        return parsedApiResponse;
+      }
+    };
+    nock('https://reveal-stage.smartregister.org')
+      .get(`/opensrp/user-details`)
+      .reply(200, {});
+    request(server)
+      .get(oauthCallbackUri)
+      .end((err, res: any) => {
+        panic(err, done);
+        expect(res.headers.location).toEqual(FRONTEND_OPENSRP_CALLBACK_URL);
+        expect(res.notFound).toBeFalsy();
+        expect(res.redirect).toBeTruthy();
+        done();
+      });
+  });
+
+  it('/oauth/state works correctly', done => {
+    request(server)
+      .get('/oauth/state')
+      .end((err, res) => {
+        panic(err, done);
+        expect(res.body).toEqual({
+          error: 'Not authorized',
+        });
+        done();
+      });
+  });
+
+  it('logs user out', done => {
+    request(server)
+      .get('/logout')
+      .end((err, res: any) => {
+        panic(err, done);
+        expect(res.headers.location).toEqual(EXPRESS_SESSION_LOGIN_URL);
+        expect(res.redirect).toBeTruthy();
+        done();
+      });
+  });
+
+  it('protects login route from authenticated users', done => {
+    request(server)
+      .get('/login')
+      .end((err, res: any) => {
+        panic(err, done);
+        expect(res.headers.location).toEqual('');
+      });
+  });
+});
