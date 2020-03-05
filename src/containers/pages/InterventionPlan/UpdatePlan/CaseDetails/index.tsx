949feaa218363f5f8103dadbb80da16d60811dbe
p-netm <pmuriuki98@gmail.com>
2020-03-05 17:34:25 +0300

    Add and test CaseDetails component

--- src/containers/pages/InterventionPlan/UpdatePlan/CaseDetails/index.tsx
+++ src/containers/pages/InterventionPlan/UpdatePlan/CaseDetails/index.tsx
@@ -1,72 +1,153 @@
 /** Accordion like component that can be used on the manage plans
  * page to display case Details
  */
-import reducerRegistry from '@onaio/redux-reducer-registry/dist/types';
+import reducerRegistry from '@onaio/redux-reducer-registry';
+import { FlexObject } from 'gisida';
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { toast } from 'react-toastify';
-import { Button, Card, CardBody, CardHeader, CardSubtitle, CardText, CardTitle } from 'reactstrap';
-import { ActionCreator } from 'redux';
+import { Card, CardBody, CardHeader, Collapse } from 'reactstrap';
+import { ActionCreator, Store } from 'redux';
+import Loading from '../../../../../components/page/Loading';
 import { OPENSRP_API_BASE_URL } from '../../../../../configs/env';
+import { CASE_DETAILS, CASE_INFORMATION, CASE_NUMBER } from '../../../../../configs/lang';
 import { OPENSRP_FIND_EVENTS_ENDPOINT } from '../../../../../constants';
+import { displayError } from '../../../../../helpers/errors';
 import { growl } from '../../../../../helpers/utils';
 import { OpenSRPService } from '../../../../../services/opensrp';
 import eventReducer, {
+  fetchEvents,
   FetchEventsAction,
+  getEventById,
   reducerName,
 } from '../../../../../store/ducks/opensrp/events';
-import { Event } from '../../../../../store/ducks/opensrp/events/utils';
+import {
+  Event,
+  RawEvent,
+  translateEvent,
+  UUID,
+} from '../../../../../store/ducks/opensrp/events/utils';
 
 reducerRegistry.register(reducerName, eventReducer);
 
-interface CaseDetailsProps {
-  eventId: string;
+/** describes props to be passed to caseDetail component */
+export interface CaseDetailsProps {
+  eventId: UUID | null;
   service: typeof OpenSRPService;
-  event: Event;
+  event: Event | null;
   fetchEventsCreator: ActionCreator<FetchEventsAction>;
 }
 
-const loadEvent = async (eventId: string, servant: typeof OpenSRPService) => {
-  const service = new servant(OPENSRP_FIND_EVENTS_ENDPOINT, OPENSRP_API_BASE_URL);
-  return await service.read(eventId);
+/** initial props for caseDetails */
+const defaultCaseDetailsProps: CaseDetailsProps = {
+  event: null,
+  eventId: null,
+  fetchEventsCreator: fetchEvents,
+  service: OpenSRPService,
 };
 
-export const CaseDetails: React.FC<CaseDetailsProps> = props => {
-  const { eventId, service, event } = props;
-  // get the opensrp event id
+/** async function to load a single event
+ * @param {UUID} eventId - id of the event to get
+ * @param {OpenSRPService} servant - the opensrp service
+ * @param {ActionCreator<FetchEventsAction>} actionCreator - the actionCreator
+ */
+const loadEvent = async (
+  eventId: UUID,
+  servant: typeof OpenSRPService,
+  actionCreator: ActionCreator<FetchEventsAction>
+) => {
+  const service = new servant(OPENSRP_FIND_EVENTS_ENDPOINT, OPENSRP_API_BASE_URL);
+  const filterParams = {
+    id: eventId,
+  };
+  service
+    .list(filterParams)
+    .then((response: RawEvent) => actionCreator([response]))
+    .catch((err: Error) => displayError(err));
+};
+
+/** CaseDetails component */
+export const CaseDetails: React.FC<CaseDetailsProps> = (props = defaultCaseDetailsProps) => {
+  const { eventId, service, event, fetchEventsCreator } = props;
+  const [isCollapsed, setCollapse] = React.useState<boolean>(false);
+  const toggleCollapse = () => setCollapse(!isCollapsed);
+
+  if (eventId === null) {
+    // if eventId is null return early
+    return <Loading />;
+  }
+
   // make a call to the events endpoint and get event
   useEffect(() => {
-    loadEvent(eventId, service).catch((err: Error) =>
+    loadEvent(eventId, service, fetchEventsCreator).catch((err: Error) =>
       growl(err.message, { type: toast.TYPE.ERROR })
     );
   }, []);
-  // render event.
+
+  if (!event) {
+    return <Loading />;
+  }
+  const translatedEvent = translateEvent(event);
+  const { caseInformation, fociInformation, caseNumber, fociInformationTitle } = translatedEvent;
+
+  const mapObjectToElement = (obj: FlexObject) =>
+    Object.entries(obj).map(([property, value], index) => {
+      return (
+        <p key={index}>
+          <span className="">{property}</span>: {value}
+        </p>
+      );
+    });
+
   return (
     <>
-      <h4>{event.caseNumber}</h4>
+      <h3>
+        {CASE_NUMBER}: {caseNumber}
+      </h3>
       <div id="accordion">
         <Card>
-          <CardHeader>
-            <h5 className="mb-0">Case Details</h5>
+          <CardHeader onClick={toggleCollapse}>
+            <h4>{CASE_DETAILS}</h4>
           </CardHeader>
-          <div id="collapseOne" className="collapse" data-parent="#accordion">
+          <Collapse isOpen={isCollapsed}>
             <CardBody>
-              <CardTitle>Card title</CardTitle>
-              <CardSubtitle>Card subtitle</CardSubtitle>
-              <CardText>
-                Some quick example text to build on the card title and make up the bulk of the
-                card's content.
-              </CardText>
-              <Button>Button</Button>
+              <h5 className="mb-3">{CASE_INFORMATION}</h5>
+              {mapObjectToElement(caseInformation)}
+              <hr />
+              <h5 className="mb-3"> {fociInformationTitle}</h5>
+              {mapObjectToElement(fociInformation)}
             </CardBody>
-          </div>
+          </Collapse>
         </Card>
       </div>
     </>
   );
 };
 
-const mapStateToProps = () => ({});
+/** describes object returned by mapState */
+type MapStateToProps = Pick<CaseDetailsProps, 'event'>;
+/** describes mapDispatch */
+type DispatchToProps = Pick<CaseDetailsProps, 'fetchEventsCreator'>;
 
-const ConnectedCaseDetails = connect()(CaseDetails);
+/**
+ *
+ * @param {Partial<Store>} state = the store slice
+ * @param {CaseDetailsProps} ownProps - the props
+ */
+const mapStateToProps = (state: Partial<Store>, ownProps: CaseDetailsProps): MapStateToProps => {
+  const eventId = ownProps.eventId || '';
+  const response = {
+    event: getEventById(state, eventId),
+  };
+  return response;
+};
+
+const mapDispatchToProps: DispatchToProps = {
+  fetchEventsCreator: fetchEvents,
+};
+
+const ConnectedCaseDetails = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CaseDetails);
 export default ConnectedCaseDetails;
--- src/containers/pages/InterventionPlan/UpdatePlan/CaseDetails/tests/index.test.tsx
+++ src/containers/pages/InterventionPlan/UpdatePlan/CaseDetails/tests/index.test.tsx
@@ -1,66 +1,105 @@
 import { mount, shallow } from 'enzyme';
+import flushPromises from 'flush-promises';
 import React from 'react';
 import { Provider } from 'react-redux';
 import ConnectedCaseDetails, { CaseDetails } from '..';
 import { OpenSRPService } from '../../../../../../services/opensrp';
 import store from '../../../../../../store';
-/** Test sequence
- *  - renders without crashing.
- *  - renders correctly - checks initial render
- *      - collapsible is not collapsed.
- *  - collapsible works correctly:
- *      - parse html text for correct content
- *  - works correctly with store
- *  - makes correct api calls
- */
+import { fetchEvents } from '../../../../../../store/ducks/opensrp/events';
+import { rawEvent1 } from '../../../../../../store/ducks/opensrp/events/tests/fixtures';
+import { extractEvent } from '../../../../../../store/ducks/opensrp/events/utils';
 
+// tslint:disable-next-line: no-var-requires
 const fetch = require('jest-fetch-mock');
 
-const props = {
-  eventId: '',
+const globalProps = {
+  event: null,
+  eventId: null,
+  fetchEventsCreator: fetchEvents,
   service: OpenSRPService,
-  event: {},
 };
 
 describe('src/containers/pages/interventionPlan/updateplan/caseDetials', () => {
   beforeEach(() => {
     jest.resetAllMocks();
+    fetch.resetMocks();
+    jest.clearAllMocks();
   });
 
   it('renders without crashing', () => {
-    shallow(<CaseDetails {...props} />);
+    shallow(<CaseDetails {...globalProps} />);
   });
 
-  it('renders correctly', () => {
-    const wrapper = mount(<CaseDetails {...props} />);
-    // accordion is not collapsed
-    expect(wrapper.text()).toMatchInlineSnapshot();
-  });
+  it('renders correctly', async () => {
+    const props = {
+      ...globalProps,
+      event: extractEvent(rawEvent1),
+      eventId: '',
+    };
 
-  it('Accordion works correctly', () => {
-    const wrapper = mount(<CaseDetails {...props} />);
-    const cardHeader = wrapper.find('CardHeader');
-    // simulate click on cardHeader(Case Details)
-    cardHeader.simulate('click');
-    wrapper.update();
-    // The case details should now be visible
-    expect(wrapper.text()).toMatchInlineSnapshot();
-  });
-
-  it('works correctly with store', () => {
+    // still need to mock out service.list request
     fetch.once(JSON.stringify({}));
+    const wrapper = mount(<CaseDetails {...props} />);
+
+    // resolve promise to get event
+    await flushPromises();
+    await new Promise<unknown>(resolve => setImmediate(resolve));
+
+    // rendered text
+    expect(wrapper.find('Ripple').length).toEqual(0);
+    expect(wrapper.text()).toMatchSnapshot(`Case Details`);
+  });
+
+  it('works well with store', async () => {
+    // still need to mock out service.list request
+    fetch.once(JSON.stringify(rawEvent1));
+    const props = {
+      ...globalProps,
+      eventId: rawEvent1._id,
+    };
     const wrapper = mount(
       <Provider store={store}>
         <ConnectedCaseDetails {...props} />
       </Provider>
     );
-    // should basically be able to render the correct output
-    expect(wrapper.find(CaseDetails).text()).toMatchInlineSnapshot();
+
+    // at this point event is null, thus should be loading
+    expect(wrapper.find('Ripple').length).toEqual(1);
+
+    // resolve promise to get event
+    await flushPromises();
+    await new Promise<unknown>(resolve => setImmediate(resolve));
+    wrapper.update();
+
+    // rendered text
+    expect(wrapper.find('Ripple').length).toEqual(0);
+    expect(wrapper.text()).toMatchSnapshot(`Case Details`);
   });
 
-  it('Makes the correct api calls', () => {
-    fetch.once(JSON.stringify({}));
-    const wrapper = mount(<CaseDetails {...props} />);
-    expect(fetch).toHaveBeenCalledWith({});
+  it('Makes the correct api calls', async () => {
+    fetch.once(JSON.stringify(rawEvent1));
+    const props = {
+      ...globalProps,
+      eventId: rawEvent1.baseEntityId,
+    };
+    mount(
+      <Provider store={store}>
+        <ConnectedCaseDetails {...props} />
+      </Provider>
+    );
+
+    await flushPromises();
+
+    expect(fetch.mock.calls[0]).toEqual([
+      'https://reveal-stage.smartregister.org/opensrp/rest/event/findById?id=88684506-605d-41db-b904-efbaf9795d2a',
+      {
+        headers: {
+          accept: 'application/json',
+          authorization: 'Bearer null',
+          'content-type': 'application/json;charset=UTF-8',
+        },
+        method: 'GET',
+      },
+    ]);
   });
 });
