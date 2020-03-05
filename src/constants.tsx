c65b51528e5cb917f935acfe4c229c503d86a5df
p-netm <pmuriuki98@gmail.com>
2020-03-05 13:06:03 +0300

    Fix opensrp endpoint for seaching events

--- src/constants.tsx
+++ src/constants.tsx
@@ -88,7 +88,7 @@ export const OPENSRP_ORG_PRACTITIONER_ENDPOINT = 'organization/practitioner';
 export const OPENSRP_DEL_PRACTITIONER_ROLE_ENDPOINT = 'practitionerRole/deleteByPractitioner';
 export const OPENSRP_ADD_PRACTITIONER_ROLE_ENDPOINT = 'practitionerRole/add';
 export const OPENSRP_USERS_ENDPOINT = 'user';
-export const OPENSRP_FIND_EVENTS_ENDPOINT = 'rest/event/findById';
+export const OPENSRP_FIND_EVENTS_ENDPOINT = 'event/findById';
 
 // colors
 export const GREEN = 'Green';
