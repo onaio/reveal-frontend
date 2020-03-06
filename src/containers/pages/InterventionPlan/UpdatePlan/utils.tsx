5131622a0f1459e219b756fc6fadac1a148362a9
p-netm <pmuriuki98@gmail.com>
2020-03-06 20:32:44 +0300

    Add docStrings to Updatplan utils

--- src/containers/pages/InterventionPlan/UpdatePlan/utils.tsx
+++ src/containers/pages/InterventionPlan/UpdatePlan/utils.tsx
@@ -2,6 +2,10 @@ import { FAILED_TO_GET_EVENT_ID } from '../../../../configs/lang';
 import { PlanDefinition, UseContext } from '../../../../configs/settings';
 import { displayError } from '../../../../helpers/errors';
 
+/** util function to get the event id for case triggered plans
+ * @params {PlanDefinition | null} aPlan - the plan to get the eventId from
+ * @returns {string| null} The event id if present, null otherwise
+ */
 export const getEventId = (aPlan: PlanDefinition | null): string | null => {
   if (!aPlan) {
     return null;
@@ -17,6 +21,10 @@ export const getEventId = (aPlan: PlanDefinition | null): string | null => {
   return contextOfInterest[0].valueCodableConcept;
 };
 
+/** Util function to check if a plan was case Triggered or is just routine
+ * @params {PlanDefinition} aPlan - the plan under inspection
+ * @returns {boolean} - if the plan is reactive
+ */
 export const planIsReactive = (aPlan: PlanDefinition | null): boolean => {
   if (aPlan === null) {
     return false;
