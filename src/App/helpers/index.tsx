65f1b354f14db7599805a9ea55e49a857f2d1bd8
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2020-03-13 11:06:55 +0300

    Refactor code

--- src/App/helpers/index.tsx
+++ src/App/helpers/index.tsx
@@ -1,13 +1,14 @@
+import { ComponentClass, FunctionComponent } from 'react';
 import WithGATracker from '../../components/page/GoogleAnalytics/WithGATracker';
 import { GA_ROUTE_COMPONENT } from '../../constants';
 
 /**
  * Return component to be tracked by google analytics
  *
- * @param ComponentToTrack React component to be tracked
- * @param trackingMethod Tracking approach
+ * @param {ComponentClass | FunctionComponent} ComponentToTrack React component to be tracked
+ * @param {string} trackingMethod Tracking approach
  */
 export const trackedComponent = (
-  ComponentToTrack: React.ComponentClass | React.FunctionComponent | React.ComponentClass<any, any>,
+  ComponentToTrack: ComponentClass | FunctionComponent | ComponentClass<any, any>,
   trackingMethod: string = GA_ROUTE_COMPONENT
 ) => (trackingMethod === GA_ROUTE_COMPONENT ? ComponentToTrack : WithGATracker(ComponentToTrack));
