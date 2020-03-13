7a5b3fcb85cd55a02ea0f9e61d1b4798102deca4
p-netm <pmuriuki98@gmail.com>
2020-03-13 18:45:12 +0300

    Fix the application auto logging out bug

--- src/components/page/CustomCallback/tests/index.test.tsx
+++ src/components/page/CustomCallback/tests/index.test.tsx
@@ -2,12 +2,17 @@ import { mount } from 'enzyme';
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { Route, Switch } from 'react-router-dom';
-import { SuccessfulLoginComponent } from '..';
+import { SuccessfulLoginComponent, UnSuccessfulLogin } from '..';
+import { EXPRESS_LOGIN_URL } from '../../../../constants';
 
 const App = () => {
   return (
     <Switch>
       <Route exact={true} path="/callback" component={SuccessfulLoginComponent} />
+      {/* tslint:disable-next-line: jsx-no-lambda */}
+      <Route exact={true} path="/failedCallback" component={UnSuccessfulLogin} />
+      {/* tslint:disable-next-line: jsx-no-lambda */}
+      <Route path="/login" component={() => <div id="login" />} />
       {/* tslint:disable-next-line: jsx-no-lambda */}
       <Route path="/teams" component={() => <div id="teams" />} />
       {/* tslint:disable-next-line: jsx-no-lambda */}
@@ -17,7 +22,6 @@ const App = () => {
     </Switch>
   );
 };
-
 describe('src/components/page/CustomCallback.SuccessfulLogin', () => {
   it('renders correctly', () => {
     const wrapper = mount(
@@ -59,5 +63,73 @@ describe('src/components/page/CustomCallback.SuccessfulLogin', () => {
     );
     // should redirect to plans
     expect(wrapper.find('#plans').length).toEqual(1);
+  });
+});
+
+describe('src/components/page/CustomCallback.UnsuccessfulLogin', () => {
+  const ActualWindowLocation = window.location;
+  afterAll(() => {
+    window.location = ActualWindowLocation;
+  });
+  delete window.location;
+  const applyHrefMock = (mock: jest.Mock) => {
+    (window.location as any) = {
+      set href(url: string) {
+        mock(url);
+      },
+    };
+  };
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+  });
+
+  it('renders unsuccessful login correctly', () => {
+    const hrefMock = jest.fn();
+    applyHrefMock(hrefMock);
+    mount(
+      <MemoryRouter
+        initialEntries={[{ pathname: `/failedCallback`, search: '', hash: '', state: {} }]}
+      >
+        <App />
+      </MemoryRouter>
+    );
+    expect(hrefMock).toBeCalledWith(EXPRESS_LOGIN_URL);
+  });
+
+  it('Appends correct path when searchParams is given', () => {
+    const hrefMock = jest.fn();
+    applyHrefMock(hrefMock);
+    mount(
+      <MemoryRouter
+        initialEntries={[
+          { pathname: `/failedCallback`, search: '?next=%2Fteams', hash: '', state: {} },
+        ]}
+      >
+        <App />
+      </MemoryRouter>
+    );
+    expect(hrefMock).toBeCalledWith(`${EXPRESS_LOGIN_URL}?next=%2Fteams`);
+  });
+
+  it('Redirects only to login if searchParam url is not allowable', () => {
+    const hrefMock = jest.fn();
+    applyHrefMock(hrefMock);
+    mount(
+      <MemoryRouter
+        initialEntries={[
+          {
+            hash: '',
+            pathname: `/failedCallback`,
+            search: '?next=%2Flogout',
+            state: {},
+          },
+        ]}
+      >
+        <App />
+      </MemoryRouter>
+    );
+    expect(hrefMock).toBeCalledWith(EXPRESS_LOGIN_URL);
   });
 });
