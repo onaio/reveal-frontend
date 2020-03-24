bf7ef3fdf76659e607e488d360cff1db12b94358
p-netm <pmuriuki98@gmail.com>
2020-03-24 10:49:47 +0300

    Move loadPlan logic to own folder for testing

--- src/containers/pages/InterventionPlan/IRS/plan/index.tsx
+++ src/containers/pages/InterventionPlan/IRS/plan/index.tsx
@@ -115,7 +115,6 @@ import plansReducer, {
   extractPlanRecordResponseFromPlanPayload,
   fetchPlanRecords,
   getPlanRecordById,
-  PlanPayload,
   PlanRecord,
   PlanStatus,
   reducerName as plansReducerName,
@@ -134,6 +133,7 @@ import AssignTeamTableCell, {
 } from '../../../../../components/forms/AssignTeamTableCell';
 import { ADMN0_PCODE, JurisdictionTypes } from '../../../../../configs/types';
 import './../../../../../styles/css/drill-down-table.css';
+import { loadPlan } from './serviceCalls';
 import './style.css';
 
 /** register the plans reducer */
@@ -259,14 +259,9 @@ class IrsPlan extends React.Component<
 
     // GET PLAN
     if (planId && !planById) {
-      await OpenSrpPlanService.read(planId)
-        .then((plan: PlanPayload[]) => {
-          const planRecord = extractPlanRecordResponseFromPlanPayload(plan[0]);
-          if (planRecord) {
-            return fetchPlansActionCreator([planRecord]);
-          }
-        })
-        .catch(err => err);
+      await loadPlan(OpenSRPService, planId, fetchPlansActionCreator).catch(err =>
+        displayError(err)
+      );
     } else if (planById) {
       this.setState({ newPlan: planById });
     }
