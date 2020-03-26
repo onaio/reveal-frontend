0a52d0f88e44acf977015defae9c9f8a06902a83
p-netm <pmuriuki98@gmail.com>
2020-03-26 15:23:26 +0300

    Remove duplicate code

--- src/components/forms/OrganizationSelect/tests/index.test.tsx
+++ src/components/forms/OrganizationSelect/tests/index.test.tsx
@@ -3,17 +3,22 @@ import { mount, shallow } from 'enzyme';
 import React from 'react';
 import { Provider } from 'react-redux';
 import OrganizationSelect from '..';
+import ConnectedOrganizationSelect from '..';
 import store from '../../../../store';
 import assignmentReducer, {
+  fetchAssignments,
   reducerName as assignmentReducerName,
 } from '../../../../store/ducks/opensrp/assignments';
 import organizationsReducer, {
   fetchOrganizations,
   reducerName as organizationsReducerName,
 } from '../../../../store/ducks/opensrp/organizations';
-import { organizations } from '../../../../store/ducks/tests/fixtures';
+import * as ducksFixtures from '../../../../store/ducks/tests/fixtures';
 
 jest.mock('../../../../configs/env');
+
+// tslint:disable-next-line: no-var-requires
+const fetch = require('jest-fetch-mock');
 
 reducerRegistry.register(assignmentReducerName, assignmentReducer);
 reducerRegistry.register(organizationsReducerName, organizationsReducer);
@@ -34,7 +39,7 @@ describe('/containers/forms/OrganizationSelect', () => {
   });
 
   it('renders select options correctly', () => {
-    store.dispatch(fetchOrganizations(organizations));
+    store.dispatch(fetchOrganizations(ducksFixtures.organizations));
 
     const props = {
       jurisdictionId: 'outpost-number-one',
@@ -56,5 +61,34 @@ describe('/containers/forms/OrganizationSelect', () => {
         .props()
     ).toMatchSnapshot('Organization Select Props');
     expect(wrapper.find('.reveal__indicator').length).toBe(2);
+  });
+
+  it('works with store', () => {
+    /** check mapState to props passes expected props to dumb component */
+
+    const { assignments, organizations } = ducksFixtures;
+    // fetch organizations dispatch assignments.
+    store.dispatch(fetchAssignments(assignments));
+    fetch.once(JSON.stringify(organizations));
+    const props = {
+      jurisdictionId: '',
+      planId: 'alpha',
+    };
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <ConnectedOrganizationSelect {...props} />
+      </Provider>
+    );
+
+    const passedProps: any = wrapper.find('OrganizationSelect').props();
+    // assignments
+    expect(passedProps.assignments).toEqual(assignments);
+    // jurisdictionId
+    expect(passedProps.jurisdictionId).toEqual('');
+    // organizations
+    expect(passedProps.organizations).toEqual(organizations);
+    // planId
+    expect(passedProps.planId).toEqual('alpha');
   });
 });
