84367feab630c7134c502c950472d055aa33a78e
p-netm <pmuriuki98@gmail.com>
2020-04-01 20:23:31 +0300

    Refactor Irs plans page

--- src/containers/pages/InterventionPlan/IRS/index.tsx
+++ src/containers/pages/InterventionPlan/IRS/index.tsx
@@ -9,7 +9,6 @@ import { Store } from 'redux';
 
 import DrillDownTable from '@onaio/drill-down-table';
 import reducerRegistry, { Registry } from '@onaio/redux-reducer-registry';
-import superset from '@onaio/superset-connector';
 
 import {
   CREATE_NEW_PLAN,
@@ -24,14 +23,13 @@ import {
   STATUS_HEADER,
 } from '../../../../configs/lang';
 import {
-  DRAFT,
+  DRAFT_IRS_PLAN_URL,
   HOME_URL,
   INTERVENTION_IRS_DRAFTS_URL,
   INTERVENTION_IRS_URL,
   NEW,
-  PLAN,
+  OPENSRP_PLANS,
   PLAN_RECORD_BY_ID,
-  REPORT,
 } from '../../../../constants';
 
 import { displayError } from '../../../../helpers/errors';
@@ -40,10 +38,8 @@ import { OpenSRPService } from '../../../../services/opensrp';
 import plansReducer, {
   fetchPlanRecords,
   fetchPlans,
-  getPlansArray,
   InterventionType,
   makePlansArraySelector,
-  Plan,
   PlanPayload,
   PlanRecord,
   PlanRecordResponse,
@@ -57,23 +53,19 @@ import HeaderBreadcrumbs, {
   BreadCrumbProps,
 } from '../../../../components/page/HeaderBreadcrumb/HeaderBreadcrumb';
 import Loading from '../../../../components/page/Loading';
-import { SUPERSET_IRS_REPORTING_PLANS_SLICE, SUPERSET_MAX_RECORDS } from '../../../../configs/env';
-import { planStatusDisplay, useContextCodes } from '../../../../configs/settings';
-import supersetFetch from '../../../../services/superset';
+import { planStatusDisplay } from '../../../../configs/settings';
 import './../../../../styles/css/drill-down-table.css';
 
 /** register the plans reducer */
 reducerRegistry.register(plansReducerName, plansReducer);
 
 /** initialize OpenSRP API services */
-const OpenSrpPlanService = new OpenSRPService('plans');
 
 /** IrsPlansProps - interface for IRS Plans page */
 export interface IrsPlansProps {
   fetchPlanRecordsActionCreator: typeof fetchPlanRecords;
   fetchPlansActionCreator: typeof fetchPlans;
-  isReporting: boolean;
-  pageTitle: string;
+  service: typeof OpenSRPService;
   plansArray: PlanRecord[];
 }
 
@@ -81,181 +73,134 @@ export interface IrsPlansProps {
 export const defaultIrsPlansProps: IrsPlansProps = {
   fetchPlanRecordsActionCreator: fetchPlanRecords,
   fetchPlansActionCreator: fetchPlans,
-  isReporting: false,
-  pageTitle: '',
   plansArray: [],
+  service: OpenSRPService,
 };
 
 /** Plans filter selector */
 const plansArraySelector = makePlansArraySelector(PLAN_RECORD_BY_ID);
 
-/** IrsPlans - component for IRS Plans page */
-class IrsPlans extends React.Component<IrsPlansProps & RouteComponentProps<RouteParams>, {}> {
-  public static defaultProps: IrsPlansProps = defaultIrsPlansProps;
-  constructor(props: RouteComponentProps<RouteParams> & IrsPlansProps) {
-    super(props);
+/** fetch all types of plans */
+const loadOpenSRPPlans = (service: typeof OpenSRPService) => {
+  const OpenSrpPlanService = new service(OPENSRP_PLANS);
+  OpenSrpPlanService.list()
+    .then((plans: PlanPayload[]) => {
+      const extractedPlanRecords = plans
+        .map(plan => extractPlanRecordResponseFromPlanPayload(plan))
+        .filter(plan => !!plan);
+      fetchPlanRecords(extractedPlanRecords as PlanRecordResponse[]);
+    })
+    .catch(err => displayError(err));
+};
+
+export const IrsPlans = (props: IrsPlansProps & RouteComponentProps<RouteParams>) => {
+  React.useEffect(() => {
+    loadOpenSRPPlans(props.service);
+  }, []);
+
+  const pageTitle = `${IRS_PLANS}${DRAFTS_PARENTHESIS}`;
+  const homePage = {
+    label: HOME,
+    url: HOME_URL,
+  };
+  const breadCrumbProps: BreadCrumbProps = {
+    currentPage: {
+      label: pageTitle,
+      url: INTERVENTION_IRS_DRAFTS_URL,
+    },
+    pages: [homePage],
+  };
+
+  const { plansArray } = props;
+  if (plansArray.length === 0) {
+    return <Loading />;
   }
 
-  public async componentDidMount() {
-    const { isReporting, fetchPlanRecordsActionCreator, fetchPlansActionCreator } = this.props;
-
-    /** define superset filter params for jurisdictions */
-    const plansParams = superset.getFormData(SUPERSET_MAX_RECORDS, [
-      { comparator: 'IRS', operator: '==', subject: 'plan_intervention_type' },
-    ]);
-    if (isReporting) {
-      // use superset endpoint in reporting
-      const plansArray = await supersetFetch(SUPERSET_IRS_REPORTING_PLANS_SLICE, plansParams).then(
-        (plans: Plan[]) => plans
-      );
-      fetchPlansActionCreator(plansArray);
-    } else {
-      // Use openSRP endpoint in planning
-      OpenSrpPlanService.list()
-        .then(plans => {
-          // filter for IRS plans
-          const irsPlans = plans.filter((p: PlanPayload) => {
-            for (const u of p.useContext) {
-              if (u.code === useContextCodes[0]) {
-                if (u.valueCodableConcept === InterventionType.IRS) {
-                  return true;
-                } else {
-                  return false;
-                }
-              }
-            }
-            return false;
-          });
-          const irsPlanRecords: PlanRecordResponse[] = irsPlans.map(
-            extractPlanRecordResponseFromPlanPayload
-          );
-          return fetchPlanRecordsActionCreator(irsPlanRecords);
-        })
-        .catch(error => displayError(error));
-    }
-  }
-
-  public render() {
-    const { isReporting, pageTitle } = this.props;
-    const homePage = {
-      label: HOME,
-      url: HOME_URL,
-    };
-    const breadCrumbProps: BreadCrumbProps = {
-      currentPage: {
-        label: pageTitle,
-        url: INTERVENTION_IRS_URL,
-      },
-      pages: [homePage],
-    };
-
-    const { plansArray } = this.props;
-    if (plansArray.length === 0) {
-      return <Loading />;
-    }
-
-    const columns: Column[] = [
-      {
-        Header: NAME,
-        columns: [
-          {
-            Cell: (cell: CellInfo) => {
-              const path = isReporting
-                ? REPORT
-                : cell.original.plan_status === DRAFT
-                ? DRAFT
-                : PLAN;
-              return (
-                <div>
-                  <Link
-                    to={`${INTERVENTION_IRS_URL}/${path}/${cell.original.id ||
-                      cell.original.plan_id}`}
-                  >
-                    {cell.value}
-                  </Link>
-                </div>
-              );
-            },
-            Header: '',
-            accessor: 'plan_title',
-            minWidth: 200,
+  const columns: Column[] = [
+    {
+      Header: NAME,
+      columns: [
+        {
+          Cell: (cell: CellInfo) => {
+            return (
+              <div>
+                <Link to={`${DRAFT_IRS_PLAN_URL}/${cell.original.id || cell.original.plan_id}`}>
+                  {cell.value}
+                </Link>
+              </div>
+            );
           },
-        ],
-      },
-      {
-        Header: DATE_CREATED,
-        columns: [
-          {
-            Header: '',
-            accessor: 'plan_date',
-          },
-        ],
-      },
-      {
-        Header: STATUS_HEADER,
-        columns: [
-          {
-            Header: '',
-            accessor: (d: PlanRecord) => planStatusDisplay[d.plan_status] || d.plan_status,
-            id: 'plan_status',
-          },
-        ],
-      },
-    ];
+          Header: '',
+          accessor: 'plan_title',
+          minWidth: 200,
+        },
+      ],
+    },
+    {
+      Header: DATE_CREATED,
+      columns: [
+        {
+          Header: '',
+          accessor: 'plan_date',
+        },
+      ],
+    },
+    {
+      Header: STATUS_HEADER,
+      columns: [
+        {
+          Header: '',
+          accessor: (d: PlanRecord) => planStatusDisplay[d.plan_status] || d.plan_status,
+          id: 'plan_status',
+        },
+      ],
+    },
+  ];
 
-    /** tableProps - props for DrillDownTable component */
-    const tableProps = {
-      CellComponent: DrillDownTableLinkedCell,
-      columns,
-      data: [...plansArray],
-      identifierField: 'id',
-      linkerField: 'id',
-      minRows: 0,
-      nextText: NEXT,
-      previousText: PREVIOUS,
-      rootParentId: null,
-      showPageSizeOptions: false,
-      showPagination: plansArray.length > 20,
-      useDrillDownTrProps: false,
-    };
+  /** tableProps - props for DrillDownTable component */
+  const tableProps = {
+    CellComponent: DrillDownTableLinkedCell,
+    columns,
+    data: [...plansArray],
+    identifierField: 'id',
+    linkerField: 'id',
+    minRows: 0,
+    nextText: NEXT,
+    previousText: PREVIOUS,
+    rootParentId: null,
+    showPageSizeOptions: false,
+    showPagination: plansArray.length > 20,
+    useDrillDownTrProps: false,
+  };
 
-    return (
-      <div className="mb-5">
-        <Helmet>
-          <title>{pageTitle.length ? pageTitle : IRS_TITLE}</title>
-        </Helmet>
-        <HeaderBreadcrumbs {...breadCrumbProps} />
-        <h2 className="page-title">{pageTitle}</h2>
-        <DrillDownTable {...tableProps} />
-        <br />
-        <Button color="primary" tag={Link} to={`${INTERVENTION_IRS_URL}/${NEW}`}>
-          {CREATE_NEW_PLAN}
-        </Button>
-      </div>
-    );
-  }
-}
-
-export { IrsPlans };
+  return (
+    <div className="mb-5">
+      <Helmet>
+        <title>{pageTitle.length ? pageTitle : IRS_TITLE}</title>
+      </Helmet>
+      <HeaderBreadcrumbs {...breadCrumbProps} />
+      <h2 className="page-title">{pageTitle}</h2>
+      <DrillDownTable {...tableProps} />
+      <br />
+      <Button color="primary" tag={Link} to={`${INTERVENTION_IRS_URL}/${NEW}`}>
+        {CREATE_NEW_PLAN}
+      </Button>
+    </div>
+  );
+};
 
 interface DispatchedStateProps {
   plansArray: PlanRecord[];
 }
 
 const mapStateToProps = (state: Partial<Store>, ownProps: any): DispatchedStateProps => {
-  const isDraftsList = ownProps.path === INTERVENTION_IRS_DRAFTS_URL;
   const planStatus = [PlanStatus.DRAFT];
-  const pageTitle = `${IRS_PLANS}${isDraftsList ? ` ${DRAFTS_PARENTHESIS}` : ''}`;
   const plansRecordsArray = plansArraySelector(state as Registry, {
     interventionType: InterventionType.IRS,
     statusList: planStatus,
   });
-  const plansArray = getPlansArray(state, InterventionType.IRS, planStatus);
-
   const props = {
-    isReporting: !isDraftsList,
-    pageTitle,
-    plansArray: plansRecordsArray.length ? plansRecordsArray : plansArray,
+    plansArray: plansRecordsArray,
     ...ownProps,
   };
   return props;
