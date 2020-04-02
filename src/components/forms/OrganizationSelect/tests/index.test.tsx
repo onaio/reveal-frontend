9b2b4c4132ac64c8f757f53863085a1610b386a9
p-netm <pmuriuki98@gmail.com>
2020-04-02 14:24:30 +0300

    Test handleChange callback

--- src/components/forms/OrganizationSelect/tests/index.test.tsx
+++ src/components/forms/OrganizationSelect/tests/index.test.tsx
@@ -2,12 +2,12 @@ import reducerRegistry from '@onaio/redux-reducer-registry';
 import { mount, shallow } from 'enzyme';
 import React from 'react';
 import { Provider } from 'react-redux';
-import OrganizationSelect from '..';
-import ConnectedOrganizationSelect from '..';
+import ConnectedOrganizationSelect, { OrganizationSelect } from '..';
 import store from '../../../../store';
 import assignmentReducer, {
   fetchAssignments,
   reducerName as assignmentReducerName,
+  resetPlanAssignments,
 } from '../../../../store/ducks/opensrp/assignments';
 import organizationsReducer, {
   fetchOrganizations,
@@ -23,9 +23,12 @@ const fetch = require('jest-fetch-mock');
 reducerRegistry.register(assignmentReducerName, assignmentReducer);
 reducerRegistry.register(organizationsReducerName, organizationsReducer);
 
+jest.mock('../../../../helpers/errors');
+
 describe('/containers/forms/OrganizationSelect', () => {
   beforeEach(() => {
     jest.resetAllMocks();
+    jest.clearAllMocks();
   });
 
   it('renders without crashing', () => {
@@ -33,9 +36,15 @@ describe('/containers/forms/OrganizationSelect', () => {
 
     mount(
       <Provider store={store}>
-        <OrganizationSelect jurisdictionId="" name="" planId="" />
+        <ConnectedOrganizationSelect jurisdictionId="" name="" planId="" />
       </Provider>
     );
+  });
+
+  it('displays errors ', async () => {
+    fetch.mockReject(new Error('Request failed'));
+    mount(<OrganizationSelect />);
+    await new Promise(resolve => setImmediate(resolve));
   });
 
   it('renders select options correctly', () => {
@@ -49,7 +58,7 @@ describe('/containers/forms/OrganizationSelect', () => {
 
     const wrapper = mount(
       <Provider store={store}>
-        <OrganizationSelect {...props} />
+        <ConnectedOrganizationSelect {...props} />
       </Provider>
     );
 
@@ -90,5 +99,74 @@ describe('/containers/forms/OrganizationSelect', () => {
     expect(passedProps.organizations).toEqual(organizations);
     // planId
     expect(passedProps.planId).toEqual('alpha');
+
+    wrapper.unmount();
+  });
+
+  it('handleChange works correctly for nominal case', () => {
+    const resetPlansAssignmentMock = jest.fn(args => resetPlanAssignments(args));
+    const fetchAssignmentMock = jest.fn(args => fetchAssignments(args));
+
+    const props = {
+      assignments: ducksFixtures.assignments,
+      fetchAssignmentsAction: fetchAssignmentMock,
+      jurisdictionId: '',
+      organizations: ducksFixtures.organizations,
+      planId: 'alpha',
+      resetPlanAssignmentsAction: resetPlansAssignmentMock,
+    };
+    const wrapper = mount(
+      <Provider store={store}>
+        <OrganizationSelect {...props} />
+      </Provider>
+    );
+
+    // simulate a change; check fetchAssignments is called correctly
+    const selectInstance = wrapper.find('Select').instance();
+    (selectInstance as any).selectOption({
+      label: 'Demo Team',
+      value: '4c506c98-d3a9-11e9-bb65-2a2ae2dbcce4',
+    });
+    wrapper.update();
+    const createdAssignment1 = {
+      jurisdiction: '',
+      organization: '4c506c98-d3a9-11e9-bb65-2a2ae2dbcce4',
+      plan: 'alpha',
+    };
+    // check that that was successfully picked up
+    expect(resetPlansAssignmentMock).not.toHaveBeenCalled();
+    expect(fetchAssignmentMock).toHaveBeenCalledWith([
+      ...ducksFixtures.assignments,
+      createdAssignment1,
+    ]);
+
+    fetchAssignmentMock.mockClear();
+
+    // select a second option
+
+    (selectInstance as any).selectOption({
+      label: 'Takang 1',
+      value: 'd23f7350-d406-11e9-bb65-2a2ae2dbcce4',
+    });
+    wrapper.update();
+    // check second option also picked up.
+    const createdAssignment2 = {
+      jurisdiction: '',
+      organization: 'd23f7350-d406-11e9-bb65-2a2ae2dbcce4',
+      plan: 'alpha',
+    };
+
+    expect(resetPlansAssignmentMock).not.toHaveBeenCalled();
+    expect(fetchAssignmentMock).toHaveBeenCalledWith([
+      ...ducksFixtures.assignments,
+      createdAssignment2,
+    ]);
+
+    wrapper.find('Select').simulate('keyDown', { key: 'Backspace', keyCode: 8 });
+    wrapper.update();
+
+    expect(resetPlansAssignmentMock).toHaveBeenCalledWith({
+      alpha: ducksFixtures.assignments,
+    });
   });
 });
