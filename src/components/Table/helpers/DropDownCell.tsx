cd3a8c56d59d757ff1245cffc22125c80d9fc0a3
p-netm <pmuriuki98@gmail.com>
2020-05-04 12:34:43 +0300

    Initial draft on a Drilldown based on react-table v7

---
+++ src/components/Table/helpers/DropDownCell.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Cell } from 'react-table';
+import { CARET, CLICKABLE_CSS_CLASS, LINKER_ITEM_CSS_CLASS } from './constants';
+
+/** Interface for DropDown cell props */
+export interface DropDownCellProps {
+  cell: Cell;
+  cellValue: Node;
+  hasChildren: boolean;
+}
+
+/** Component that will be rendered in drop-down table cells showing a caret
+ * that indicates if you can drill-down a row of data or not.
+ */
+const DropDownCell: React.ElementType = (props: DropDownCellProps) => {
+  const { cellValue, hasChildren } = props;
+  return (
+    <div className={hasChildren ? CLICKABLE_CSS_CLASS : LINKER_ITEM_CSS_CLASS}>
+      <span>
+        {cellValue}
+        {hasChildren && CARET}
+      </span>
+    </div>
+  );
+};
+
+export default DropDownCell;
