555c0b431350191b3e4f5df4983f560d73eb5a74
p-netm <pmuriuki98@gmail.com>
2020-05-19 20:50:48 +0300

    Scaffolding component for filters displayed as dropdowns

---
+++ src/components/DropDownRenderer/index.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { Dropdown, DropdownToggle } from 'reactstrap';
+import './index.css';
+
+/** DropDownRenderer props */
+export interface DropDownRendererProps {
+  renderMenu: () => React.ReactNode;
+  renderToggle: () => React.ReactNode;
+}
+
+/** default props */
+export const defaultDropDownRenderProps = {
+  renderMenu: () => null,
+  renderToggle: () => null,
+};
+
+/** a wrapper used to render filter buttons on the table's filter bar */
+export const DropDownRenderer = (props: DropDownRendererProps = defaultDropDownRenderProps) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  return (
+    <Dropdown
+      size="sm"
+      className="mr-3 drop-down-container"
+      style={{ display: 'inline' }}
+      isOpen={isOpen}
+      toggle={toggle}
+    >
+      <DropdownToggle outline={true} className="filter-bar-btns">
+        {props.renderToggle()}
+      </DropdownToggle>
+      {props.renderMenu()}
+    </Dropdown>
+  );
+};
