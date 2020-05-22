248d09e1ba222a6296dee0787e2938ba3a277b05
Kahama <kahama94@gmail.com>
2020-05-22 16:20:27 +0300

    Add jurisdiction select to upload from

--- src/containers/pages/MDAPoint/ClientUpload/index.tsx
+++ src/containers/pages/MDAPoint/ClientUpload/index.tsx
@@ -1,25 +1,37 @@
-import { Formik } from 'formik';
+import { Dictionary } from '@onaio/utils';
+import { ErrorMessage, Field, Formik } from 'formik';
 import React, { useState } from 'react';
 import { Redirect } from 'react-router';
 import { Button, FormGroup, Input, Label, Modal, ModalBody, ModalHeader } from 'reactstrap';
 import * as Yup from 'yup';
+import JurisdictionSelect from '../../../../components/forms/JurisdictionSelect';
 import LinkAsButton from '../../../../components/LinkAsButton';
+import { REQUIRED } from '../../../../configs/lang';
 import { STUDENTS_LIST_URL } from '../../../../constants';
 import { postUploadedFile } from '../ClientListView/helpers/serviceHooks';
 import UploadStatus from '../ClientUploadStatus/';
 export const uploadValidationSchema = Yup.object().shape({
   file: Yup.mixed().required(),
+  jurisdictions: Yup.object().shape({
+    id: Yup.string().required(REQUIRED),
+    name: Yup.string(),
+  }),
 });
 /** interface to describe props for ExportForm component */
 export interface UploadFormField {
   file: string | null;
   location: any;
+  jurisdictions: Dictionary;
 }
 export const ClientUpload = (props: any) => {
   const [selectedFile, setSelectedFile] = useState<any>(null);
   const [ifDoneHere, setIfDoneHere] = useState<boolean>(false);
   const defaultInitialValues: UploadFormField = {
     file: null,
+    jurisdictions: {
+      id: '',
+      name: '',
+    },
     location: props.location,
   };
   const closeUploadModal = {
@@ -42,15 +54,44 @@ export const ClientUpload = (props: any) => {
             initialValues={defaultInitialValues}
             validationSchema={uploadValidationSchema}
             // tslint:disable-next-line: jsx-no-lambda
-            onSubmit={async (_, { setSubmitting }) => {
+            onSubmit={async (values, { setSubmitting }) => {
               const setSubmittingStatus = () => setSubmitting(false);
               const data = new FormData();
               data.append('file', selectedFile);
-              await postUploadedFile(data, setStateIfDone, setSubmittingStatus);
+              const uploadParams = `&location_id=${values.jurisdictions.id}`;
+              await postUploadedFile(data, setStateIfDone, setSubmittingStatus, uploadParams);
             }}
           >
             {({ values, setFieldValue, handleSubmit, errors, isSubmitting }) => (
               <form onSubmit={handleSubmit} data-enctype="multipart/form-data">
+                <FormGroup className={'async-select-container'}>
+                  <Label for={`jurisdictions-${1}-id`}>{'Geographical level to include'}</Label>
+                  &nbsp;
+                  <div style={{ display: 'inline-block', width: '24rem' }}>
+                    <Field
+                      required={true}
+                      component={JurisdictionSelect}
+                      cascadingSelect={true}
+                      name={`jurisdictions.id`}
+                      id={`jurisdictions-id`}
+                      className={'async-select'}
+                      labelFieldName={`jurisdictions.name`}
+                    />
+                  </div>
+                  <Field type="hidden" name={`jurisdictions.name`} id={`jurisdictions-name`} />
+                  {errors.jurisdictions && (
+                    <small className="form-text text-danger jurisdictions-error">
+                      {'Please select location'}
+                    </small>
+                  )}
+                  {
+                    <ErrorMessage
+                      name={`jurisdictions.id`}
+                      component="small"
+                      className="form-text text-danger"
+                    />
+                  }
+                </FormGroup>
                 <FormGroup>
                   <Label for="upload-file">Upload File</Label>
                   <Input
