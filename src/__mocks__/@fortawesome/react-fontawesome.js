bda4361f219868bcdefea12b7dff8479c66fc93d
p-netm <pmuriuki98@gmail.com>
2020-05-26 14:29:07 +0300

    Mock out fontawesome

---
+++ src/__mocks__/@fortawesome/react-fontawesome.js
@@ -0,0 +1,7 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable import/prefer-default-export */
+import React from 'react';
+
+export function FontAwesomeIcon() {
+  return <i className="fa" />;
+}
