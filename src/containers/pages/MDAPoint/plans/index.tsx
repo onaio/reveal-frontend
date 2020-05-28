33bccf31d10d0decef62d0af26ba076d838529a7
ciremusyoka <wambua.eric01@gmail.com>
2020-05-28 13:13:37 +0300

    Refactor mda point page to use irs plans page

--- src/containers/pages/MDAPoint/plans/index.tsx
+++ src/containers/pages/MDAPoint/plans/index.tsx
@@ -1,125 +1,45 @@
-import ListView from '@onaio/list-view';
 import reducerRegistry from '@onaio/redux-reducer-registry';
-import React, { useEffect, useState } from 'react';
-import { Helmet } from 'react-helmet';
+import React from 'react';
 import { connect } from 'react-redux';
 import { RouteComponentProps } from 'react-router';
-import { Link } from 'react-router-dom';
-import { Col, Row } from 'reactstrap';
 import { Store } from 'redux';
-import { SearchForm } from '../../../../components/forms/Search';
-import HeaderBreadcrumb from '../../../../components/page/HeaderBreadcrumb/HeaderBreadcrumb';
-import Loading from '../../../../components/page/Loading';
 import { SUPERSET_MDA_POINT_REPORTING_PLANS_SLICE } from '../../../../configs/env';
-import {
-  DATE_CREATED,
-  END_DATE,
-  HOME,
-  MDA_POINT_PLANS,
-  START_DATE,
-  STATUS_HEADER,
-  TITLE,
-} from '../../../../configs/lang';
-import { planStatusDisplay } from '../../../../configs/settings';
-import { HOME_URL, QUERY_PARAM_TITLE, REPORT_MDA_POINT_PLAN_URL } from '../../../../constants';
-import { displayError } from '../../../../helpers/errors';
+import { MDA_POINT_PLANS } from '../../../../configs/lang';
+import { QUERY_PARAM_TITLE, REPORT_MDA_POINT_PLAN_URL } from '../../../../constants';
 import { getQueryParams } from '../../../../helpers/utils';
-import supersetFetch from '../../../../services/superset';
 import MDAPointPlansReducer, {
   fetchMDAPointPlans,
   makeMDAPointPlansArraySelector,
   MDAPointPlan,
   reducerName as MDAPointPlansReducerName,
 } from '../../../../store/ducks/generic/mdaPointPlan';
+import ConnectedIRSPlansList from '../../IRS/plans';
 
 /** register the plan definitions reducer */
 reducerRegistry.register(MDAPointPlansReducerName, MDAPointPlansReducer);
 
 /** interface for PlanList props */
 interface PlanListProps {
-  fetchPlans: typeof fetchMDAPointPlans;
   plans: MDAPointPlan[];
-  service: typeof supersetFetch;
+  ownProps: any;
+  fetchPlans: typeof fetchMDAPointPlans;
 }
 
 /** Simple component that loads the new plan form and allows you to create a new plan */
 const MDAPointPlansList = (props: PlanListProps & RouteComponentProps) => {
-  const { fetchPlans, plans, service } = props;
-  const [loading, setLoading] = useState<boolean>(false);
-  const pageTitle: string = MDA_POINT_PLANS;
-  const breadcrumbProps = {
-    currentPage: {
-      label: pageTitle,
-      url: REPORT_MDA_POINT_PLAN_URL,
-    },
-    pages: [
-      {
-        label: HOME,
-        url: HOME_URL,
-      },
-    ],
-  };
-
-  /** async function to load the data */
-  async function loadData() {
-    try {
-      setLoading(plans.length < 1); // only set loading when there are no plans
-      await service(SUPERSET_MDA_POINT_REPORTING_PLANS_SLICE).then((result: MDAPointPlan[]) =>
-        fetchPlans(result)
-      );
-    } catch (e) {
-      // do something with the error?
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    loadData().catch(error => displayError(error));
-  }, []);
-
-  const listViewData = (planList: MDAPointPlan[]) =>
-    planList.map(planObj => {
-      return [
-        <Link to={`${REPORT_MDA_POINT_PLAN_URL}/${planObj.plan_id}`} key={planObj.plan_id}>
-          {planObj.plan_title}
-        </Link>,
-        planObj.plan_date,
-        planObj.plan_effective_period_start,
-        planObj.plan_effective_period_end,
-        planStatusDisplay[planObj.plan_status] || planObj.plan_status,
-      ];
-    });
-
-  if (loading === true) {
-    return <Loading />;
-  }
-
-  const listViewProps = {
-    data: listViewData(plans),
-    headerItems: [TITLE, DATE_CREATED, START_DATE, END_DATE, STATUS_HEADER],
-    tableClass: 'table table-bordered plans-list',
+  const { plans, ownProps, fetchPlans } = props;
+  const IRSPlansListprops = {
+    ...ownProps,
+    fetchPlans,
+    pageTitle: MDA_POINT_PLANS,
+    pageUrl: REPORT_MDA_POINT_PLAN_URL,
+    plans,
+    supersetReportingSlice: SUPERSET_MDA_POINT_REPORTING_PLANS_SLICE,
   };
 
   return (
     <div>
-      <Helmet>
-        <title>{pageTitle}</title>
-      </Helmet>
-      <HeaderBreadcrumb {...breadcrumbProps} />
-      <Row>
-        <Col md={8}>
-          <h3 className="mt-3 mb-3 page-title">{pageTitle}</h3>
-        </Col>
-      </Row>
-      <hr />
-      <SearchForm history={props.history} location={props.location} />
-
-      <Row>
-        <Col>
-          <ListView {...listViewProps} />
-        </Col>
-      </Row>
+      <ConnectedIRSPlansList {...IRSPlansListprops} />
     </div>
   );
 };
@@ -127,8 +47,8 @@ const MDAPointPlansList = (props: PlanListProps & RouteComponentProps) => {
 /** Declare default props for MDAPointPlansList */
 const defaultProps: PlanListProps = {
   fetchPlans: fetchMDAPointPlans,
+  ownProps: {},
   plans: [],
-  service: supersetFetch,
 };
 
 MDAPointPlansList.defaultProps = defaultProps;
@@ -139,6 +59,7 @@ export { MDAPointPlansList };
 
 /** interface to describe props from mapStateToProps */
 interface DispatchedStateProps {
+  ownProps: any;
   plans: MDAPointPlan[];
 }
 
@@ -148,6 +69,7 @@ const mapStateToProps = (state: Partial<Store>, ownProps: any): DispatchedStateP
   const MDAPointPlansArray = makeMDAPointPlansArraySelector()(state, { plan_title: searchedTitle });
 
   return {
+    ownProps,
     plans: MDAPointPlansArray,
   };
 };
