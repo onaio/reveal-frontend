018e3a5a32f06b87a5f18d05e4229f39762b93b8
Kahama <kahama94@gmail.com>
2020-05-29 17:00:13 +0300

    Use reactstrap form

--- src/containers/pages/MDAPoint/ClientUpload/index.tsx
+++ src/containers/pages/MDAPoint/ClientUpload/index.tsx
@@ -2,7 +2,7 @@ import { Dictionary } from '@onaio/utils';
 import { ErrorMessage, Field, Formik } from 'formik';
 import React, { useState } from 'react';
 import { Redirect } from 'react-router';
-import { Button, FormGroup, Input, Label, Modal, ModalBody, ModalHeader } from 'reactstrap';
+import { Button, Form, FormGroup, Input, Label, Modal, ModalBody, ModalHeader } from 'reactstrap';
 import * as Yup from 'yup';
 <<<<<<< HEAD
 import JurisdictionSelect from '../../../../components/forms/JurisdictionSelect';
@@ -71,7 +71,7 @@ export const ClientUpload = () => {
             }}
           >
             {({ values, setFieldValue, handleSubmit, errors, isSubmitting }) => (
-              <form onSubmit={handleSubmit} data-enctype="multipart/form-data">
+              <Form onSubmit={handleSubmit} data-enctype="multipart/form-data">
                 <FormGroup className={'async-select-container'}>
                   <Label for={`jurisdictions-${1}-id`}>{'Geographical level to include'}</Label>
                   &nbsp;
@@ -150,7 +150,7 @@ export const ClientUpload = () => {
                   </Button>
                   <LinkAsButton {...closeUploadModal} />
                 </div>
-              </form>
+              </Form>
             )}
           </Formik>
         </ModalBody>
