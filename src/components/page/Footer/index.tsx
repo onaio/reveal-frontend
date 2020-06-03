a9bab426c74764fd8b1767a00019e1c5c2da8c3b
p-netm <pmuriuki98@gmail.com>
2020-06-03 14:45:39 +0300

    Add a floating logo to footer if web app is in TH

--- src/components/page/Footer/index.tsx
+++ src/components/page/Footer/index.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import { REACT_APP_NAME, REACT_APP_VERSION } from '../../../configs/env';
+import logo from '../../../assets/images/logo.png';
+import {
+  REACT_APP_NAME,
+  REACT_APP_PRODUCTION_INST,
+  REACT_APP_VERSION,
+  WEBSITE_NAME,
+} from '../../../configs/env';
+import { TH_PRODUCTION_INST } from '../../../constants';
+import './footer.css';
 
 /** footer component; to render react-version on the web ui */
 export const Footer = () => {
+  if (REACT_APP_PRODUCTION_INST === TH_PRODUCTION_INST) {
+    return (
+      <div className="reveal-floating-logo">
+        <img src={logo} alt={WEBSITE_NAME} />
+        <span className="text-muted floating-logo-text">{REACT_APP_VERSION}</span>
+      </div>
+    );
+  }
   return (
     <footer className="footer">
+      <hr className="p-0 m-0" />
       <span className="text-muted">
         {REACT_APP_NAME}: {REACT_APP_VERSION}
       </span>
