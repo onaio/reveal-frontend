7ead1fc5e0b27272531018f62cc51ffcdad2d308
p-netm <pmuriuki98@gmail.com>
2020-06-04 21:04:54 +0300

    Refactor footer section

--- src/components/page/Footer/index.tsx
+++ src/components/page/Footer/index.tsx
@@ -1,30 +1,14 @@
 import React from 'react';
-import logo from '../../../assets/images/logo.png';
-import {
-  REACT_APP_NAME,
-  REACT_APP_PRODUCTION_INST,
-  REACT_APP_VERSION,
-  WEBSITE_NAME,
-} from '../../../configs/env';
-import { TH_PRODUCTION_INST } from '../../../constants';
+import { REACT_APP_VERSION, REVEAL_BRAND_IMG_SRC, WEBSITE_NAME } from '../../../configs/env';
 import './footer.css';
 
 /** footer component; to render react-version on the web ui */
 export const Footer = () => {
-  if (REACT_APP_PRODUCTION_INST === TH_PRODUCTION_INST) {
-    return (
-      <div className="reveal-floating-logo">
-        <img src={logo} alt={WEBSITE_NAME} />
-        <span className="text-muted floating-logo-text">{REACT_APP_VERSION}</span>
-      </div>
-    );
-  }
   return (
     <footer className="footer">
-      <hr className="p-0 m-0" />
-      <span className="text-muted">
-        {REACT_APP_NAME}: {REACT_APP_VERSION}
-      </span>
+      <hr className="p-0 m-0 mb-1" />
+      <img src={REVEAL_BRAND_IMG_SRC} alt={WEBSITE_NAME} />{' '}
+      <span className="text-muted floating-logo-text">{REACT_APP_VERSION}</span>
     </footer>
   );
 };
