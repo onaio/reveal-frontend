ecedc65ba18798e235d864358e37825895a484d5
ciremusyoka <wambua.eric01@gmail.com>
2020-06-04 09:18:13 +0300

    Refactor MDA Plan list to use GenericPlanLists component

--- src/containers/pages/MDAPoint/plans/index.tsx
+++ src/containers/pages/MDAPoint/plans/index.tsx
@@ -7,39 +7,29 @@ import { SUPERSET_MDA_POINT_REPORTING_PLANS_SLICE } from '../../../../configs/en
 import { MDA_POINT_PLANS } from '../../../../configs/lang';
 import { QUERY_PARAM_TITLE, REPORT_MDA_POINT_PLAN_URL } from '../../../../constants';
 import { getQueryParams } from '../../../../helpers/utils';
+import { RouteParams } from '../../../../helpers/utils';
+import supersetFetch from '../../../../services/superset';
 import MDAPointPlansReducer, {
   fetchMDAPointPlans,
   makeMDAPointPlansArraySelector,
   MDAPointPlan,
   reducerName as MDAPointPlansReducerName,
 } from '../../../../store/ducks/generic/MDAPointPlans';
-import ConnectedIRSPlansList from '../../IRS/plans';
+import { GenericPlanListProps, GenericPlansList } from '../../GenericPlansList';
 
-/** register the plan definitions reducer */
+/** register the MDA plan definitions reducer */
 reducerRegistry.register(MDAPointPlansReducerName, MDAPointPlansReducer);
 
-/** interface for PlanList props */
-interface PlanListProps {
-  plans: MDAPointPlan[];
+// /** interface for MDA PlanList props */
+interface PlanListProps extends GenericPlanListProps {
   ownProps: any;
-  fetchPlans: typeof fetchMDAPointPlans;
 }
 
-/** Simple component that loads the new plan form and allows you to create a new plan */
+/** Simple component that loads a preview list of MDA plans */
 const MDAPointPlansList = (props: PlanListProps & RouteComponentProps) => {
-  const { plans, ownProps, fetchPlans } = props;
-  const IRSPlansListprops = {
-    ...ownProps,
-    fetchPlans,
-    pageTitle: MDA_POINT_PLANS,
-    pageUrl: REPORT_MDA_POINT_PLAN_URL,
-    plans,
-    supersetReportingSlice: SUPERSET_MDA_POINT_REPORTING_PLANS_SLICE,
-  };
-
   return (
     <div>
-      <ConnectedIRSPlansList {...IRSPlansListprops} />
+      <GenericPlansList {...props} />
     </div>
   );
 };
@@ -48,7 +38,11 @@ const MDAPointPlansList = (props: PlanListProps & RouteComponentProps) => {
 const defaultProps: PlanListProps = {
   fetchPlans: fetchMDAPointPlans,
   ownProps: {},
+  pageTitle: MDA_POINT_PLANS,
+  pageUrl: REPORT_MDA_POINT_PLAN_URL,
   plans: [],
+  service: supersetFetch,
+  supersetReportingSlice: SUPERSET_MDA_POINT_REPORTING_PLANS_SLICE,
 };
 
 MDAPointPlansList.defaultProps = defaultProps;
@@ -58,18 +52,20 @@ export { MDAPointPlansList };
 /** Connect the component to the store */
 
 /** interface to describe props from mapStateToProps */
-interface DispatchedStateProps {
-  ownProps: any;
+interface DispatchedStateProps extends RouteComponentProps<RouteParams> {
   plans: MDAPointPlan[];
 }
 
 /** map state to props */
-const mapStateToProps = (state: Partial<Store>, ownProps: any): DispatchedStateProps => {
+const mapStateToProps = (
+  state: Partial<Store>,
+  ownProps: RouteComponentProps<RouteParams>
+): DispatchedStateProps => {
   const searchedTitle = getQueryParams(ownProps.location)[QUERY_PARAM_TITLE] as string;
   const MDAPointPlansArray = makeMDAPointPlansArraySelector()(state, { plan_title: searchedTitle });
 
   return {
-    ownProps,
+    ...ownProps,
     plans: MDAPointPlansArray,
   };
 };
