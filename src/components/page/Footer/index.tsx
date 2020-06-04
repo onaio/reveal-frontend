930fdca154b8b3b6013b8a27c6320b1ec445d9be
p-netm <pmuriuki98@gmail.com>
2020-06-04 21:59:18 +0300

    Style tweaks for the header component

--- src/components/page/Footer/index.tsx
+++ src/components/page/Footer/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { REACT_APP_VERSION, REVEAL_BRAND_IMG_SRC, WEBSITE_NAME } from '../../../configs/env';
-import './footer.css';
 
 /** footer component; to render react-version on the web ui */
 export const Footer = () => {
   return (
     <footer className="footer">
-      <hr className="p-0 m-0 mb-1" />
+      <hr className="p-0 m-0" />
       <img src={REVEAL_BRAND_IMG_SRC} alt={WEBSITE_NAME} />{' '}
-      <span className="text-muted floating-logo-text">{REACT_APP_VERSION}</span>
+      <span className="text-muted" style={{ lineHeight: '40px', verticalAlign: 'bottom' }}>
+        {REACT_APP_VERSION}
+      </span>
     </footer>
   );
 };
