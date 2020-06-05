03e077c029b8b5e908ab7ae011a8e03d55b768cb
p-netm <pmuriuki98@gmail.com>
2020-06-05 18:05:20 +0300

    Style bottom logo to be on same line with version text.

--- src/components/page/Footer/index.tsx
+++ src/components/page/Footer/index.tsx
@@ -6,10 +6,10 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <hr className="p-0 m-0" />
-      {REVEAL_BRAND_IMG_SRC && <img src={REVEAL_BRAND_IMG_SRC} alt={WEBSITE_NAME} />}
-      <span className="text-muted" style={{ lineHeight: '40px', verticalAlign: 'bottom' }}>
-        {REACT_APP_VERSION}
-      </span>
+      {REVEAL_BRAND_IMG_SRC && (
+        <img src={REVEAL_BRAND_IMG_SRC} alt={WEBSITE_NAME} style={{ paddingBottom: '5px' }} />
+      )}
+      <small className="text-muted">{REACT_APP_VERSION}</small>
     </footer>
   );
 };
