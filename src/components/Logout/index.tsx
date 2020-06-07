d61cda860108f189e353a045b2b31e3d0050d627
p-netm <pmuriuki98@gmail.com>
2020-06-07 14:08:53 +0300

    Make final changes to how windows are opened closed and redirected
    during logging out when we are using react-backend

--- src/components/Logout/index.tsx
+++ src/components/Logout/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { EXPRESS_OAUTH_LOGOUT_URL } from '../../configs/env';
 
 /** interface to describe props for Logout component
@@ -15,21 +14,26 @@ export interface LogoutProps {
  */
 export const logoutFromAuthServer = (logoutURL: string) => {
   const logoutWindow = window.open(logoutURL);
-  // if(logoutWindow){
-  //   setTimeout(() => logoutWindow.close(), 2000);
-  // }
-  setTimeout(() => {window.location.href = EXPRESS_OAUTH_LOGOUT_URL}, 2000)
-  // window.location.href = EXPRESS_OAUTH_LOGOUT_URL;
+  /** I am not sure the optimum value for this, mozilla firefox seems to take some
+   * time loading up the logoutWindow and actually making the request.
+   */
+  const RedirectAfter = 1000;
+  if (logoutWindow) {
+    logoutWindow.opener.focus();
+    setTimeout(() => logoutWindow.close(), RedirectAfter);
+  }
+  /** Redirect to express logout after the logoutWindow has loaded,
+   */
+  setTimeout(() => {
+    window.location.href = EXPRESS_OAUTH_LOGOUT_URL;
+  }, RedirectAfter);
 };
 
 /** Component handles opensrp logout and goes to express serve */
 const Logout = (props: LogoutProps) => {
   const { logoutURL } = props;
-  // store.dispatch(logOutUser());
   logoutFromAuthServer(logoutURL);
-  // window.focus();
-  // window.location.href = EXPRESS_OAUTH_LOGOUT_URL;
-  return <>This is the Logout page</>;
+  return null;
 };
 
 export default Logout;
