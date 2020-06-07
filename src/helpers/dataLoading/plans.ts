b9376f353d3ae0c4a4c8d868b0d0b2d99e37288d
p-netm <pmuriuki98@gmail.com>
2020-06-07 10:29:06 +0300

    Refactor the error display function in plans dataloading

--- src/helpers/dataLoading/plans.ts
+++ src/helpers/dataLoading/plans.ts
@@ -1,10 +1,9 @@
-import { toast } from 'react-toastify';
 import { ActionCreator, AnyAction } from 'redux';
 import { OPENSRP_PLANS_BY_USER_FILTER } from '../../constants';
 import { OpenSRPService } from '../../services/opensrp';
 import store from '../../store';
 import { fetchPlansByUser, FetchPlansByUserAction } from '../../store/ducks/opensrp/planIdsByUser';
-import { growl } from '../utils';
+import { displayError } from '../errors';
 
 /** find plans that the given user has access to
  * @param {string} userName - username
@@ -28,6 +27,6 @@ export async function loadPlansByUserFilter<T>(
       }
     })
     .catch((err: Error) => {
-      growl(err.message, { type: toast.TYPE.ERROR });
+      displayError(err);
     });
 }
