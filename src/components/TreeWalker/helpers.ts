c7a5a379d1489f78c0680c9b5dee9a2642695cba
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-06-19 11:45:15 +0300

    Add support for limitTree in getChildren
    Also:
      - Use an object for API endpoints
      - Set defaults for service class and API endpoints

--- src/components/TreeWalker/helpers.ts
+++ src/components/TreeWalker/helpers.ts
@@ -1,7 +1,6 @@
 import { Result } from '@onaio/utils';
-import { URLParams } from '@opensrp/server-service/dist/types';
-import { OpenSRPService } from '../../services/opensrp';
-import { OpenSRPJurisdiction } from './types';
+import { OpenSRPService, URLParams } from '../../services/opensrp';
+import { APIEndpoints, OpenSRPJurisdiction, SimpleJurisdicion } from './types';
 
 /** Default params to be used when fetching locations from OpenSRP */
 export const defaultLocationParams = {
@@ -12,6 +11,12 @@ export const defaultLocationParams = {
 /** Default property to be used when fetching locations from OpenSRP */
 export const defaultLocationPropertyFilters = {
   status: 'Active',
+};
+
+export const locationListAPIEndpoints: APIEndpoints = {
+  findByJurisdictionIds: 'location/findByJurisdictionIds',
+  findByProperties: 'location/findByProperties',
+  location: 'location',
 };
 
 /** Get ancestors of a jurisdiction from OpenSRP
@@ -27,9 +32,9 @@ export const defaultLocationPropertyFilters = {
  */
 export const getAncestors = async (
   jurisdiction: OpenSRPJurisdiction,
-  apiEndpoint: string,
-  serviceClass: typeof OpenSRPService,
-  path: OpenSRPJurisdiction[] = []
+  path: OpenSRPJurisdiction[] = [],
+  apiEndpoint: string = locationListAPIEndpoints.location,
+  serviceClass: typeof OpenSRPService = OpenSRPService
 ): Promise<Result<OpenSRPJurisdiction[]>> => {
   if (!path.includes(jurisdiction)) {
     path.unshift(jurisdiction);
@@ -62,7 +67,7 @@ export const getAncestors = async (
     };
   } else {
     if (result.value !== null) {
-      return getAncestors(result.value, apiEndpoint, serviceClass, path);
+      return getAncestors(result.value, path);
     }
     return result;
   }
@@ -72,16 +77,60 @@ export const getAncestors = async (
  *
  * @param params - URL params to send with the request to the API
  * @param jurisdiction - the jurisdiction in question
- * @param apiEndpoint - the API endpoint
+ * @param limitTree - an array that limits the children we try and get from the API
+ * @param apiEndpoints - the API endpoints to use
  * @param serviceClass - the API helper class
  */
 export const getChildren = async (
   params: URLParams,
-  _: OpenSRPJurisdiction | null,
-  apiEndpoint: string,
-  serviceClass: typeof OpenSRPService
+  jurisdiction: OpenSRPJurisdiction | null,
+  limitTree: SimpleJurisdicion[] = [
+    { id: '1337', parentId: '3019' },
+    {
+      id: 'dad42fa6-b9b8-4658-bf25-bfa7ab5b16ae',
+      parentId: '872cc59e-0bce-427a-bd1f-6ef674dba8e2',
+    },
+    {
+      id: 'd56a4344-a7d0-4285-9fe4-c64fd7fc27d4',
+      parentId: '872cc59e-0bce-427a-bd1f-6ef674dba8e2',
+    },
+  ],
+  apiEndpoints: APIEndpoints = locationListAPIEndpoints,
+  serviceClass: typeof OpenSRPService = OpenSRPService
 ): Promise<Result<OpenSRPJurisdiction[]>> => {
-  const service = new serviceClass(apiEndpoint);
+  let service = new serviceClass(apiEndpoints.findByProperties);
+
+  // console.log("jurisdiction >>> ", jurisdiction)
+  // console.log("params >>> ", params)
+
+  if (limitTree && limitTree.length > 0) {
+    // Basically if limitTree has any elements, then we need to ensure that when
+    // getting children from the API, we limit ourselves only to the jurisdictions
+    // contained in limitTree
+
+    // first we get the current parent i.e. the one whose children we want
+    let currentParentId: string | undefined;
+    if (jurisdiction !== null) {
+      currentParentId = jurisdiction.id;
+    } else if (typeof params.parentId === 'string') {
+      currentParentId = params.parentId;
+    }
+
+    // console.log("currentParentId >>> ", currentParentId)
+
+    // Next, if limitTree contains any elements whose parent is currentParentId then
+    // we only fetch those and not all the children that may possibly be on the API server
+    if (currentParentId) {
+      const jurisdictionIds = limitTree
+        .filter(elem => elem.parentId === currentParentId)
+        .map(elem => elem.id);
+      // console.log("jurisdictionIds >>> ", jurisdictionIds)
+      if (jurisdictionIds.length > 0) {
+        service = new serviceClass(apiEndpoints.findByJurisdictionIds);
+        params.jurisdiction_ids = jurisdictionIds.join(',');
+      }
+    }
+  }
 
   const result = await service
     .list(params)
