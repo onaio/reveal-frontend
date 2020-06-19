3ea01504a0c149bce2cc04ee2a18f3f8e0722f2e
p-netm <pmuriuki98@gmail.com>
2020-06-19 10:12:33 +0300

    Test title search in IRS plans page

--- src/containers/pages/InterventionPlan/IRS/tests/IRS.test.tsx
+++ src/containers/pages/InterventionPlan/IRS/tests/IRS.test.tsx
@@ -1,3 +1,4 @@
+import { renderTable } from '@onaio/drill-down-table';
 import reducerRegistry from '@onaio/redux-reducer-registry';
 import { mount, shallow } from 'enzyme';
 import { createBrowserHistory } from 'history';
@@ -7,7 +8,7 @@ import { Provider } from 'react-redux';
 import { Router } from 'react-router';
 import ConnectedIrsPlans, { IrsPlans } from '..';
 import { DRAFTS_PARENTHESIS, IRS_PLANS } from '../../../../../configs/lang';
-import { INTERVENTION_IRS_DRAFTS_URL } from '../../../../../constants';
+import { INTERVENTION_IRS_DRAFTS_URL, QUERY_PARAM_TITLE } from '../../../../../constants';
 import store from '../../../../../store';
 import { plansJSON } from '../../../../../store/ducks/opensrp/PlanDefinition/tests/fixtures';
 import plansReducer, {
@@ -121,6 +122,74 @@ describe('containers/pages/IRS', () => {
     wrapper.update();
 
     expect((wrapper.find('IrsPlans').props() as any).plansArray.length).toEqual(1);
+    wrapper.unmount();
+  });
+
+  it('Search works correctly', async () => {
+    fetch.mockResponse(plansJSON);
+    const props = {
+      history,
+      location: {
+        hash: '',
+        pathname: '',
+        search: `${QUERY_PARAM_TITLE}=Khlong `,
+        state: '',
+      },
+      match: {
+        isExact: true,
+        params: {},
+        path: INTERVENTION_IRS_DRAFTS_URL,
+        url: INTERVENTION_IRS_DRAFTS_URL,
+      },
+    };
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <Router history={history}>
+          <ConnectedIrsPlans {...props} />
+        </Router>
+      </Provider>
+    );
+
+    await new Promise(resolve => setImmediate(resolve));
+    wrapper.update();
+
+    renderTable(wrapper, 'find single row for entry with Khlong ');
+    wrapper.unmount();
+  });
+
+  it('does not show loader for no data', async () => {
+    fetch.mockReject(new Error('No Plans returned'));
+    const props = {
+      history,
+      location: {
+        hash: '',
+        pathname: '',
+        search: `${QUERY_PARAM_TITLE}=usadafasdgaka`,
+        state: '',
+      },
+      match: {
+        isExact: true,
+        params: {},
+        path: INTERVENTION_IRS_DRAFTS_URL,
+        url: INTERVENTION_IRS_DRAFTS_URL,
+      },
+    };
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <Router history={history}>
+          <ConnectedIrsPlans {...props} />
+        </Router>
+      </Provider>
+    );
+
+    expect(wrapper.find('Ripple').length).toEqual(1);
+
+    await new Promise(resolve => setImmediate(resolve));
+    wrapper.update();
+
+    renderTable(wrapper, 'find No Data Found text');
     wrapper.unmount();
   });
 });
