5bc8167c58cef4c5b1c11661aa6444bef8c0a8ea
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-06-21 16:42:58 +0300

    Add JurisdictionCell
    - Move jurisdiction name renderign to its own component
    - determine if to render a link or not

---
+++ src/components/TreeWalker/JurisdictionCell.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { PlanDefinition } from '../../configs/settings';
+import { ASSIGN2_PLAN_URL } from '../../constants';
+import { OpenSRPJurisdiction, SimpleJurisdiction } from './types';
+
+interface JurisdictionCellProps {
+  limits: SimpleJurisdiction[];
+  node: OpenSRPJurisdiction;
+  plan: PlanDefinition;
+}
+
+const JurisdictionCell = (props: JurisdictionCellProps) => {
+  const { limits, node, plan } = props;
+
+  const focusAreas = plan.jurisdiction.map(item => item.code);
+  const knownParentIds = limits.map(item => item.jurisdiction_parent_id);
+  const stopHere: boolean = !knownParentIds.includes(node.id) || focusAreas.includes(node.id);
+
+  if (stopHere) {
+    return <span key={`${node.id}-span`}>{node.properties.name}</span>;
+  }
+
+  return (
+    <Link key={`${node.id}-link`} to={`${ASSIGN2_PLAN_URL}/${plan.identifier}/${node.id}`}>
+      {node.properties.name}
+    </Link>
+  );
+};
+
+export { JurisdictionCell };
