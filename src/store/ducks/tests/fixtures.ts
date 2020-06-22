026fca038f3cf282be0a1174dda143367556b248
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-06-22 19:27:27 +0300

    Add more fixtures

--- src/store/ducks/tests/fixtures.ts
+++ src/store/ducks/tests/fixtures.ts
@@ -1619,6 +1619,22 @@ export const assignment5 = {
   plan: 'beta',
   toDate: '2019-12-31T16:00:01-08:00',
 };
+export const assignment6 = {
+  // slightly different from 1
+  fromDate: '2019-08-31T16:00:00-08:00',
+  jurisdiction: 'outpost-number-one',
+  organization: 'griff',
+  plan: 'alpha',
+  toDate: '1979-12-31T16:00:01-08:00', // different end date to simulate retirement
+};
+export const assignment7 = {
+  // slightly different from 1
+  fromDate: '2019-08-31T16:00:00-08:00',
+  jurisdiction: 'outpost-number-one',
+  organization: 'griff',
+  plan: 'alpha',
+  toDate: '2020-12-31T16:00:01-08:00', // different end date to simulate retirement
+};
 export const assignments = [assignment1, assignment2, assignment3, assignment4];
 
 export const practitioners: Practitioner[] = [practitioner1, practitioner2, practitioner3];
