de4509eeb20e463182892e8955b90ed04afbeb28
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-06-22 16:22:03 +0300

    Add better error handling
    - Use Promise.All() to ensure that the loading indicator runs until all
      initial fetching is complete
    - Add specific error messages
    - Handle all promise rejections

--- src/containers/pages/PlanAssignment/index.tsx
+++ src/containers/pages/PlanAssignment/index.tsx
@@ -52,6 +52,8 @@ interface PlanAssignmentProps extends JurisdictionTableProps {
 
 const PlanAssignment = (props: PlanAssignmentProps) => {
   const [loading, setLoading] = useState<boolean>(true);
+  const [broken, setBroken] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const [hierarchyLimits, setHierarchyLimits] = useState<SimpleJurisdiction[]>([]);
 
   const {
@@ -68,25 +70,36 @@ const PlanAssignment = (props: PlanAssignmentProps) => {
     return null;
   }
 
-  /** define superset filter params for jurisdictions */
-  const supersetParams = superset.getFormData(15000, [
-    { comparator: planId, operator: '==', subject: 'plan_id' },
-  ]);
+  const handleBrokenPage = (message: string) => {
+    displayError(Error(message));
+    setErrorMessage(message);
+    setBroken(true);
+  };
 
   useEffect(() => {
     // Get Plan hierarchy
-    supersetFetch(SUPERSET_PLAN_HIERARCHY_SLICE, supersetParams)
+    const supersetParams = superset.getFormData(15000, [
+      { comparator: planId, operator: '==', subject: 'plan_id' },
+    ]);
+    const planHierarchyPromise = supersetFetch(SUPERSET_PLAN_HIERARCHY_SLICE, supersetParams)
       .then((result: SimpleJurisdiction[]) => {
         if (result) {
           setHierarchyLimits(result);
+        } else {
+          if (hierarchyLimits.length < 1) {
+            // this means that we basically have not succeeded to make this call
+            // if hierarchyLimits is not populated we cannot proceed
+            handleBrokenPage('Unable to the plan jurisdiction hierarchy');
+          }
         }
       })
-      .finally(() => setLoading(false))
-      .catch(e => displayError(e));
+      .catch(e => {
+        handleBrokenPage(e.message);
+      });
 
     // get all assignments
     const OpenSrpAssignedService = new OpenSRPService(OPENSRP_GET_ASSIGNMENTS_ENDPOINT);
-    OpenSrpAssignedService.list({ plan: planId })
+    const assignmentsPromise = OpenSrpAssignedService.list({ plan: planId })
       .then((response: any[]) => {
         // TODO: remove any
         if (response) {
@@ -101,40 +114,74 @@ const PlanAssignment = (props: PlanAssignmentProps) => {
           });
           // save assignments to store
           fetchAssignmentsActionCreator(receivedAssignments);
+        } else {
+          displayError(Error('Unable to load assignments'));
         }
-        // TODO: add error if no response
       })
       .catch(e => displayError(e));
 
     // fetch all organizations
     const OpenSRPOrganizationService = new OpenSRPService(OPENSRP_ORGANIZATION_ENDPOINT);
-    OpenSRPOrganizationService.list()
+    const organizationsPromise = OpenSRPOrganizationService.list()
       .then((response: Organization[]) => {
         if (response) {
           // save all organizations to store
           fetchOrganizationsActionCreator(response);
+        } else {
+          if (organizations.length < 1) {
+            // this means that we basically have not succeeded to make this call
+            // if organizations is not populated we cannot proceed
+            handleBrokenPage('Unable to load teams');
+          }
         }
-        // TODO: add error if no response
       })
-      .catch(e => displayError(e));
+      .catch(e => {
+        handleBrokenPage(e.message);
+      });
 
     // fetch current plan
     const OpenSRPPlanService = new OpenSRPService(OPENSRP_PLANS);
-    OpenSRPPlanService.read(planId)
+    const plansPromise = OpenSRPPlanService.read(planId)
       .then((response: PlanDefinition[]) => {
         if (response && response.length > 0) {
           // save plan to store
           addPlanActionCreator(response[0]);
+        } else {
+          if (!plan) {
+            // this means that we basically have not succeeded to make this call
+            // if no plan then we cannot proceed
+            handleBrokenPage('Unable to load plan');
+          }
         }
-        // TODO: add error if no response
       })
-      .catch(e => displayError(e));
+      .catch(e => {
+        handleBrokenPage(e.message);
+      });
+
+    Promise.all([planHierarchyPromise, assignmentsPromise, organizationsPromise, plansPromise])
+      .finally(() => setLoading(false))
+      .catch(e => {
+        handleBrokenPage(e.message);
+      });
   }, []);
 
   if (loading) {
     // TODO: show message of what is actually loading
     // return <Fragment>plan hierarchy loading...</Fragment>;
     return <Loading />;
+  }
+
+  if (broken) {
+    return (
+      <div>
+        <p>An error ocurred. Please try and refresh the page.</p>
+        {errorMessage && (
+          <p>
+            The specific error is: <span className="text-danger">{errorMessage}</span>
+          </p>
+        )}
+      </div>
+    );
   }
 
   let jurisdictionId = props.match.params.jurisdictionId;
