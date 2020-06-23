586860efc05cabe8e7916b82cd2ef5de46e48023
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-06-23 19:13:14 +0300

    Complete tests for form working

--- src/containers/pages/PlanAssignment/helpers/JurisdictionAssignmentForm/tests/fixtures.ts
+++ src/containers/pages/PlanAssignment/helpers/JurisdictionAssignmentForm/tests/fixtures.ts
@@ -52,3 +52,35 @@ export const assignment5 = {
   plan: '356b6b84-fc36-4389-a44a-2b038ed2f38d',
   toDate: '2019-08-30T00:00:00+00:00',
 };
+
+export const apiCall = [
+  [
+    'https://test.smartregister.org/opensrp/rest/organization/assignLocationsAndPlans',
+    {
+      'Cache-Control': 'no-cache',
+      Pragma: 'no-cache',
+      body:
+        '[{"fromDate":"2019-12-30T00:00:00+00:00","jurisdiction":"3091","organization":"x","plan":"8fa7eb32-99d7-4b49-8332-9ecedd6d51ae","toDate":"2019-07-30T00:00:00+00:00"}]',
+      headers: {
+        accept: 'application/json',
+        authorization: 'Bearer null',
+        'content-type': 'application/json;charset=UTF-8',
+      },
+      method: 'POST',
+    },
+  ],
+];
+
+export const submitCallbackPayload = [
+  [
+    [
+      {
+        fromDate: '2019-12-30T00:00:00+00:00',
+        jurisdiction: '3091',
+        organization: 'x',
+        plan: '8fa7eb32-99d7-4b49-8332-9ecedd6d51ae',
+        toDate: '2019-07-30T00:00:00+00:00',
+      },
+    ],
+  ],
+];
