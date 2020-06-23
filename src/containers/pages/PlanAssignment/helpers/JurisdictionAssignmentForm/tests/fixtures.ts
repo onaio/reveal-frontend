b643931ddd445cfdaca014785e1b60a905935b47
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-06-23 13:42:53 +0300

    Add more tests for getPayload

--- src/containers/pages/PlanAssignment/helpers/JurisdictionAssignmentForm/tests/fixtures.ts
+++ src/containers/pages/PlanAssignment/helpers/JurisdictionAssignmentForm/tests/fixtures.ts
@@ -12,3 +12,43 @@ export const openSRPJurisdiction: OpenSRPJurisdiction = {
   serverVersion: 123456789,
   type: 'Feature',
 };
+
+export const assignments = [
+  {
+    fromDate: '2019-12-30T00:00:00+00:00',
+    jurisdiction: '3091',
+    organization: '1',
+    plan: '356b6b84-fc36-4389-a44a-2b038ed2f38d',
+    toDate: '2019-08-30T00:00:00+00:00',
+  },
+  {
+    fromDate: '2019-12-30T00:00:00+00:00',
+    jurisdiction: '3091',
+    organization: '2',
+    plan: '356b6b84-fc36-4389-a44a-2b038ed2f38d',
+    toDate: '2019-08-30T00:00:00+00:00',
+  },
+  {
+    fromDate: '2019-12-30T00:00:00+00:00',
+    jurisdiction: '3091',
+    organization: '3',
+    plan: '356b6b84-fc36-4389-a44a-2b038ed2f38d',
+    toDate: '2019-08-30T00:00:00+00:00',
+  },
+];
+
+export const assignment4 = {
+  fromDate: '2019-12-30T00:00:00+00:00',
+  jurisdiction: '3091',
+  organization: '4',
+  plan: '356b6b84-fc36-4389-a44a-2b038ed2f38d',
+  toDate: '2019-08-30T00:00:00+00:00',
+};
+
+export const assignment5 = {
+  fromDate: '2019-12-30T00:00:00+00:00',
+  jurisdiction: '3091',
+  organization: '5',
+  plan: '356b6b84-fc36-4389-a44a-2b038ed2f38d',
+  toDate: '2019-08-30T00:00:00+00:00',
+};
--- src/containers/pages/PlanAssignment/helpers/JurisdictionAssignmentForm/tests/helpers.test.ts
+++ src/containers/pages/PlanAssignment/helpers/JurisdictionAssignmentForm/tests/helpers.test.ts
@@ -1,7 +1,7 @@
 import MockDate from 'mockdate';
 import { plans } from '../../../../../../store/ducks/opensrp/PlanDefinition/tests/fixtures';
 import { getPayload } from '../helpers';
-import { openSRPJurisdiction } from './fixtures';
+import { assignment4, assignment5, assignments, openSRPJurisdiction } from './fixtures';
 
 describe('PlanAssignment/helpers', () => {
   beforeEach(() => {
@@ -9,32 +9,69 @@ describe('PlanAssignment/helpers', () => {
     MockDate.set('12/30/2019', 0);
   });
 
-  it('check extractActivityForForm returns the correct value for BCC', () => {
+  it('works for new assignments', () => {
     const plan = plans[0];
     const selectedOrgs = ['1', '2', '3'];
 
     const payload = getPayload(selectedOrgs, plan, openSRPJurisdiction);
+    expect(payload).toEqual(assignments);
+  });
+
+  it('works for removing assignments', () => {
+    const plan = plans[0];
+    const initialOrgs = ['1', '2', '3'];
+    const selectedOrgs = ['1'];
+
+    const payload = getPayload(selectedOrgs, plan, openSRPJurisdiction, initialOrgs);
     expect(payload).toEqual([
+      assignments[0],
       {
-        fromDate: '2019-12-30T00:00:00+00:00',
-        jurisdiction: '3091',
-        organization: '1',
-        plan: '356b6b84-fc36-4389-a44a-2b038ed2f38d',
-        toDate: '2019-08-30T00:00:00+00:00',
+        ...assignments[1],
+        toDate: '2019-12-29T00:00:00+00:00',
       },
       {
-        fromDate: '2019-12-30T00:00:00+00:00',
-        jurisdiction: '3091',
-        organization: '2',
-        plan: '356b6b84-fc36-4389-a44a-2b038ed2f38d',
-        toDate: '2019-08-30T00:00:00+00:00',
+        ...assignments[2],
+        toDate: '2019-12-29T00:00:00+00:00',
+      },
+    ]);
+  });
+
+  it('works for removing and adding at the same time assignments', () => {
+    const plan = plans[0];
+    const initialOrgs = ['1', '2', '3'];
+    const selectedOrgs = ['1', '4', '5'];
+
+    const payload = getPayload(selectedOrgs, plan, openSRPJurisdiction, initialOrgs);
+    expect(payload).toEqual([
+      assignments[0],
+      assignment4,
+      assignment5,
+      {
+        ...assignments[1],
+        toDate: '2019-12-29T00:00:00+00:00',
       },
       {
-        fromDate: '2019-12-30T00:00:00+00:00',
-        jurisdiction: '3091',
-        organization: '3',
-        plan: '356b6b84-fc36-4389-a44a-2b038ed2f38d',
-        toDate: '2019-08-30T00:00:00+00:00',
+        ...assignments[2],
+        toDate: '2019-12-29T00:00:00+00:00',
+      },
+    ]);
+  });
+
+  it('does not allow duplicates', () => {
+    const plan = plans[0];
+    const initialOrgs = ['2', '3'];
+    const selectedOrgs = ['1', '1', '1'];
+
+    const payload = getPayload(selectedOrgs, plan, openSRPJurisdiction, initialOrgs);
+    expect(payload).toEqual([
+      assignments[0],
+      {
+        ...assignments[1],
+        toDate: '2019-12-29T00:00:00+00:00',
+      },
+      {
+        ...assignments[2],
+        toDate: '2019-12-29T00:00:00+00:00',
       },
     ]);
   });
