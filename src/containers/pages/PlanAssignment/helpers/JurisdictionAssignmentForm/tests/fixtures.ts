72b76140db1d5470e565bfa6cbcb84810ae5220a
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-06-23 13:21:58 +0300

    Add initial test for getPayload

---
+++ src/containers/pages/PlanAssignment/helpers/JurisdictionAssignmentForm/tests/fixtures.ts
@@ -0,0 +1,14 @@
+import { OpenSRPJurisdiction } from '../../../../../../components/TreeWalker/types';
+
+export const openSRPJurisdiction: OpenSRPJurisdiction = {
+  id: '3091',
+  properties: {
+    geographicLevel: 3,
+    name: 'Rural',
+    parentId: 'root',
+    status: 'Active',
+    version: 2,
+  },
+  serverVersion: 123456789,
+  type: 'Feature',
+};
