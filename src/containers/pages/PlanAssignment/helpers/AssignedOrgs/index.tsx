43709ab470940a03046a6a3594d47b6e354b699c
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-06-24 16:24:23 +0300

    Clean up and add docstrings

--- src/containers/pages/PlanAssignment/helpers/AssignedOrgs/index.tsx
+++ src/containers/pages/PlanAssignment/helpers/AssignedOrgs/index.tsx
@@ -1,19 +1,39 @@
 import React, { Fragment, useState } from 'react';
 import { Tooltip } from 'reactstrap';
+import { AND, MORE } from '../../../../../configs/lang';
 import { Organization } from '../../../../../store/ducks/opensrp/organizations';
 
-interface AssignedOrgsProps {
-  displayLimit: number;
-  id: string;
-  orgs: Organization[];
+/** Props for AssignedOrgs  */
+export interface AssignedOrgsProps {
+  displayLimit: number /** The max number of orgs to show initially */;
+  id: string /** id to use for tooltip */;
+  labels: {
+    and: string;
+    more: string;
+  };
+  orgs: Organization[] /** array of organisations */;
 }
 
+/** default props for AssignedOrgs */
 const defaultProps: AssignedOrgsProps = {
   displayLimit: 3,
   id: '',
+  labels: {
+    and: AND,
+    more: MORE,
+  },
   orgs: [],
 };
 
+/**
+ * AssignedOrgs
+ *
+ * This component displays a comma separated list of organization names.
+ * When the number of names is more than displayLimit (default 3), then
+ * the component only shows a few names, and hides the rest in a Tooltip.
+ *
+ * @param props - the props!
+ */
 const AssignedOrgs = (props: AssignedOrgsProps) => {
   const { id, orgs, displayLimit } = props;
 
