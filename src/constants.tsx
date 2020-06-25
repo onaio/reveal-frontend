301746f67569b34c7216b4ab3e28bde2138936cc
Brian Mwasi <>
2020-06-25 12:17:07 +0300

    added additional csv MIME types to enable validation on windows machines

--- src/constants.tsx
+++ src/constants.tsx
@@ -170,3 +170,14 @@ export const ROUTINE_QUERY_PARAM = 'routine_plans';
 
 /** Settings Configuration */
 export const SETTINGS_CONFIGURATION = 'SettingConfiguration';
+
+/** CSV File MIME Types */
+export const TEXT_CSV = 'text/csv';
+export const APPLICATION_CSV = 'application/csv';
+export const TEXT_PLAIN = 'text/plain';
+export const TEXT_X_CSV = 'text/x-csv';
+export const APPLICATION_VND_EXCEL = 'application/vnd.ms-excel';
+export const APPLICATION_X_CSV = 'application/x-csv';
+export const TEXT_COMMA_SEPARATED_VALUES = 'text/comma-separated-values';
+export const TEXT_X_COMMA_SEPARATED_VALUES = 'text/x-comma-separated-values';
+export const TEXT_TAB_SEPARATED_VALUES = 'text/tab-separated-values';
