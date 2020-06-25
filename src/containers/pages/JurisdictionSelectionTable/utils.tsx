a8dcc7c59c9c22aea2a8f289010409170f2b45bc
p-netm <pmuriuki98@gmail.com>
2020-06-25 15:50:21 +0300

    add helper function to parse hierarchy into a tree model

---
+++ src/containers/pages/JurisdictionSelectionTable/utils.tsx
@@ -0,0 +1,40 @@
+/** utils for the jurisdiction selection table  */
+import { cloneDeep } from 'lodash';
+import TreeModel from 'tree-model';
+
+/** inline data store to store hierarchical jurisdictions data */
+export const tree = new TreeModel();
+
+export const generateJurisdictionTree = (apiResponse: any) => {
+  // keep reference to jurisdiction tree, give back a few helper functions (facade)
+  // to return the jurisdiction tree info manipulated in a few ways.
+  const hierarchy: any = parseHierarchy(apiResponse);
+  const root = tree.parse(hierarchy);
+  return root;
+};
+
+const parseChildren = (map: any) => {
+  if (map.children) {
+    map.children = Object.entries(map.children).map(([key, value]) => value);
+    map.children.forEach((child: any) => {
+      parseChildren(child);
+    });
+  }
+};
+
+const parseParent = (map: any) => {
+  const parentJurisdiction = Object.entries(map).map(([key, value]) => value)[0];
+  return parentJurisdiction;
+};
+
+// locationsTree
+const parseHierarchy = (locationsTree: any) => {
+  // clone the locationTree, to avoid sideEffects
+  const locationsTreeClone = cloneDeep(locationsTree);
+  // assumption here: locationsTreeClone has a single object under map, i.e there is only one root jurisdiction
+  const { map } = locationsTreeClone.locationsHierarchy;
+  const parentJurisdiction = Object.entries(map).map(([key, value]) => value)[0];
+  const rootJurisdiction = parseParent(locationsTreeClone);
+  parseChildren(rootJurisdiction);
+  return parentJurisdiction;
+};
