e6536887a829820567e8a5420137aa052098e054
p-netm <pmuriuki98@gmail.com>
2020-06-30 15:05:45 +0300

    Test base list component

---
+++ src/containers/pages/InterventionPlan/PlanningView/helpers/BaseListing/tests/index.test.tsx
@@ -0,0 +1,46 @@
+import { renderTable } from '@onaio/drill-down-table';
+import { mount } from 'enzyme';
+import React from 'react';
+import { BaseListComponent } from '..';
+import { PlanRecord } from '../../../../../../../store/ducks/plans';
+import { irsDraftPageColumns } from '../../../utils';
+
+describe('src/../pages/InterventionPlan/PlanningView/helpers', () => {
+  it('base list is renders correctly', () => {
+    const props = {
+      plansArray: [],
+    };
+
+    const wrapper = mount(<BaseListComponent {...props} />);
+
+    renderTable(wrapper, 'should have no data');
+    expect(wrapper.text()).toMatchInlineSnapshot(`"NameDate CreatedStatusNo Data Found"`);
+  });
+
+  it('base list with props is rendered correctly', () => {
+    const loadDataMock = (setLoading: React.Dispatch<React.SetStateAction<boolean>>) => {
+      setLoading(false);
+    };
+    const noDataMessage = 'just blank';
+    const getTableProps = (loading: boolean, data: PlanRecord[]) => {
+      return {
+        columns: irsDraftPageColumns,
+        data,
+        loading,
+        loadingComponent: () => <>Loading...</>,
+        renderNullDataComponent: () => <>{noDataMessage}</>,
+        useDrillDown: false,
+      };
+    };
+    const props = {
+      getTableProps,
+      loadData: loadDataMock,
+      plansArray: [],
+    };
+
+    const wrapper = mount(<BaseListComponent {...props} />);
+
+    renderTable(wrapper, 'Has no data');
+    expect(wrapper.text().includes(noDataMessage)).toBeTruthy();
+  });
+});
