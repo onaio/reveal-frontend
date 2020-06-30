56e6e86a023b95e963f59f9e4f2b3997092aa713
p-netm <pmuriuki98@gmail.com>
2020-06-30 12:46:20 +0300

    Refactor container view for opensrp plans

--- src/containers/pages/InterventionPlan/PlanningView/OpenSRPPlansList.tsx
+++ src/containers/pages/InterventionPlan/PlanningView/OpenSRPPlansList.tsx
@@ -1,124 +1,140 @@
 /** presentational component that renders a list of plan definitions( plans fetched from the opensrp api ) */
-import { DrillDownColumn, DrillDownTable, DrillDownTableProps } from '@onaio/drill-down-table';
+import { DrillDownColumn } from '@onaio/drill-down-table';
+import { Registry } from '@onaio/redux-reducer-registry/dist/types';
 import React from 'react';
-import Helmet from 'react-helmet';
+import { connect } from 'react-redux';
 import { RouteComponentProps } from 'react-router';
-import { Link } from 'react-router-dom';
-import Button from 'reactstrap/lib/Button';
-import HeaderBreadcrumbs, {
-  BreadCrumbProps,
-} from '../../../../components/page/HeaderBreadcrumb/HeaderBreadcrumb';
+import { ActionCreator, Store } from 'redux';
 import Loading from '../../../../components/page/Loading';
 import {
   defaultOptions,
   renderInFilterFactory,
 } from '../../../../components/Table/DrillDownFilters/utils';
 import { NoDataComponent } from '../../../../components/Table/NoDataComponent';
-import { CREATE_NEW_PLAN, DRAFT_PLANS, HOME } from '../../../../configs/lang';
-import { HOME_URL, NEW, PLANNING_VIEW_URL, QUERY_PARAM_TITLE } from '../../../../constants';
-import { PlanRecord } from '../../../../store/ducks/plans';
+import { PLAN_RECORD_BY_ID, QUERY_PARAM_TITLE } from '../../../../constants';
+import { loadOpenSRPPlans } from '../../../../helpers/dataLoading/plans';
+import { getQueryParams } from '../../../../helpers/utils';
+import { OpenSRPService } from '../../../../services/opensrp';
+import {
+  fetchPlanRecords,
+  FetchPlanRecordsAction,
+  makePlansArraySelector,
+  PlanRecord,
+  PlanStatus,
+} from '../../../../store/ducks/plans';
+import {
+  BaseListComponent,
+  BaseListComponentProps,
+  BaseListTableProps,
+} from './helpers/BaseListing';
 import { draftPageColumns } from './utils';
 
-/** propTypes for presentation component */
-export interface OpenSRPPlansListProps {
-  loadData?: (setLoading: React.Dispatch<React.SetStateAction<boolean>>) => void;
-  plansArray: PlanRecord[];
+type RenderProp = () => React.ReactNode;
+
+/** props for opensrpPlansList view */
+export interface OpenSRPPlanListViewProps
+  extends Omit<BaseListComponentProps, 'loadData' | 'getTableProps'> {
+  fetchPlanRecordsCreator: ActionCreator<FetchPlanRecordsAction>;
+  serviceClass: typeof OpenSRPService;
   tableColumns: Array<DrillDownColumn<PlanRecord>>;
-  pageTitle: string;
-  pageUrl: string;
-  newPlanUrl: string;
+  renderBody: (renderProp: RenderProp) => React.ReactNode;
 }
 
-/** default Props for the presentational component */
-export const defaultProps: OpenSRPPlansListProps = {
-  newPlanUrl: `${PLANNING_VIEW_URL}/${NEW}`,
-  pageTitle: DRAFT_PLANS,
-  pageUrl: PLANNING_VIEW_URL,
+/** default body render */
+const defaultBodyRenderer = (componentRender: RenderProp) => {
+  return <>{componentRender()}</>;
+};
+
+export const defaultProps: OpenSRPPlanListViewProps = {
+  fetchPlanRecordsCreator: fetchPlanRecords,
   plansArray: [],
+  renderBody: defaultBodyRenderer,
+  serviceClass: OpenSRPService,
   tableColumns: draftPageColumns,
 };
 
-/** table prop type */
-type TableProps = Pick<
-  DrillDownTableProps<PlanRecord>,
-  | 'columns'
-  | 'data'
-  | 'loading'
-  | 'loadingComponent'
-  | 'renderInBottomFilterBar'
-  | 'renderInTopFilterBar'
-  | 'useDrillDown'
-  | 'renderNullDataComponent'
->;
-
-/** TODO - investigate an appropriate strategy of making a presentational view like this configurable
- * - maybe keep the rendered components and hoist all their props to the component's props level.
+/** container view that renders opensrp plans from store , adds reveal-domain specific
+ * stuff like the ui and functionality of the filters to be added to the table component
  */
+const OpenSRPPlansList = (props: OpenSRPPlanListViewProps & RouteComponentProps) => {
+  const { plansArray, fetchPlanRecordsCreator, serviceClass, tableColumns } = props;
+  const loadData = (setLoading: React.Dispatch<React.SetStateAction<boolean>>) =>
+    loadOpenSRPPlans(serviceClass, fetchPlanRecordsCreator, setLoading);
 
-/** presentational view component that renders opensrp plans */
-const OpenSRPPlansList = (props: OpenSRPPlansListProps & RouteComponentProps) => {
-  const [loading, setLoading] = React.useState<boolean>(props.plansArray.length === 0);
-
-  React.useEffect(() => {
-    if (!props.loadData) {
-      setLoading(false);
-    } else {
-      props.loadData(setLoading);
-    }
-  }, []);
-
-  const { pageTitle, pageUrl, plansArray } = props;
-  const homePage = {
-    label: HOME,
-    url: HOME_URL,
-  };
-  const breadCrumbProps: BreadCrumbProps = {
-    currentPage: {
-      label: pageTitle,
-      url: pageUrl,
-    },
-    pages: [homePage],
-  };
-
-  /** tableProps - props for DrillDownTable component */
-  const tableProps: TableProps = {
-    columns: props.tableColumns,
-    data: plansArray,
-    loading,
-    loadingComponent: Loading,
-    renderInBottomFilterBar: renderInFilterFactory({
-      showColumnHider: false,
-      showFilters: false,
-      showPagination: true,
-      showRowHeightPicker: false,
-      showSearch: false,
-    }),
-    renderInTopFilterBar: renderInFilterFactory({
-      ...defaultOptions,
-      componentProps: props,
-      queryParam: QUERY_PARAM_TITLE,
-      showFilters: false,
-    }),
-    renderNullDataComponent: () => <NoDataComponent />,
-    useDrillDown: false,
+  const getTableProps = (loading: boolean): BaseListTableProps => {
+    return {
+      columns: tableColumns,
+      data: plansArray,
+      loading,
+      loadingComponent: Loading,
+      renderInBottomFilterBar: renderInFilterFactory({
+        showColumnHider: false,
+        showFilters: false,
+        showPagination: true,
+        showRowHeightPicker: false,
+        showSearch: false,
+      }),
+      renderInTopFilterBar: renderInFilterFactory({
+        ...defaultOptions,
+        componentProps: props,
+        queryParam: QUERY_PARAM_TITLE,
+        showFilters: false,
+      }),
+      renderNullDataComponent: () => <NoDataComponent />,
+      useDrillDown: false,
+    };
   };
 
-  return (
-    <div className="mb-5">
-      <Helmet>
-        <title>{pageTitle}</title>
-      </Helmet>
-      <HeaderBreadcrumbs {...breadCrumbProps} />
-      <h2 className="page-title">{pageTitle}</h2>
-      <DrillDownTable {...tableProps} />
-      <br />
-      <Button className="create-plan" color="primary" tag={Link} to={props.newPlanUrl}>
-        {CREATE_NEW_PLAN}
-      </Button>
-    </div>
-  );
+  const connectedBaseViewProps = {
+    ...props,
+    getTableProps,
+    loadData,
+  };
+
+  const renderConnectedOpenSRPList = () => {
+    return <BaseListComponent {...connectedBaseViewProps} />;
+  };
+
+  return <>{props.renderBody(renderConnectedOpenSRPList)}</>;
 };
 
 OpenSRPPlansList.defaultProps = defaultProps;
 
 export { OpenSRPPlansList };
+
+/** describes props returned by mapStateToProps */
+type DispatchedStateProps = Pick<BaseListComponentProps, 'plansArray'>;
+/** describe mapDispatchToProps object */
+type MapDispatchToProps = Pick<OpenSRPPlanListViewProps, 'fetchPlanRecordsCreator'>;
+
+/** maps props  to state */
+const mapStateToProps = (
+  state: Partial<Store>,
+  ownProps: RouteComponentProps
+): DispatchedStateProps => {
+  const plansArraySelector = makePlansArraySelector(PLAN_RECORD_BY_ID);
+  const title = getQueryParams(ownProps.location)[QUERY_PARAM_TITLE] as string;
+  const planStatus = [PlanStatus.DRAFT];
+  const plansRecordsArray = plansArraySelector(state as Registry, {
+    statusList: planStatus,
+    title,
+  });
+  const props = {
+    plansArray: plansRecordsArray,
+  };
+  return props;
+};
+
+/** map action creators  */
+const mapDispatchToProps: MapDispatchToProps = {
+  fetchPlanRecordsCreator: fetchPlanRecords,
+};
+
+/** container creator that allows for configurable containers */
+// TODO - will need to explicit set types for the args here, for true flexibility
+export const createConnectedOpenSRPPlansList = (
+  mapState = mapStateToProps,
+  mapDispatch = mapDispatchToProps
+) => {
+  return connect(mapState, mapDispatch)(OpenSRPPlansList);
+};
