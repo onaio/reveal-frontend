b7f01a00bbb39fcf2c32a458c4e22bd60b5d24c1
p-netm <pmuriuki98@gmail.com>
2020-07-02 23:24:04 +0300

    Refactor data loading functions

--- src/helpers/dataLoading/jurisdictions.ts
+++ src/helpers/dataLoading/jurisdictions.ts
@@ -1,12 +1,15 @@
 import { Dictionary } from '@onaio/utils/dist/types/types';
 import { URLParams } from '@opensrp/server-service';
-import { PlanDefinition } from '../../configs/settings';
-import { OPENSRP_JURISDICTION_HIERARCHY_ENDPOINT, OPENSRP_LOCATION, OPENSRP_PLANS } from '../../constants';
-import jurisdiction from '../../containers/pages/FocusInvestigation/jurisdiction';
-import { RawOpenSRPHierarchy } from '../../containers/pages/JurisdictionSelectionTable/utils';
-import { OpenSRPService } from '../../services/opensrp';
-import { failure, success } from './utils';
 import { OpenSRPJurisdiction } from '../../components/TreeWalker/types';
+import { PlanDefinition } from '../../configs/settings';
+import {
+  OPENSRP_JURISDICTION_HIERARCHY_ENDPOINT,
+  OPENSRP_LOCATION,
+  OPENSRP_PLANS,
+} from '../../constants';
+import { OpenSRPService } from '../../services/opensrp';
+import { RawOpenSRPHierarchy } from '../../store/ducks/opensrp/hierarchies/types';
+import { failure, success } from './utils';
 
 /** find plans that the given user has access to
  * @param {string} jurisdictionId - username
@@ -16,10 +19,8 @@ import { OpenSRPJurisdiction } from '../../components/TreeWalker/types';
 export async function LoadOpenSRPHierarchy<T = RawOpenSRPHierarchy>(
   jurisdictionId: string,
   service: typeof OpenSRPService = OpenSRPService,
-  params: URLParams | null = null,
-  setLoading: React.Dispatch<React.SetStateAction<boolean>>
+  params: URLParams | null = null
 ) {
-  setLoading(true);
   const serve = new service(OPENSRP_JURISDICTION_HIERARCHY_ENDPOINT);
   return await serve
     .read(jurisdictionId, params)
@@ -28,25 +29,19 @@ export async function LoadOpenSRPHierarchy<T = RawOpenSRPHierarchy>(
     })
     .catch((err: Error) => {
       return failure(err);
-    })
-    .finally(() => {
-      setLoading(false);
     });
 }
 
 /** send plan payload to api
- * @param {string} jurisdictionId - username
- * @param {typeof OpenSRPService} service -  the OpenSRP service
- * @param {URLParams | null} params - search params filters
+ * @param planPayload - original plan definition
+ * @param jurisdictionIds -  the ids to attach to plan
+ * @param  service - the openSRP service
  */
 export async function putJurisdictionsToPlan(
   planPayload: PlanDefinition,
   jurisdictionIds: string[],
-  service: typeof OpenSRPService = OpenSRPService,
-  setLoading?: React.Dispatch<React.SetStateAction<boolean>>
+  service: typeof OpenSRPService = OpenSRPService
 ) {
-  // TODO
-  setLoading && setLoading(true);
   const serve = new service(OPENSRP_PLANS);
   const jurisdictions = jurisdictionIds.map(jurisdictionId => ({ code: jurisdictionId }));
   const payload = { ...planPayload, jurisdiction: jurisdictions };
@@ -57,25 +52,23 @@ export async function putJurisdictionsToPlan(
     })
     .catch((err: Error) => {
       return failure(err);
-    })
-    .finally(() => {
-      setLoading && setLoading(false);
     });
 }
+
+const locationParams = {
+  is_jurisdiction: true,
+  return_geometry: false,
+};
 
 /** given a single jurisdiction, this will return the Jurisdiction object from opensrp */
 export async function loadJurisdiction(
   jurisdictionId: string,
   serviceClass: typeof OpenSRPService,
-  params?: any
+  params: URLParams = locationParams
 ) {
-  const locationParams = {
-    is_jurisdiction: true,
-    return_geometry: false,
-  }
   const service = new serviceClass(OPENSRP_LOCATION);
   return await service
-    .read(jurisdictionId, locationParams)
+    .read(jurisdictionId, params)
     .then((response: OpenSRPJurisdiction) => {
       if (response) {
         return { error: null, value: response };
@@ -85,4 +78,3 @@ export async function loadJurisdiction(
       return { error, value: null };
     });
 }
-
