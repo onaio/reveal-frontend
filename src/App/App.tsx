2a59ba28b314d8301a4c720f35e526a64b4c38e1
p-netm <pmuriuki98@gmail.com>
2020-07-03 18:37:25 +0300

    Register another route for jurisdiction Assignment
    In effect when there is no current parent Id

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -548,6 +548,13 @@ const App = (props: AppProps) => {
                   redirectPath={APP_CALLBACK_URL}
                   disableLoginProtection={DISABLE_LOGIN_PROTECTION}
                   exact={true}
+                  path={`${ASSIGN_JURISDICTIONS_URL}/:planId/:rootId`}
+                  component={ConnectedJurisdictionAssignmentView}
+                />
+                <ConnectedPrivateRoute
+                  redirectPath={APP_CALLBACK_URL}
+                  disableLoginProtection={DISABLE_LOGIN_PROTECTION}
+                  exact={true}
                   path={`${ASSIGN_JURISDICTIONS_URL}/:planId`}
                   component={ConnectedJurisdictionAssignmentView}
                 />
