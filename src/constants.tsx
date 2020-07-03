f48b10130ebf335fd9c0de8e9969c0bf0ef3d9f8
p-netm <pmuriuki98@gmail.com>
2020-07-03 12:33:36 +0300

    update opensrp endpoints

--- src/constants.tsx
+++ src/constants.tsx
@@ -143,8 +143,8 @@ export const TEAM_ID_PARAM = 'team_id';
 export const OPENSRP_TEMPLATE_ENDPOINT = 'template';
 export const OPENSRP_PLANS_BY_USER_FILTER = 'plans/user';
 export const OPENSRP_V1_SETTINGS_ENDPOINT = 'settings/sync';
-// TODO - define this
 export const OPENSRP_JURISDICTION_HIERARCHY_ENDPOINT = 'location/getHierarchy';
+export const OPENSRP_FIND_LOCATION_BY_JURISDICTION_IDS = `location/findByJurisdictionIds`;
 export const OPENSRP_ACTIVE = 'Active';
 export const OPENSRP_STATUS = 'status';
 export const OPENSRP_MANIFEST_ENDPOINT = 'manifest';
