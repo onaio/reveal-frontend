ededed72cf8ecdb3b77fc1c7afd3cc3a99cb7203
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-07-08 12:08:52 +0300

    Remove unused type

--- src/components/TreeWalker/types.ts
+++ src/components/TreeWalker/types.ts
@@ -13,12 +13,6 @@ export interface OpenSRPJurisdiction {
   type: 'Feature';
 }
 
-/** Used to describe OpenSRP jurisdictions in short form */
-export interface SimpleJurisdiction {
-  jurisdiction_id: string;
-  jurisdiction_parent_id: string;
-}
-
 /** Object containing known API endpoints by name */
 export interface APIEndpoints {
   [key: string]: string;
