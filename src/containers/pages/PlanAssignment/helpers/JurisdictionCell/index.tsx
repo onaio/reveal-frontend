2c1db3b22878b1cae848d2dfee94dff1c63d6bcb
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-07-08 15:07:09 +0300

    Refactor to work with tree nodes

--- src/containers/pages/PlanAssignment/helpers/JurisdictionCell/index.tsx
+++ src/containers/pages/PlanAssignment/helpers/JurisdictionCell/index.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {
-  OpenSRPJurisdiction,
-  SimpleJurisdiction,
-} from '../../../../../components/TreeWalker/types';
+import { TreeNode } from '../../../../../store/ducks/opensrp/hierarchies/types';
 
 /**
  * Props for JurisdictionCell
  */
 export interface JurisdictionCellProps {
-  jurisdictionTree: SimpleJurisdiction[] /** the jurisdiction tree, structured as in the adjacency list model */;
-  node: OpenSRPJurisdiction /** the current jurisdiction */;
+  node: TreeNode /** the current jurisdiction as a node from the Jurisdiction Tree */;
   url: string /** the URL to use for display of nodes that do have children */;
 }
 
@@ -23,18 +19,15 @@ export interface JurisdictionCellProps {
  * @param props - the props
  */
 const JurisdictionCell = (props: JurisdictionCellProps) => {
-  const { jurisdictionTree, node, url } = props;
+  const { node, url } = props;
 
-  const knownParentIds = jurisdictionTree.map(item => item.jurisdiction_parent_id);
-  const isLeafNode: boolean = !knownParentIds.includes(node.id);
-
-  if (isLeafNode) {
-    return <span key={`${node.id}-span`}>{node.properties.name}</span>;
+  if (!node.hasChildren()) {
+    return <span key={`${node.model.id}-span`}>{node.model.label}</span>;
   }
 
   return (
-    <Link key={`${node.id}-link`} to={url}>
-      {node.properties.name}
+    <Link key={`${node.model.id}-link`} to={url}>
+      {node.model.label}
     </Link>
   );
 };
