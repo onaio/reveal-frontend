9d1eb18dac4a06d5462e7d2f7d7d6378d177120d
ciremusyoka <wambua.eric01@gmail.com>
2020-07-09 17:24:13 +0300

    Handle expression and discription mixup

--- src/components/forms/PlanForm/components/actions.tsx
+++ src/components/forms/PlanForm/components/actions.tsx
@@ -61,60 +61,62 @@ export const getConditionAndTriggers = (
     }
     if (element.trigger) {
       triggers[element.actionCode] = element.trigger.map((item, mapIndex) => {
-        return (
-          <FormGroup
-            className="trigger-group"
-            row={true}
-            key={`${element.actionCode}-${index}-trigger-${mapIndex}`}
-          >
-            {item.name && (
-              <React.Fragment>
-                <Label for={`activities[${index}].trigger[${mapIndex}].name`} sm={2}>
-                  Name
-                </Label>
-                <Field
-                  className="form-control col-sm-10"
-                  sm={10}
-                  required={true}
-                  type="text"
-                  name={`activities[${index}].trigger[${mapIndex}].name`}
-                  id={`activities[${index}].trigger[${mapIndex}].name`}
-                  disabled={disabledFields}
-                />
-              </React.Fragment>
-            )}
-            {item.description && (
-              <React.Fragment>
-                <Label for={`activities[${index}].trigger[${mapIndex}].expression`} sm={2}>
-                  Condition
-                </Label>
-                <Field
-                  className="form-control col-sm-10"
-                  required={true}
-                  component="textarea"
-                  name={`activities[${index}].trigger[${mapIndex}].expression`}
-                  id={`activities[${index}].trigger[${mapIndex}].expression`}
-                  disabled={disabledFields}
-                />
-              </React.Fragment>
-            )}
-            {item.expression && (
-              <React.Fragment>
-                <Label for={`activities[${index}].trigger[${mapIndex}].description`} sm={2}>
-                  Description
-                </Label>
-                <Field
-                  className="form-control col-sm-10"
-                  required={false}
-                  component="textarea"
-                  name={`activities[${index}].trigger[${mapIndex}].description`}
-                  id={`activities[${index}].trigger[${mapIndex}].description`}
-                  disabled={disabledFields}
-                />
-              </React.Fragment>
-            )}
-          </FormGroup>
-        );
+        if (item.hasOwnProperty('expression')) {
+          return (
+            <FormGroup
+              className="trigger-group"
+              row={true}
+              key={`${element.actionCode}-${index}-trigger-${mapIndex}`}
+            >
+              {item.name && (
+                <React.Fragment>
+                  <Label for={`activities[${index}].trigger[${mapIndex}].name`} sm={2}>
+                    Name
+                  </Label>
+                  <Field
+                    className="form-control col-sm-10"
+                    sm={10}
+                    required={true}
+                    type="text"
+                    name={`activities[${index}].trigger[${mapIndex}].name`}
+                    id={`activities[${index}].trigger[${mapIndex}].name`}
+                    disabled={disabledFields}
+                  />
+                </React.Fragment>
+              )}
+              {item.expression && (
+                <React.Fragment>
+                  <Label for={`activities[${index}].trigger[${mapIndex}].expression`} sm={2}>
+                    Expression
+                  </Label>
+                  <Field
+                    className="form-control col-sm-10"
+                    required={true}
+                    component="textarea"
+                    name={`activities[${index}].trigger[${mapIndex}].expression`}
+                    id={`activities[${index}].trigger[${mapIndex}].expression`}
+                    disabled={disabledFields}
+                  />
+                </React.Fragment>
+              )}
+              {item.description && (
+                <React.Fragment>
+                  <Label for={`activities[${index}].trigger[${mapIndex}].description`} sm={2}>
+                    Description
+                  </Label>
+                  <Field
+                    className="form-control col-sm-10"
+                    required={false}
+                    component="textarea"
+                    name={`activities[${index}].trigger[${mapIndex}].description`}
+                    id={`activities[${index}].trigger[${mapIndex}].description`}
+                    disabled={disabledFields}
+                  />
+                </React.Fragment>
+              )}
+            </FormGroup>
+          );
+        }
       });
     }
   }
