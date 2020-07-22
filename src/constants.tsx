73c15f4a786c12c867bddbd99fd96b394fb37e75
p-netm <pmuriuki98@gmail.com>
2020-07-23 00:06:04 +0300

    Add constants

--- src/constants.tsx
+++ src/constants.tsx
@@ -110,6 +110,7 @@ export const EDIT_SERVER_SETTINGS_URL = '/server-settings';
 export const PLANNING_VIEW_URL = '/plans/planning';
 export const NEW_PLANNING_PLAN_URL = `${PLANNING_VIEW_URL}/${NEW}`;
 export const ASSIGN_JURISDICTIONS_URL = '/assignJurisdictions';
+export const MANUAL_ASSIGN_JURISDICTIONS_URL = '/manualSelectJurisdictions';
 export const AUTO_ASSIGN_JURISDICTIONS_URL = '/auoSelectJurisdictions';
 /** how long after opening a logout window should we wait before redirecting to express' server logout
  * I am not sure the optimum value for this, mozilla firefox seems to take some
