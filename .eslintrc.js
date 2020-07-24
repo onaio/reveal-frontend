03832daa84bc72d8277f6d2e2b9e3c132a2d081b
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2020-07-24 17:54:15 +0300

    Whitelist snapshots

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const { mapKeys } = require('lodash');
+
+const allowedSnapshots = {
+  'src/components/GisidaWrapper/tests/__snapshots__/index.test.tsx.snap': [
+    'components/GisidaWrapper renders map component with FeatureCollection 1',
+  ],
+  'src/containers/pages/AssigmentMapWrapper/tests/__snapshots__/index.test.tsx.snap': [
+    'containers/pages/AssigmentMapWrapper renders jurisdictions assignment map 1',
+  ],
+};
+const whitelistedSnapshots = mapKeys(allowedSnapshots, (val, file) =>
+  path.resolve(__dirname, file)
+);
+
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    'jest/globals': true,
+  },
+  extends: ['airbnb', 'plugin:prettier/recommended'],
+  rules: {
+    strict: 0,
+    'jest/no-large-snapshots': [
+      'error',
+      {
+        maxSize: 1000,
+        whitelistedSnapshots,
+      },
+    ],
+  },
+  plugins: ['jest'],
+  parserOptions: {
+    ecmaVersion: 2015,
+  },
+};
