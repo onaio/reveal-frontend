4ceadf9baf9b4d7386b4f2ba175c43d6ee5fb8cd
p-netm <pmuriuki98@gmail.com>
2020-07-29 10:43:36 +0300

    Add tests for timeline slider component

--- src/components/TimeLineSlider/tests/index.test.tsx
+++ src/components/TimeLineSlider/tests/index.test.tsx
@@ -6,7 +6,7 @@ import { TimelineSlider, TimelineSliderProps } from '..';
 describe('src/components/TimeLineSlider', () => {
   it('works correctly', () => {
     const props: TimelineSliderProps = {
-      keyOfCurrentStop: '0',
+      keyOfCurrentStop: '1',
       stops: [
         {
           keys: '1',
@@ -24,6 +24,48 @@ describe('src/components/TimeLineSlider', () => {
     expect(wrapper.text()).toMatchInlineSnapshot(
       `"AUTO_TARGET_JURISDICTIONS_BY_RISKREFINE_SELECTED_JURISDICTIONS"`
     );
+    wrapper.find('li').forEach(li => {
+      expect(toJson(li)).toMatchSnapshot();
+    });
+  });
+  it('adds correct classNames ', () => {
+    const props: TimelineSliderProps = {
+      keyOfCurrentStop: '3',
+      stops: [
+        {
+          keys: '1',
+          labelBelowStop: 'AUTO_TARGET_JURISDICTIONS_BY_RISK',
+          labelInStop: '1',
+        },
+        {
+          keys: ['2', '3'],
+          labelBelowStop: 'REFINE_SELECTED_JURISDICTIONS',
+          labelInStop: '2',
+        },
+      ],
+    };
+    const wrapper = mount(<TimelineSlider {...props} />);
+    wrapper.find('li').forEach(li => {
+      expect(toJson(li)).toMatchSnapshot();
+    });
+  });
+  it('edge case: key is not part of any stop ', () => {
+    const props: TimelineSliderProps = {
+      keyOfCurrentStop: 'wierdStuff',
+      stops: [
+        {
+          keys: '1',
+          labelBelowStop: 'AUTO_TARGET_JURISDICTIONS_BY_RISK',
+          labelInStop: '1',
+        },
+        {
+          keys: ['2', '3'],
+          labelBelowStop: 'REFINE_SELECTED_JURISDICTIONS',
+          labelInStop: '2',
+        },
+      ],
+    };
+    const wrapper = mount(<TimelineSlider {...props} />);
     wrapper.find('li').forEach(li => {
       expect(toJson(li)).toMatchSnapshot();
     });
