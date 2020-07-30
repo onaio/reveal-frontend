9c01a3fd139f16f698b42775ba1b4633fc63d3f0
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2020-07-30 17:58:10 +0300

    Display plan form global errors as toast errors
    In order to improve usability on the PlanForm, the global errors are now displayed as toast errors with the toast requiring to be closed manually

--- src/helpers/errors.ts
+++ src/helpers/errors.ts
@@ -6,12 +6,22 @@ import { growl } from './utils';
 /** union of all ErrorTypes that we are working with */
 type ServiceError = HTTPError | Error | NetworkError;
 
+/** autoClose types */
+type AutoClose = 5000 | false;
+
 /**
  * Display error message using growl
  * @param error - the error object
  * @param customMessage - custom error message
+ * @param autoClose - Delay in ms to close the toast. If set to false,
+ * the notification needs to be closed manually
  */
-export const displayError = (error: ServiceError, customMessage: string = '') => {
+
+export const displayError = (
+  error: ServiceError,
+  customMessage: string = '',
+  autoClose: AutoClose = 5000
+) => {
   /** opensrp error object for plans after filtering plans by a user when the assignments are expired
    * unfortunately putting this in constants does not work; it has a value of undefined during runtime.
    */
@@ -19,20 +29,20 @@ export const displayError = (error: ServiceError, customMessage: string = '') =>
     const isPlansFilterByUserRoute = error.url.includes('rest/plans/user/');
     if (error.statusCode === 403) {
       const msg = customMessage !== '' ? customMessage : ACCESS_DENIED;
-      growl(msg, { type: toast.TYPE.ERROR });
+      growl(msg, { autoClose, type: toast.TYPE.ERROR });
       return;
     }
     if (error.statusCode === 500 && isPlansFilterByUserRoute) {
       const msg = customMessage !== '' ? customMessage : USER_HAS_NO_VALID_ASSIGNMENTS;
-      growl(msg, { type: toast.TYPE.ERROR });
+      growl(msg, { autoClose, type: toast.TYPE.ERROR });
       return;
     }
   }
   if (error instanceof NetworkError) {
     const msg = customMessage !== '' ? customMessage : NETWORK_ERROR;
-    growl(msg, { type: toast.TYPE.ERROR });
+    growl(msg, { autoClose, type: toast.TYPE.ERROR });
     return;
   }
   const message = customMessage !== '' ? customMessage : error.message;
-  growl(message, { type: toast.TYPE.ERROR });
+  growl(message, { autoClose, type: toast.TYPE.ERROR });
 };
