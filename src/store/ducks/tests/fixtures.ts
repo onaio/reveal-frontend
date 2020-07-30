233bcbbac5ad86e6d5563e0e4b27dc4a1c95fc76
p-netm <pmuriuki98@gmail.com>
2020-07-30 20:23:42 +0300

    Fix regression errors in tests

--- src/store/ducks/tests/fixtures.ts
+++ src/store/ducks/tests/fixtures.ts
@@ -1794,3 +1794,70 @@ export const jurisdictionsMetadataArray = [
     serverVersion: 1592833111093,
   },
 ];
+
+export const irsPlanDefinition1 = {
+  action: [
+    {
+      code: 'IRS',
+      description: 'Visit each structure in the operational area and attempt to spray',
+      goalId: 'IRS',
+      identifier: '79255d56-9190-578d-8452-fc8520f24f2b',
+      prefix: 1,
+      reason: 'Routine',
+      subjectCodableConcept: {
+        text: 'Residential_Structure',
+      },
+      taskTemplate: 'Spray_Structures',
+      timingPeriod: {
+        end: '2019-08-16',
+        start: '2019-08-09',
+      },
+      title: 'Spray Structures',
+    },
+  ],
+  date: '2019-08-09',
+  effectivePeriod: {
+    end: '2019-08-29',
+    start: '2019-08-09',
+  },
+  goal: [
+    {
+      description: 'Spray structures in the operational area',
+      id: 'IRS',
+      priority: 'medium-priority',
+      target: [
+        {
+          detail: {
+            detailQuantity: {
+              comparator: '>=',
+              unit: 'Percent',
+              value: 90,
+            },
+          },
+          due: '2019-08-16',
+          measure: 'Percent of structures sprayed',
+        },
+      ],
+    },
+  ],
+  identifier: '0230f9e8-1f30-5e91-8693-4c993661785e',
+  jurisdiction: [
+    {
+      code: '3952',
+    },
+  ],
+  name: 'IRS-2019-08-09',
+  status: 'draft',
+  title: 'IRS 2019-08-09',
+  useContext: [
+    {
+      code: 'interventionType',
+      valueCodableConcept: 'IRS',
+    },
+    {
+      code: 'taskGenerationStatus',
+      valueCodableConcept: 'False',
+    },
+  ],
+  version: '1',
+};
