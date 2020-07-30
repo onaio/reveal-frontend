61ae1738b297bc7b8d0679180dc2a8cc577967f1
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2020-07-30 18:07:49 +0300

    Refactor code

--- src/helpers/errors.ts
+++ src/helpers/errors.ts
@@ -1,5 +1,5 @@
 import { HTTPError, NetworkError } from '@opensrp/server-service';
-import { toast } from 'react-toastify';
+import { toast, ToastOptions } from 'react-toastify';
 import { ACCESS_DENIED, NETWORK_ERROR, USER_HAS_NO_VALID_ASSIGNMENTS } from '../configs/lang';
 import { growl } from './utils';
 
@@ -25,24 +25,26 @@ export const displayError = (
   /** opensrp error object for plans after filtering plans by a user when the assignments are expired
    * unfortunately putting this in constants does not work; it has a value of undefined during runtime.
    */
+  const toastOptions: ToastOptions = { autoClose, type: toast.TYPE.ERROR };
+
   if (error instanceof HTTPError) {
     const isPlansFilterByUserRoute = error.url.includes('rest/plans/user/');
     if (error.statusCode === 403) {
       const msg = customMessage !== '' ? customMessage : ACCESS_DENIED;
-      growl(msg, { autoClose, type: toast.TYPE.ERROR });
+      growl(msg, toastOptions);
       return;
     }
     if (error.statusCode === 500 && isPlansFilterByUserRoute) {
       const msg = customMessage !== '' ? customMessage : USER_HAS_NO_VALID_ASSIGNMENTS;
-      growl(msg, { autoClose, type: toast.TYPE.ERROR });
+      growl(msg, toastOptions);
       return;
     }
   }
   if (error instanceof NetworkError) {
     const msg = customMessage !== '' ? customMessage : NETWORK_ERROR;
-    growl(msg, { autoClose, type: toast.TYPE.ERROR });
+    growl(msg, toastOptions);
     return;
   }
   const message = customMessage !== '' ? customMessage : error.message;
-  growl(message, { autoClose, type: toast.TYPE.ERROR });
+  growl(message, toastOptions);
 };
