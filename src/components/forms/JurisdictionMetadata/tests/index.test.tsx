ab5baba68513168d61c91d9442f78165a82792db
ciremusyoka <wambua.eric01@gmail.com>
2020-08-14 15:49:26 +0300

    Test submit button text change

--- src/components/forms/JurisdictionMetadata/tests/index.test.tsx
+++ src/components/forms/JurisdictionMetadata/tests/index.test.tsx
@@ -3,7 +3,7 @@ import { mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import React from 'react';
 import JurisdictionMetadataForm, { submitForm } from '..';
-import { INVALID_CSV } from '../../../../configs/lang';
+import { INVALID_CSV, UPLOAD_FILE, UPLOADING_FILE } from '../../../../configs/lang';
 
 describe('components/forms/JurisdictionMetadata', () => {
   beforeEach(() => {
@@ -54,9 +54,16 @@ describe('components/forms/JurisdictionMetadata', () => {
     const fileInput = getByText('Upload File');
     fireEvent.change(fileInput, { target: { file: [file] } });
     fireEvent.submit(getByTestId('form'));
+    // submit button text changes
+    expect((container.getElementsByClassName('btn-primary').item(0) as any).textContent).toEqual(
+      UPLOADING_FILE
+    );
     /** Assert Validation Response and Button disable */
     await waitFor(() => {
       expect(getByText('Upload File')).toBeDisabled();
+      expect((container.getElementsByClassName('btn-primary').item(0) as any).textContent).toEqual(
+        UPLOAD_FILE
+      );
     });
     /** Assert uploaded csv is invalid */
     expect(container.getElementsByClassName('text-danger').length).toEqual(1);
