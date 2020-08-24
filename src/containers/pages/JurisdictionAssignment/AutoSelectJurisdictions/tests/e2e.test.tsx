406141995e7cbd6d529e8eed3013a49c5fb63bd4
p-netm <pmuriuki98@gmail.com>
2020-08-24 13:31:49 +0300

    Test autoselection when no jurisdiction selections exist

--- src/containers/pages/JurisdictionAssignment/AutoSelectJurisdictions/tests/e2e.test.tsx
+++ src/containers/pages/JurisdictionAssignment/AutoSelectJurisdictions/tests/e2e.test.tsx
@@ -10,11 +10,14 @@ import { AUTO_ASSIGN_JURISDICTIONS_URL } from '../../../../../constants';
 import { renderTable } from '../../../../../helpers/testUtils';
 import store from '../../../../../store';
 import hierarchiesReducer, {
+  deforest,
+  deselectAllNodes,
   reducerName as hierarchiesReducerName,
 } from '../../../../../store/ducks/opensrp/hierarchies';
 import { zambiaHierarchy } from '../../../../../store/ducks/opensrp/hierarchies/tests/fixtures';
 import plansReducer, { reducerName } from '../../../../../store/ducks/opensrp/PlanDefinition';
 import { plans } from '../../../../../store/ducks/opensrp/PlanDefinition/tests/fixtures';
+import { ResourceCalculation } from '../../helpers/ResourceCalcWidget';
 import { autoSelectionJurisdictionMeta } from './fixtures';
 
 reducerRegistry.register(reducerName, plansReducer);
@@ -36,7 +39,11 @@ jest.mock('../../../AssigmentMapWrapper', () => {
 });
 
 describe('e2e test auto-selection flow', () => {
-  it('from slider auto-selection to refine jurisdictions', async () => {
+  afterEach(() => {
+    store.dispatch(deselectAllNodes('22bc44dd-752d-4c20-8761-617361b4f1e7'));
+    store.dispatch(deforest());
+  });
+  it('from slider auto-selection to refine jurisdictions', async done => {
     const envModule = require('../../../../../configs/env');
     envModule.ASSIGNMENT_PAGE_SHOW_MAP = true;
 
@@ -296,5 +303,181 @@ describe('e2e test auto-selection flow', () => {
         ).toBeFalsy();
       }
     });
+    done();
+  });
+
+  it('when there are no selected jurisdictions(without metaData)', async done => {
+    const envModule = require('../../../../../configs/env');
+    envModule.ASSIGNMENT_PAGE_SHOW_MAP = true;
+
+    const App = () => {
+      return (
+        <Switch>
+          <Route
+            exact={true}
+            path={`${AUTO_ASSIGN_JURISDICTIONS_URL}/:planId/:rootId/:parentId`}
+            component={ConnectedAutoSelectView}
+          />
+          <Route
+            exact={true}
+            path={`${AUTO_ASSIGN_JURISDICTIONS_URL}/:planId/:rootId`}
+            component={ConnectedAutoSelectView}
+          />
+        </Switch>
+      );
+    };
+
+    const plan = plans[0];
+    const rootId = '22bc44dd-752d-4c20-8761-617361b4f1e7';
+    fetch
+      .once(JSON.stringify([]), { status: 200 })
+      .once(JSON.stringify(plan), { status: 200 })
+      .once(JSON.stringify(zambiaHierarchy), { status: 200 });
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter
+          initialEntries={[
+            {
+              hash: '',
+              pathname: `${AUTO_ASSIGN_JURISDICTIONS_URL}/${plan.identifier}/${rootId}`,
+              search: '',
+              state: {},
+            },
+          ]}
+        >
+          <App />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    await act(async () => {
+      await new Promise(resolve => setImmediate(resolve));
+      wrapper.update();
+    });
+
+    expect(wrapper.text()).toMatchSnapshot('general page snapshot');
+
+    /** we start with the slider component : there is no metadata so no matter what
+     * threshold we set there will be no selected jurisdictions
+     */
+    // CWY_25 (178 structures) is selected by default during mount since it has a threshold of 80 > 0
+    expect(wrapper.find('div.slider-section').text()).toMatchInlineSnapshot(`"Risk  0%00100"`);
+    expect(wrapper.find('div.info-section').text()).toMatchInlineSnapshot(
+      `"NUMBER OF STRUCTURES IN SELECTED JURISDICTIONS0"`
+    );
+
+    // click next to proceed to the structureSummary view
+    wrapper.find('JurisdictionSelectionsSlider button.btn-success').simulate('click');
+
+    act(() => {
+      wrapper.update();
+    });
+
+    expect(wrapper.text()).toMatchSnapshot('structure summary step snapshot');
+
+    /** structure summary step, this would not have any records */
+
+    renderTable(wrapper, 'will not have any nodes');
+    const tbodyRow = wrapper.find('tbody tr');
+    expect(tbodyRow.length).toEqual(0);
+    expect(wrapper.find('SelectedStructuresTable').text()).toMatchInlineSnapshot(
+      `"....NameStructures CountNo rows foundContinue to next step"`
+    );
+
+    /** What of the resource calculation component */
+
+    // is an error page => will refactor this to an info message instead
+    expect(wrapper.find(ResourceCalculation).text()).toMatchInlineSnapshot(
+      `"An error ocurred. Please try and refresh the page.The specific error is: No Jurisdiction selections were found"`
+    );
+
+    // click next goto the refine jurisdiction selections section
+    wrapper.find('SelectedStructuresTable button.btn-success').simulate('click');
+
+    await act(async () => {
+      await new Promise(resolve => setImmediate(resolve));
+      wrapper.update();
+    });
+
+    // expect(wrapper.text()).toMatchSnapshot('refine selected jurisdictions component');
+
+    /** we should now be in the jurisdictions refinement component,  */
+
+    // here we check that the whole tree is shown
+    expect(wrapper.text()).toMatchSnapshot('full page rendered text');
+
+    const bodyRows = wrapper.find('tbody tr');
+    // test drilldown
+    bodyRows
+      .at(0)
+      .find('NodeCell Link a')
+      .simulate('click', { button: 0 });
+
+    await act(async () => {
+      await new Promise(resolve => setImmediate(resolve));
+      wrapper.update();
+    });
+
+    // all children of Zambia are shown:
+    expect(wrapper.find('tbody tr').length).toEqual(6);
+
+    // simulate a click to check one of the immediate children of Zambia: Western
+    // => effectively checking all leaf nodes that are descendants of Western
+    wrapper.find('tbody tr').forEach(tr => {
+      if (tr.text().includes('Western')) {
+        // check the checkbox
+        tr.find('input').simulate('change', { target: { name: '', checked: true } });
+      }
+    });
+
+    await act(async () => {
+      await new Promise(resolve => setImmediate(resolve));
+      wrapper.update();
+    });
+
+    // check for new selections
+    wrapper.find('tbody tr').forEach(tr => {
+      if (tr.text().includes('Western')) {
+        expect(
+          tr
+            .find('td input')
+            .at(0)
+            .props().checked
+        ).toBeTruthy();
+      } else {
+        expect(
+          tr
+            .find('td input')
+            .at(0)
+            .props().checked
+        ).toBeFalsy();
+      }
+    });
+
+    /** the resource calculation component updates accordingly */
+    expect(wrapper.find(ResourceCalculation).text()).toMatchInlineSnapshot(
+      `"Resource Estimate for Zambia0 daysat a rate of   structures per team per day with  Teams"`
+    );
+
+    // check resource calc behavior when drilling down to children node that does not
+    // have selected jurisdictions
+    wrapper.find('tbody tr').forEach(tr => {
+      if (tr.text().includes('Eastern')) {
+        // check the checkbox
+        tr.find('NodeCell Link a').simulate('click', { button: 0 });
+      }
+    });
+    await act(async () => {
+      await new Promise(resolve => setImmediate(resolve));
+      wrapper.update();
+    });
+
+    /** the resource calculation component updates accordingly */
+    expect(wrapper.find(ResourceCalculation).text()).toMatchInlineSnapshot(
+      `"An error ocurred. Please try and refresh the page.The specific error is: No Jurisdiction selections were found"`
+    );
+
+    done();
   });
 });
