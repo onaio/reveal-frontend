e23719ba097b25dfa5657387574bf3d1d07d8be3
p-netm <pmuriuki98@gmail.com>
2020-09-04 13:00:49 +0300

    Remove metadata from jurisdiction name when creating setting payload

--- src/components/forms/JurisdictionMetadata/tests/index.test.tsx
+++ src/components/forms/JurisdictionMetadata/tests/index.test.tsx
@@ -1,9 +1,14 @@
-import { fireEvent, render, waitFor } from '@testing-library/react';
+import { act, fireEvent, render, waitFor } from '@testing-library/react';
 import { mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
+import fs from 'fs';
+import { createBrowserHistory } from 'history';
+import path from 'path';
 import React from 'react';
+import { Router } from 'react-router';
 import JurisdictionMetadataForm, { submitForm } from '..';
 import { INVALID_CSV, UPLOAD_FILE, UPLOADING_FILE } from '../../../../configs/lang';
+import { body } from './fixtures';
 
 describe('components/forms/JurisdictionMetadata', () => {
   beforeEach(() => {
@@ -98,5 +103,60 @@ describe('components/forms/JurisdictionMetadata', () => {
     await waitFor(() => {
       expect(mockedOpenSRPservice).toHaveBeenCalledTimes(1);
     });
+  });
+});
+
+describe('uploads form', () => {
+  const fetch = require('jest-fetch-mock');
+  fetch.mockResponse(JSON.stringify({}), { status: 201 });
+
+  const history = createBrowserHistory();
+
+  it('uploads a csv file correctly, makes correct calls', async () => {
+    const wrapper = mount(
+      <Router history={history}>
+        <JurisdictionMetadataForm />
+      </Router>
+    );
+
+    const riskFileSampleDir = path.join(__dirname, 'fixtures', 'riskTest.csv');
+    const fileContents = fs.readFileSync(riskFileSampleDir);
+    const derivedBlob = new File([fileContents], 'riskTest.csv', { type: 'text/csv' });
+
+    // simulate uploading the csv file.
+    const fileInputField = wrapper.find('input#file');
+    expect(toJson(fileInputField)).toMatchSnapshot('file input field');
+    fileInputField.simulate('change', { target: { files: [derivedBlob] } });
+
+    expect(wrapper.find('button[type="submit"]').props().disabled).toBeFalsy();
+
+    // simulate submit
+    wrapper.find('form').simulate('submit');
+
+    await act(async () => {
+      await new Promise(resolve => setImmediate(resolve));
+      wrapper.update();
+    });
+
+    await waitFor(() => {
+      expect(fetch).toHaveBeenCalled();
+    });
+
+    expect(fetch.mock.calls).toEqual([
+      [
+        'https://reveal-stage.smartregister.org/opensrp/rest/settings/sync',
+        {
+          'Cache-Control': 'no-cache',
+          Pragma: 'no-cache',
+          body: JSON.stringify(body),
+          headers: {
+            accept: 'application/json',
+            authorization: 'Bearer null',
+            'content-type': 'application/json;charset=UTF-8',
+          },
+          method: 'POST',
+        },
+      ],
+    ]);
   });
 });
