69293ce6759780b12be7bd9f59c5bc6e8f94dace
p-netm <pmuriuki98@gmail.com>
2020-09-21 21:53:34 +0300

    Add an integration test

---
+++ src/containers/pages/InterventionPlan/UpdatePlan/tests/dynamicFi.test.tsx
@@ -0,0 +1,129 @@
+import { mount } from 'enzyme';
+import { createBrowserHistory } from 'history';
+import { cloneDeep } from 'lodash';
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+import { Router } from 'react-router';
+import { UpdatePlan } from '..';
+import { DynamicFIPlan } from '../../../../../components/forms/PlanForm/tests/fixtures';
+import { PlanDefinition } from '../../../../../configs/settings';
+import { PLAN_UPDATE_URL } from '../../../../../constants';
+import store from '../../../../../store';
+import { removePlanDefinitions } from '../../../../../store/ducks/opensrp/PlanDefinition';
+
+/* tslint:disable-next-line no-var-requires */
+const fetch = require('jest-fetch-mock');
+
+const history = createBrowserHistory();
+
+jest.mock('../PlanLocationNames', () => {
+  const PlanLocationNamesMock = () => <div id="Helmuth"> No plan survives enemy contact. </div>;
+
+  return PlanLocationNamesMock;
+});
+
+describe('components/InterventionPlan/UpdatePlan', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+    fetch.resetMocks();
+    jest.clearAllMocks();
+    store.dispatch(removePlanDefinitions());
+  });
+
+  it('checking taskGeneration status is correct for dynamic-fI plans when one is given', async () => {
+    fetch.mockResponseOnce(JSON.stringify({}));
+
+    const plan = cloneDeep(DynamicFIPlan) as PlanDefinition;
+    fetch.mockResponseOnce(JSON.stringify({}));
+    const genericMock: any = jest.fn();
+
+    const props = {
+      history,
+      location: genericMock,
+      match: {
+        isExact: true,
+        params: { id: plan.identifier },
+        path: `${PLAN_UPDATE_URL}/:id`,
+        url: `${PLAN_UPDATE_URL}/${plan.identifier}`,
+      },
+      plan,
+    };
+    const wrapper = mount(
+      <Router history={history}>
+        <UpdatePlan {...props} />
+      </Router>
+    );
+    await act(async () => {
+      await new Promise<unknown>(resolve => setImmediate(resolve));
+      wrapper.update();
+    });
+
+    wrapper.find('form').simulate('submit');
+
+    await act(async () => {
+      await new Promise<any>(resolve => setImmediate(resolve));
+      wrapper.update();
+    });
+
+    const createdPayload = fetch.mock.calls[1][1].body;
+    const createdContext = JSON.parse(createdPayload).useContext;
+
+    expect(createdContext).toEqual([
+      { code: 'interventionType', valueCodableConcept: 'Dynamic-FI' },
+      { code: 'fiStatus', valueCodableConcept: 'A1' },
+      { code: 'fiReason', valueCodableConcept: 'Routine' },
+      { code: 'taskGenerationStatus', valueCodableConcept: 'True' },
+    ]);
+  });
+
+  it('checking taskGeneration status is correct for dynamic-fI plans where one is not defined', async () => {
+    fetch.mockResponseOnce(JSON.stringify({}));
+
+    const plan = cloneDeep(DynamicFIPlan) as PlanDefinition;
+    plan.useContext = [
+      { code: 'interventionType', valueCodableConcept: 'Dynamic-FI' },
+      { code: 'fiStatus', valueCodableConcept: 'A1' },
+      { code: 'fiReason', valueCodableConcept: 'Routine' },
+    ];
+    fetch.mockResponseOnce(JSON.stringify({}));
+    const genericMock: any = jest.fn();
+
+    const props = {
+      history,
+      location: genericMock,
+      match: {
+        isExact: true,
+        params: { id: plan.identifier },
+        path: `${PLAN_UPDATE_URL}/:id`,
+        url: `${PLAN_UPDATE_URL}/${plan.identifier}`,
+      },
+      plan,
+    };
+    const wrapper = mount(
+      <Router history={history}>
+        <UpdatePlan {...props} />
+      </Router>
+    );
+    await act(async () => {
+      await new Promise<unknown>(resolve => setImmediate(resolve));
+      wrapper.update();
+    });
+
+    wrapper.find('form').simulate('submit');
+
+    await act(async () => {
+      await new Promise<any>(resolve => setImmediate(resolve));
+      wrapper.update();
+    });
+
+    const createdPayload = fetch.mock.calls[1][1].body;
+    const createdContext = JSON.parse(createdPayload).useContext;
+
+    expect(createdContext).toEqual([
+      { code: 'interventionType', valueCodableConcept: 'Dynamic-FI' },
+      { code: 'fiStatus', valueCodableConcept: 'A1' },
+      { code: 'fiReason', valueCodableConcept: 'Routine' },
+      { code: 'taskGenerationStatus', valueCodableConcept: 'Disabled' },
+    ]);
+  });
+});
