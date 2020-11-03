83e8d273db22dfb0ba20525cfc0b0e4e20a671f6
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-11-03 12:32:37 +0300

    Add array index to key

--- src/components/forms/PlanForm/index.tsx
+++ src/components/forms/PlanForm/index.tsx
@@ -695,7 +695,7 @@ const PlanForm = (props: PlanFormProps) => {
               render={arrayHelpers => (
                 <div>
                   {values.activities.map((arrItem, index) => (
-                    <div className="card mb-3" key={`div${arrItem.actionCode}`}>
+                    <div className="card mb-3" key={`div${arrItem.actionCode}-${index}`}>
                       <h5 className="card-header position-relative">
                         {values.activities[index].actionTitle}
                         {values.activities && values.activities.length > 1 && !editMode && (
@@ -710,7 +710,7 @@ const PlanForm = (props: PlanFormProps) => {
                         )}
                       </h5>
                       <div className="card-body">
-                        <fieldset key={`fieldset${arrItem.actionCode}`}>
+                        <fieldset key={`fieldset${arrItem.actionCode}-${index}`}>
                           {errors.activities && errors.activities[index] && (
                             <div
                               className={`alert alert-danger activities-${index}-errors`}
