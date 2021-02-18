e9e6b00f9850239ecf94c5fdd6cf98399bffc66c
ciremusyoka <wambua.eric01@gmail.com>
2021-02-18 16:01:26 +0300

    Build jurisdiction tree

--- src/components/forms/LocationSelect/helpers.tsx
+++ src/components/forms/LocationSelect/helpers.tsx
@@ -1,22 +1,55 @@
+import _, { keyBy } from 'lodash';
 import { JurisdictionOption } from '../JurisdictionSelect';
 
+/** structure of jurisdiction tree */
 export interface JurisdictionTree {
   id: string;
+  isOpen?: boolean;
   key: string;
   label: string;
-  node: JurisdictionTree[];
+  nodes: {
+    [key: string]: JurisdictionTree;
+  };
   parentId: string;
 }
+
+export interface JurisdictionTreeById {
+  [key: string]: JurisdictionTree;
+}
+
+/**
+ * builds the jurisdiction tree
+ * @param {JurisdictionOption} jurisdictions - api response
+ * @param {string} parentId
+ * @param {JurisdictionTreeById} currentTreeData - The current displayed tree
+ * @param {string} key
+ */
 export const buildLocationTree = (
   jurisdictions: JurisdictionOption[],
-  parentId: string
-): JurisdictionTree[] => {
-  const tree: JurisdictionTree[] = jurisdictions.map(jur => ({
-    id: jur.id,
-    key: jur.id,
-    label: jur.properties.name,
-    node: [],
-    parentId,
-  }));
+  parentId: string,
+  currentTreeData: JurisdictionTreeById,
+  key: string
+): JurisdictionTreeById => {
+  const tree: JurisdictionTreeById = keyBy(
+    jurisdictions.map(jur => ({
+      id: jur.id,
+      key: jur.id,
+      label: jur.properties.name,
+      nodes: {},
+      parentId,
+    })),
+    'key'
+  );
+  if (Object.keys(currentTreeData).length) {
+    const usedKeys = key ? key.split('/') : [];
+    if (usedKeys.length === 1) {
+      currentTreeData[usedKeys[0]].nodes = tree;
+    }
+    if (usedKeys.length > 1) {
+      const listToString = `${usedKeys.join('.nodes.')}.nodes`;
+      _.set(currentTreeData, listToString, tree);
+    }
+    return currentTreeData;
+  }
   return tree;
 };
--- src/components/forms/LocationSelect/index.tsx
+++ src/components/forms/LocationSelect/index.tsx
@@ -1,15 +1,17 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react';
-import TreeMenu, { ItemComponent } from 'react-simple-tree-menu';
+import TreeMenu, { ItemComponent, TreeNode } from 'react-simple-tree-menu';
 import 'react-simple-tree-menu/dist/main.css';
+import { toast } from 'react-toastify';
 import { Col, Row } from 'reactstrap';
 import { OPENSRP_ACTIVE } from '../../../constants';
 import { displayError } from '../../../helpers/errors';
+import { growl } from '../../../helpers/utils';
 import { getFilterParams, OpenSRPService } from '../../../services/opensrp';
 import { NoDataComponent } from '../../Table/NoDataComponent';
 import { FIND_BY_PROPERTIES } from '../../TreeWalker/constants';
 import { JurisdictionOption } from '../JurisdictionSelect';
-import { buildLocationTree, JurisdictionTree } from './helpers';
+import { buildLocationTree, JurisdictionTreeById } from './helpers';
 import './index.css';
 
 interface LocationSelectProps {
@@ -19,9 +21,24 @@ interface LocationSelectProps {
 
 const LocationSelect = (props: LocationSelectProps) => {
   const { service, endpoint } = props;
-  const [treeData, setTreeData] = useState<JurisdictionTree[]>([]);
+  const [treeData, setTreeData] = useState<JurisdictionTreeById>({});
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+  const [openNodes, setOpenNodes] = useState<string[]>([]);
 
-  const getJurisdiction = (parentId: string = '') => {
+  const getJurisdiction = (
+    parentId: string = '',
+    key: string = '',
+    clickedNodes: string[] = []
+  ) => {
+    if (isFetching) {
+      return;
+    }
+    if (clickedNodes.length && clickedNodes.includes(key)) {
+      const newNodeitems = clickedNodes.filter(node => node !== key);
+      setOpenNodes(newNodeitems);
+      return;
+    }
+    setIsFetching(true);
     const propertiesToFilter = {
       status: OPENSRP_ACTIVE,
       ...(parentId === '' ? { geographicLevel: 0 } : { parentId }),
@@ -35,30 +52,40 @@ const LocationSelect = (props: LocationSelectProps) => {
     apiService
       .list({ ...params })
       .then((jurisdictionApiPayload: JurisdictionOption[]) => {
-        if (jurisdictionApiPayload.length) {
-          const tree = buildLocationTree(jurisdictionApiPayload, parentId);
+        if (!jurisdictionApiPayload.length) {
+          growl('Jurisdiction has no children', {
+            type: toast.TYPE.SUCCESS,
+          });
+        } else {
+          const tree = buildLocationTree(jurisdictionApiPayload, parentId, treeData, key);
           setTreeData(tree);
+          const openCurrent = [...clickedNodes, key];
+          setOpenNodes(parentId === key ? [parentId] : openCurrent);
         }
       })
       .catch((error: Error) => {
         displayError(error);
-      });
+      })
+      .finally(() => setIsFetching(false));
   };
 
   useEffect(() => {
     getJurisdiction();
   }, []);
 
-  if (!treeData.length) {
+  if (!Object.keys(treeData).length) {
     return <NoDataComponent />;
   }
   return (
     <TreeMenu
-      data={treeData}
+      data={treeData as TreeNode}
       hasSearch={false}
       debounceTime={125}
+      openNodes={openNodes}
       // tslint:disable-next-line: jsx-no-lambda
-      onClickItem={() => getJurisdiction}
+      onClickItem={propsss => {
+        getJurisdiction(propsss.id, propsss.key, propsss.openNodes);
+      }}
     >
       {({ items }) => (
         <React.Fragment>
