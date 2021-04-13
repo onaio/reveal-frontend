bf55e0fa74b881cb5c01192e85644596b76d70f9
ciremusyoka <wambua.eric01@gmail.com>
2021-04-13 10:16:20 +0300

    Translate missed string

--- src/containers/pages/MDALite/wardReports/helpers.tsx
+++ src/containers/pages/MDALite/wardReports/helpers.tsx
@@ -3,6 +3,7 @@ import { Dictionary } from '@onaio/utils';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Cell } from 'react-table';
+import { NAME } from '../../../../configs/lang';
 import { REPORT_MDA_LITE_CDD_REPORT_URL } from '../../../../constants';
 import { censusPopColumns, genderReportColumns } from '../../GenericJurisdictionReport/helpers';
 
@@ -14,7 +15,7 @@ export const wardColumns: Array<DrillDownColumn<Dictionary>> = [
       const url = `${REPORT_MDA_LITE_CDD_REPORT_URL}/${original.plan_id}/${original.base_entity_id}`;
       return <Link to={url}>{cell.value}</Link>;
     },
-    Header: 'Name',
+    Header: NAME,
     accessor: 'ward_name',
   },
   ...genderReportColumns,
