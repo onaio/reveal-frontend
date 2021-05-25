57ef2fa4b3280b66e025e9f2a79518e5846eb5bb
ciremusyoka <wambua.eric01@gmail.com>
2021-05-25 18:49:36 +0300

    Filter superset request for getting MDA-Point Child Report data. Closes #1575

--- src/containers/pages/MDAPoint/ChildReports/ChildSupersetDataTable/tests/index.test.tsx
+++ src/containers/pages/MDAPoint/ChildReports/ChildSupersetDataTable/tests/index.test.tsx
@@ -1,7 +1,29 @@
+import { SupersetFormData } from '@onaio/superset-connector';
 import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
+import flushPromises from 'flush-promises';
 import React from 'react';
 import { ChildSupersetDataTable } from '..';
+
+const supersetFetchParams = {
+  adhoc_filters: [
+    {
+      clause: 'WHERE',
+      comparator: '99642369-fec4-4cb2-99c6-c901228b3adf',
+      expressionType: 'SIMPLE',
+      operator: '==',
+      subject: 'jurisdiction_id',
+    },
+    {
+      clause: 'WHERE',
+      comparator: 'eb601285-d5c2-55c3-bcd5-c29ae71a6236',
+      expressionType: 'SIMPLE',
+      operator: '==',
+      subject: 'plan_id',
+    },
+  ],
+  row_limit: 150,
+} as SupersetFormData;
 
 const props = {
   data: [
@@ -11,6 +33,7 @@ const props = {
   fetchItems: jest.fn(),
   headerItems: ['name', 'col-1', 'col-2', 'col-3'],
   service: jest.fn(),
+  supersetFetchParams,
   supersetSliceId: '1',
 };
 
@@ -23,8 +46,11 @@ describe('components/ChildSupersetDataTable', () => {
     shallow(<ChildSupersetDataTable {...props} />);
   });
 
-  it('should render list correctly', () => {
+  it('should render list correctly', async () => {
     const wrapper = mount(<ChildSupersetDataTable {...props} />);
+
+    await flushPromises();
+    expect(props.service).toBeCalledWith(props.supersetSliceId, props.supersetFetchParams);
 
     expect(wrapper.find('ListView').length).toEqual(1);
     expect(toJson(wrapper.find('ListView table'))).toMatchSnapshot('table with data');
