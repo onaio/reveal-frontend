9736bd73dac119113d5423cfd6cf3d0b639ed09d
haseemVD <haseem.haq@venturedive.com>
2021-05-26 13:40:42 +0500

    update case triggered and routine plan checks

--- src/containers/pages/InterventionPlan/UpdatePlan/index.tsx
+++ src/containers/pages/InterventionPlan/UpdatePlan/index.tsx
@@ -27,7 +27,6 @@ import {
   NEW_PLAN_URL,
   OPENSRP_PLANS,
   PLAN_LIST_URL,
-  ROUTINE,
 } from '../../../../constants';
 import { displayError } from '../../../../helpers/errors';
 import { OpenSRPService } from '../../../../services/opensrp';
@@ -141,18 +140,12 @@ const UpdatePlan = (props: RouteComponentProps<RouteParams> & UpdatePlanProps) =
     initialValues.fiReason === CASE_TRIGGERED;
 
   const isCaseTriggeredOrRoutinePlan =
-    CASE_TRIGGERED_DRAFT_EDIT_ADD_ACTIVITIES &&
-    (planStatus === PlanStatus.DRAFT ||
-      planStatus === PlanStatus.ACTIVE ||
-      planStatus === PlanStatus.COMPLETE) &&
-    isFIOrDynamicFI(initialValues.interventionType) &&
-    initialValues.fiReason !== CASE_TRIGGERED &&
-    initialValues.fiReason !== ROUTINE;
+    CASE_TRIGGERED_DRAFT_EDIT_ADD_ACTIVITIES && isFIOrDynamicFI(initialValues.interventionType);
 
   const hiddenFields =
     HIDE_PLAN_FORM_FIELDS_ON_EDIT.length > 0 &&
     hideFieldsOnPlanStatuses.includes(initialValues.status) &&
-    !isCaseTriggeredOrRoutinePlan
+    isCaseTriggeredOrRoutinePlan
       ? HIDE_PLAN_FORM_FIELDS_ON_EDIT
       : [];
 
